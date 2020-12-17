816bae620fde0a13990647a0fee6ac7b780ea5fb
estebandalelr <estebandalelr@gmail.com>
2020-12-16 20:14:00 -0600

    Improved Api getAllQuestions

--- pages/api/getAllQuestions.js
+++ pages/api/getAllQuestions.js
@@ -1,6 +1,11 @@
+let db = { questions: [] }
 
+const sendMail = () => {
+  console.log("mail")
+}
 export default function handler(req, res) {
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify({ questions: ["hola"] }))
+  sendMail()
+  res.end(JSON.stringify({ questions: db }))
 }
