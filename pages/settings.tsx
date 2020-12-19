5cb34f4c4b99b2880df38909ad937b1bc5858367
estebandalelr <estebandalelr@gmail.com>
2020-12-18 19:16:57 -0600

    added Save button

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,7 +1,11 @@
+import Button from "../components/Button"
 function Settings() {
   return (
     <div>
       <h1>Settings</h1>
+      <div className="w-full flex justify-end">
+        <Button onClick={e => console.log(e)} text="Save" />
+      </div>
     </div>
   )
 }
