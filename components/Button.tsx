b437492812f2edd208fd27b4b3faa840ca37aebe
estebandalel-p44 <esteban.dalel@project44.com>
2020-12-20 22:43:47 -0500

    Style button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,5 @@
 const Button = ({ onClick, text, disabled = false }) =>
-  <button onClick={onClick} disabled={disabled}>
+  <button onClick={onClick} disabled={disabled} className="bg-pink-800 text-white px-4 py-2 rounded shadow-sm">
     {text}
   </button>
 export default Button
