aca13612045f8af9d666121ffab786d21765bb48
estebandalel-p44 <esteban.dalel@project44.com>
2020-12-28 19:24:00 -0500

    Created global firebase object

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,26 @@
 import Layout from '../components/Layout'
 import '../styles/index.css'
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAxio45RoVcMHwwjYnl7-QcvTVzm46X7fk",
+  authDomain: "watermelon-b6dd2.firebaseapp.com",
+  projectId: "watermelon-b6dd2",
+  storageBucket: "watermelon-b6dd2.appspot.com",
+  messagingSenderId: "736231654320",
+  appId: "1:736231654320:web:ca4e788e8ceca4a89a0c75"
+}
+let firebaseApp
+if (!firebase.apps.length) {
+  firebaseApp = firebase.initializeApp(firebaseConfig)
+}
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <>
       <Layout>
-        <Component {...pageProps} />
+        <Component {...pageProps} firebaseApp={firebaseApp} />
       </Layout>
     </>
   )
