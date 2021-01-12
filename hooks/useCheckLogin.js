5a7b5a45f5c7414dded4cf10738584c3326f6a53
estebandalel-p44 <esteban.dalel@project44.com>
2021-01-12 18:05:00 -0500

    Added console log for debug

--- hooks/useCheckLogin.js
+++ hooks/useCheckLogin.js
@@ -9,6 +9,7 @@ function useCheckLogin() {
     router = useRouter();
     isLoggedIn = window?.localStorage?.getItem("sign_in_token");
     hasAddedToSlack = window?.localStorage?.getItem("add_to_slack_token");
+    console.log(isLoggedIn, hasAddedToSlack);
     if (!isLoggedIn) router.push("/login");
     if (!hasAddedToSlack) router.push("/welcome");
   }, []);
