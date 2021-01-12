930494cf192a85430ca24af6728d7b5ea3da92bd
estebandalel-p44 <esteban.dalel@project44.com>
2021-01-12 17:06:22 -0500

    Fixed router case

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
-import Layout from '../components/Layout'
-import '../styles/index.css'
-import firebase from 'firebase';
-import 'firebase/firestore';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import Layout from "../components/Layout";
+import "../styles/index.css";
+import firebase from "firebase";
+import "firebase/firestore";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAxio45RoVcMHwwjYnl7-QcvTVzm46X7fk",
@@ -11,30 +11,29 @@ const firebaseConfig = {
   projectId: "watermelon-b6dd2",
   storageBucket: "watermelon-b6dd2.appspot.com",
   messagingSenderId: "736231654320",
-  appId: "1:736231654320:web:ca4e788e8ceca4a89a0c75"
-}
-let firebaseApp
+  appId: "1:736231654320:web:ca4e788e8ceca4a89a0c75",
+};
+let firebaseApp;
 if (!firebase.apps.length) {
-  firebaseApp = firebase.initializeApp(firebaseConfig)
+  firebaseApp = firebase.initializeApp(firebaseConfig);
 }
 const MyApp = ({ Component, pageProps }) => {
-
-let isLoggedIn = false
-let hasAddedToSlack =false
-useEffect(()=>{
- isLoggedIn= window?.localStorage?.getItem("sign_in_token");
- hasAddedToSlack= window?.localStorage?.getItem("add_to_slack_token");
-},[])
-  const router = useRouter()
-  if(!isLoggedIn) router.push("login")
-  if(!hasAddedToSlack) router.push("welcome")
+  let isLoggedIn = false;
+  let hasAddedToSlack = false;
+  const router = useRouter();
+  useEffect(() => {
+    isLoggedIn = window?.localStorage?.getItem("sign_in_token");
+    hasAddedToSlack = window?.localStorage?.getItem("add_to_slack_token");
+  }, []);
+  if (!isLoggedIn) router.push("login");
+  if (!hasAddedToSlack) router.push("welcome");
   return (
     <>
       <Layout isLoggedIn={isLoggedIn}>
         <Component {...pageProps} firebaseApp={firebaseApp} />
       </Layout>
     </>
-  )
-}
+  );
+};
 
-export default MyApp
+export default MyApp;
