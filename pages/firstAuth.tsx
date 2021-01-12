b59c8dd3a3c41090698582b3f4c528ff072c137e
estebandalel-p44 <esteban.dalel@project44.com>
2021-01-11 19:10:39 -0500

    Change text in first auth

--- pages/firstAuth.tsx
+++ pages/firstAuth.tsx
@@ -13,8 +13,17 @@ const FirstAuth = ({}) => {
   });
   return (
     <div>
-      <h1>Redirecting you</h1>
-      <p>Hang on a second</p>
+      <h1>Welcome to watermelon</h1>
+      <p>Please install the app on your workspace</p>
+      <a href="https://slack.com/oauth/v2/authorize?client_id=1471534976662.1575212081829&scope=chat:write,team:read,channels:manage,commands,users:read.email,users:read,dnd:read&user_scope=">
+        <img
+          alt="Add to Slack"
+          height="40"
+          width="139"
+          src="https://platform.slack-edge.com/img/add_to_slack.png"
+          srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+        />
+      </a>
     </div>
   );
 };
