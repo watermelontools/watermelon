768987e0490b153c3c22a79c392187c949f375e6
estebandalel-p44 <esteban.dalel@project44.com>
2021-01-19 20:52:59 -0500

    Removed broken button

--- pages/firstAuth.tsx
+++ pages/firstAuth.tsx
@@ -45,15 +45,6 @@ const FirstAuth = ({ firebaseApp, token }) => {
     <div>
       <h1>Welcome to watermelon</h1>
       <p>Please install the app on your workspace</p>
-      <a href="https://slack.com/oauth/v2/authorize?client_id=1471534976662.1575212081829&scope=channels:manage,chat:write,commands,team:read,users:read&user_scope=">
-        <img
-          alt="Add to Slack"
-          height="40"
-          width="139"
-          src="https://platform.slack-edge.com/img/add_to_slack.png"
-          srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
-        />
-      </a>
       <a
         href={`https://slack.com/oauth/v2/authorize?scope=incoming-webhook,commands,chat:write&client_id=1471534976662.1575212081829&team=${token.team.id}&redirect_uri=https://app.watermelon.tools/welcome`}
       >
