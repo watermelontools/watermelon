ceed6a53fd8eb41b2de5b502d86f390de20ea9f9
estebandalel-p44 <esteban.dalel@project44.com>
2021-03-14 20:04:03 -0500

    Add bicolor bg to firstauth

--- pages/firstAuth.tsx
+++ pages/firstAuth.tsx
@@ -42,22 +42,26 @@ const FirstAuth = ({ firebaseApp, token }) => {
   }, []);
   console.log(token);
   return (
-    <div className="flex justify-center items-center h-screen w-full">
-      <div className="rounded shadow p-4">
-        <h1>Welcome to Watermelon!</h1>
-        <p>Please install the app on your workspace</p>
-        <div className="w-full flex justify-center items-center my-2">
-          <a
-            href={`https://slack.com/oauth/v2/authorize?scope=incoming-webhook,groups:write,channels:manage,channels:read,chat:write,commands&client_id=1471534976662.1575212081829&team=${token.team.id}&redirect_uri=https://app.watermelon.tools/welcome`}
-          >
-            <img
-              alt="Add to Slack"
-              height="40"
-              width="139"
-              src="https://platform.slack-edge.com/img/add_to_slack.png"
-              srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
-            />
-          </a>
+    <div className="grid-rows-2">
+      <div className="row-start-1 row-end-2 bg-pink-600 w-full"></div>
+      <div className="row-start-2 row-end-3 white w-full"></div>
+      <div className="flex justify-center items-center h-screen w-full row-span-full">
+        <div className="rounded shadow p-4">
+          <h1>Welcome to Watermelon!</h1>
+          <p>Please install the app on your workspace</p>
+          <div className="w-full flex justify-center items-center my-2">
+            <a
+              href={`https://slack.com/oauth/v2/authorize?scope=incoming-webhook,groups:write,channels:manage,channels:read,chat:write,commands&client_id=1471534976662.1575212081829&team=${token.team.id}&redirect_uri=https://app.watermelon.tools/welcome`}
+            >
+              <img
+                alt="Add to Slack"
+                height="40"
+                width="139"
+                src="https://platform.slack-edge.com/img/add_to_slack.png"
+                srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+              />
+            </a>
+          </div>
         </div>
       </div>
     </div>
