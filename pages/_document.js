f78956c0bd4b8176299e02456f1d79d344aac323
estebandalel-p44 <esteban.dalel@project44.com>
2021-03-20 16:34:27 -0500

    Fix charSet error

--- pages/_document.js
+++ pages/_document.js
@@ -1,26 +1,26 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
 
   render() {
     return (
       <Html>
-        <Head >
-        <meta charset="utf-8" />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"></link>
-        <meta name="slack-app-id" content="A01GX682DQD"></meta>
+        <Head>
+          <meta charSet="utf-8" />
+          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+          <meta name="slack-app-id" content="A01GX682DQD"></meta>
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
