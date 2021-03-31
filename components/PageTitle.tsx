f8df756cf77192a2cf47877e5a5bcb7f6e398b1e
estebandalel-p44 <esteban.dalel@project44.com>
2021-03-31 10:22:22 -0500

    Added Page title component

---
+++ components/PageTitle.tsx
@@ -0,0 +1,7 @@
+const PageTitle = ({ pageTitle }: { pageTitle: string }) => {
+    return <div style={{ background: "linear-gradient(42deg, rgba(173,56,56,1) 0%, rgba(219,97,97,1) 50%, rgba(249,246,244,1) 90%, rgba(255,255,255,1) 100%)" }}
+        className="text-white py-2 font-semibold text-xl">
+        <h1>{pageTitle}</h1>
+    </div>
+}
+export default PageTitle
