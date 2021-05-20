974738285c35b188a89f176b8a95996df32df632
estebandalel-p44 <esteban.dalel@project44.com>
2021-05-19 21:51:03 -0500

    Added firebase fetch

--- pages/api/admin/ping.js
+++ pages/api/admin/ping.js
@@ -1,13 +1,30 @@
+import admin from "../../utils/firebase/backend";
+
 import Airtable from "airtable";
 Airtable.configure({
   endpointUrl: "https://api.airtable.com",
   apiKey: process.env.AIRTABLE_API_KEY,
 });
 const airtableBase = Airtable.base("appyNw8U8LEBl4iPs");
+let db = admin.firestore();
+
 export default function handler(req, res) {
   const settings = { language: "en", category: "hobbies" };
   let allQuestions = [];
-
+  db.collection("teams")
+    .doc(`T01JG9GD4JW`)
+    .get()
+    .then((fbRes) => {
+      let responseData = fbRes.data();
+      if (responseData.settings) {
+        settings = responseData.settings;
+      }
+      console.log("settings", settings);
+    })
+    .catch(function (error) {
+      console.error("Error writing: ", error);
+      res.status(500).json(JSON.stringify({ ok: false }));
+    });
   airtableBase(settings.language)
     .select({
       // Selecting the first 3 records in Grid view:
@@ -18,7 +35,7 @@ export default function handler(req, res) {
       function page(records, fetchNextPage) {
         // This function (`page`) will get called for each page of records.
         records.forEach(function (record) {
-          allQuestions.push(record);
+          allQuestions.push(record.fields);
         });
         fetchNextPage();
       },
