7f7a1dcd5ad0070ec62bdba46218c8dfaa293a0f
estebandalel-p44 <esteban.dalel@project44.com>
2021-05-19 21:28:43 -0500

    Added airtable

--- pages/api/admin/ping.js
+++ pages/api/admin/ping.js
@@ -1,4 +1,34 @@
+Airtable.configure({
+  endpointUrl: "https://api.airtable.com",
+  apiKey: process.env.AIRTABLE_API_KEY,
+});
+const airtableBase = Airtable.base("appyNw8U8LEBl4iPs");
 export default function handler(req, res) {
-  console.log("ping", Date.now());
-  res.status(200).send({ ok: "ok" });
+  const settings = { language: "en", category: "hobbies" };
+  let allQuestions = [];
+
+  airtableBase(settings.language)
+    .select({
+      // Selecting the first 3 records in Grid view:
+
+      filterByFormula: `({Category} = '${settings.category}')`,
+    })
+    .eachPage(
+      function page(records, fetchNextPage) {
+        // This function (`page`) will get called for each page of records.
+        records.forEach(function (record) {
+          allQuestions.push(record);
+        });
+        fetchNextPage();
+      },
+
+      function done(err) {
+        if (err) {
+          console.error("errDone", err);
+          return;
+        }
+        console.log(allQuestions);
+        res.status(200).send({ ok: "ok" });
+      }
+    );
 }
