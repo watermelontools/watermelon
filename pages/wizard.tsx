097d696a249076cd06e22c4f39146119ee0cd4aa
estebandalel-p44 <esteban.dalel@project44.com>
2021-05-23 15:13:07 -0500

    Add dropdown for weekday and hours

--- pages/wizard.tsx
+++ pages/wizard.tsx
@@ -8,6 +8,9 @@ const Wizard = ({ token, redirect}) => {
   const router = useRouter();
   const [lang, setLang] = useState("en")
   const [cat, setCat] = useState("hobbies")
+  const [weekday, setWeekday] = useState(0)
+  const [hour, setHour] = useState(15)
+  const [timezone, setTimezone ] = useState("")
   const [exampleQuestion, setExampleQuestion] = useState(1)
   let signInToken = {team: {id:""}}
   useEffect(()=>{
@@ -82,6 +85,28 @@ const Wizard = ({ token, redirect}) => {
     {value:"hobbies", label:"Hobbies"},
     {value:"profDev", label:"Professional Development"},
   ]
+  const weekdayOpts =[
+    {value:0, label:"Monday"},
+    {value:1, label:"Tuesday"},
+    {value:2, label:"Wednesday"},
+    {value:3, label:"Thursday"},
+    {value:4, label:"Friday"},
+    {value:5, label:"Saturday"},
+  ]
+  const hourOpts =[
+    {value:7,label:'07:00'},
+    {value:8,label:'08:00'},
+    {value:9,label:'09:00'},
+    {value:10,label:'10:00'},
+    {value:11,label:'11:00'},
+    {value:12,label:'12:00'},
+    {value:13,label:'13:00'},
+    {value:14,label:'14:00'},
+    {value:15,label:'15:00'},
+    {value:16,label:'16:00'},
+    {value:17,label:'17:00'},
+    {value:18,label:'18:00'},
+  ]
   return (
     <>
       <PageTitle pageTitle="The finishing touches" />
@@ -96,9 +121,23 @@ const Wizard = ({ token, redirect}) => {
           </div>
           <div className="card-style flex flex-col justify-between w-80">
             <div>
-
             <h2 className="font-bold text-xl">Question Type</h2>
             <p>Select the category of the questions to be shown</p>
+            </div>
+            <Select onChange={e => setWeekday(e.value)} value={weekdayOpts.find(el=> el.value=== weekday)} options={weekdayOpts} />
+          </div>
+          <div className="card-style flex flex-col justify-between w-80">
+            <div>
+            <h2 className="font-bold text-xl">Weekday to ask</h2>
+            <p>Select the day of the week to send the questions</p>
+            </div>
+            <Select onChange={e => setHour(e.value)} value={hourOpts.find(el=> el.value=== hour)} options={hourOpts} />
+          </div>
+          <div className="card-style flex flex-col justify-between w-80">
+            <div>
+            <h2 className="font-bold text-xl">Hour to ask</h2>
+            <p>Select the hour of the day to send the questions</p>
+            <p>This will happen on {weekday}</p>
             </div>
             <Select onChange={e => setCat(e.value)} value={catOpts.find(el=> el.value=== cat)} options={catOpts} />
           </div>
