58254a65c40aa7e35073201d166bde3a0a657ba9
estebandalel-p44 <esteban.dalel@project44.com>
2021-05-26 20:55:06 -0500

    Fix error return on empty params

--- pages/api/admin/cron/create/sendquestions.ts
+++ pages/api/admin/cron/create/sendquestions.ts
@@ -1,24 +1,25 @@
 export default function handler(req, res) {
   let { signInToken, weekday, hour } = req.body;
+
+  if (!signInToken) return res.status(401).json({ error: "no token", code: 1 });
+
+  let responseObject = {};
   let url = `https://${
     process.env.isDev === "true" ? "dev." : ""
   }app.watermelon.tools/api/admin/slack/sendquestions/${signInToken?.team?.id}`;
-  if(!signInToken)
-   return res.status(401).json({error: "no token", code: 1})
-  if(!weekday)
-   return res.status(401).json({error: "no weekday", code: 2})
-  if(!hour)
-   return res.status(401).json({error: "no hour", code: 3})
+
+  if (!weekday) responseObject = { weekday: "no weekday" };
+  if (!hour) responseObject = { hour: "no hour" };
   fetch(
     `https://www.easycron.com/rest/add?token=${process.env.EASYCRON_API_KEY}
-    &cron_expression=0 ${hour} * * ${weekday} *
+    &cron_expression=0 ${hour || 15} * * ${weekday || "THU"} *
     &url=${url}
-    &cron_job_name=sendquestions-${ signInToken?.team?.id }`
+    &cron_job_name=sendquestions-${signInToken?.team?.id}`
   )
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      res.status(200).json(JSON.stringify({ ok: "ok" }));
+      res.status(200).json(JSON.stringify({ ok: "ok", ...responseObject }));
     })
     .catch((error) => res.status(500).json(error));
 }
