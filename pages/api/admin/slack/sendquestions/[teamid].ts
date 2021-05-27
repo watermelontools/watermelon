3166e67775e1679301d11ab13b0559a1317a0245
estebandalel-p44 <esteban.dalel@project44.com>
2021-05-27 18:12:52 -0500

    Fix function to use await

--- pages/api/admin/slack/sendquestions/[teamid].ts
+++ pages/api/admin/slack/sendquestions/[teamid].ts
@@ -1,12 +1,12 @@
 import admin from "../../../../../utils/firebase/backend";
 import logger from "../../../../../logger/logger";
-
+import { postMessage } from "../../../../../utils/slack/backend";
 import Airtable from "airtable";
 Airtable.configure({
   endpointUrl: "https://api.airtable.com",
   apiKey: process.env.AIRTABLE_API_KEY,
 });
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const {
     query: { teamid },
   } = req;
@@ -17,51 +17,32 @@ export default function handler(req, res) {
     res.status(400).json({ status: "error", error: "no team id" });
   }
   let db = admin.firestore();
-  const postMessage = async ({ data, token }) => {
-    let postData = { ...data };
-    console.log("postData", postData);
-    let postURL = `https://slack.com/api/chat.postMessage?channel=${
-      data.channel
-    }&text=${encodeURI(data.text)}`;
-    console.log(postURL);
-    fetch(postURL, {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .then((response) => response.json())
-      .then((resjson) => {
-        console.log("postmessage", resjson);
-        res.status(200).send({ status: "finished", resjson });
-      })
-      .catch((err) => {
-        console.error("post Message", err);
+  let fbRes = await db.collection("teams").doc(teamid).get();
+  if (fbRes.exists) {
+    let data = fbRes.data();
+    if (data?.add_to_slack_token?.access_token) {
+      let postMessageRes = await postMessage({
+        data: {
+          text: "Hellow from watermelon api aaaa",
+          channel: data.add_to_slack_token.incoming_webhook.channel_id,
+        },
+        token: data.add_to_slack_token.access_token,
       });
-  };
-
-  db.collection("teams")
-    .doc(teamid)
-    .get()
-    .then(async (fbres) => {
-      if (fbres.exists) {
-        let data = fbres.data();
-        if (data?.add_to_slack_token?.access_token)
-          await postMessage({
-            data: {
-              text: "Hellow from watermelon api",
-              channel: data.add_to_slack_token.incoming_webhook.channel_id,
-            },
-            token: data.add_to_slack_token.access_token,
-          });
-        else {
-          logger.error({ message: "no access token", data });
-          res.status(500).send({ status: "error", error: "no access token" });
-        }
+      if (postMessageRes.status === "ok") {
+        logger.info(postMessageRes);
+        res.status(200).send(postMessageRes);
+      } else {
+        logger.error(postMessageRes);
+        res.status(500).send(postMessageRes);
       }
-    })
-    .catch(function (error) {
-      logger.error({ message: "Error fetching: ", error });
-      res.status(500).json(JSON.stringify({ ok: false }));
-    });
+    } else {
+      logger.error({ message: "no access token", data });
+      res.status(500).send({ status: "error", error: "no access token" });
+    }
+  } else {
+    logger.error({ message: "Error fetching: " });
+    res
+      .status(500)
+      .json(JSON.stringify({ ok: false, message: "error fetching" }));
+  }
 }
