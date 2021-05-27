9eafb702315bfb8bc1718c411a496318467d4e7f
estebandalel-p44 <esteban.dalel@project44.com>
2021-05-26 21:58:00 -0500

    Delete test lambda

--- pages/api/admin/testlogger.js
+++
@@ -1,8 +0,0 @@
-import logger from "../../../logger/logger";
-
-export default function handler(req, res) {
-  logger.error("loggertest");
-
-  console.error("no team id");
-  res.status(400).json({ status: "error", error: "no team id" });
-}
