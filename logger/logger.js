4a47d5a7954432773f9610190f141a745434f730
estebandalel-p44 <esteban.dalel@project44.com>
2021-06-02 22:28:53 -0500

    Use set logger

--- logger/logger.js
+++ logger/logger.js
@@ -1,30 +1,27 @@
 import pino from "pino";
 import { logflarePinoVercel } from "pino-logflare";
 
-let logger = {};
 // create pino-logflare console stream for serverless functions and send function for browser logs
-if (process.env.LOGFLARE_API_KEY) {
-  const { stream, send } = logflarePinoVercel({
-    apiKey: process.env.LOGFLARE_API_KEY,
-    sourceToken: "d2681dae-244d-495c-a0d1-2fc3e4c88dba",
-  });
+const { stream, send } = logflarePinoVercel({
+  apiKey: "GzedT0NDpXjZ",
+  sourceToken: "d2681dae-244d-495c-a0d1-2fc3e4c88dba",
+});
 
-  // create pino loggger
-  const logger = pino(
-    {
-      browser: {
-        transmit: {
-          send: send,
-        },
-      },
-      level: "debug",
-      base: {
-        processes_str: JSON.stringify(process.versions),
-        revision: process.env.VERCEL_GITHUB_COMMIT_SHA,
+// create pino loggger
+const logger = pino(
+  {
+    browser: {
+      transmit: {
+        send: send,
       },
     },
-    stream
-  );
-}
+    level: "debug",
+    base: {
+      processes_str: JSON.stringify(process.versions),
+      revision: process.env.VERCEL_GITHUB_COMMIT_SHA,
+    },
+  },
+  stream
+);
 
 export default logger;
