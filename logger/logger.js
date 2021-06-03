b8d2d6448bedd8c58e617033ee5c978244f06f08
estebandalel-p44 <esteban.dalel@project44.com>
2021-06-02 22:22:26 -0500

    Chhanged logger to reduce error surface

--- logger/logger.js
+++ logger/logger.js
@@ -1,27 +1,30 @@
 import pino from "pino";
 import { logflarePinoVercel } from "pino-logflare";
 
+let logger = {};
 // create pino-logflare console stream for serverless functions and send function for browser logs
-const { stream, send } = logflarePinoVercel({
-  apiKey: process.env.LOGFLARE_API_KEY,
-  sourceToken: "d2681dae-244d-495c-a0d1-2fc3e4c88dba",
-});
+if (process.env.LOGFLARE_API_KEY) {
+  const { stream, send } = logflarePinoVercel({
+    apiKey: process.env.LOGFLARE_API_KEY,
+    sourceToken: "d2681dae-244d-495c-a0d1-2fc3e4c88dba",
+  });
 
-// create pino loggger
-const logger = pino(
-  {
-    browser: {
-      transmit: {
-        send: send,
+  // create pino loggger
+  const logger = pino(
+    {
+      browser: {
+        transmit: {
+          send: send,
+        },
+      },
+      level: "debug",
+      base: {
+        processes_str: JSON.stringify(process.versions),
+        revision: process.env.VERCEL_GITHUB_COMMIT_SHA,
       },
     },
-    level: "debug",
-    base: {
-      processes_str: JSON.stringify(process.versions),
-      revision: process.env.VERCEL_GITHUB_COMMIT_SHA,
-    },
-  },
-  stream
-);
+    stream
+  );
+}
 
 export default logger;
