1db27e93137590b3f08bff5d5d6e3d10cffc40fb
estebandalel-p44 <esteban.dalel@project44.com>
2021-06-09 23:55:14 -0500

    Create all crons on creation

--- pages/api/saveSettings.js
+++ pages/api/saveSettings.js
@@ -4,25 +4,56 @@ export default function handler(req, res) {
   let db = admin.firestore();
   let body = JSON.parse(req.body);
   let { lang, cat, signInToken, weekday, hour, isWizard } = body;
-  let url = `https://${
+  const baseurl = `https://${
     proces.env.isDev === "true" ? "dev." : ""
-  }app.watermelon.tools/api/admin/ping`;
+  }app.watermelon.tools/api/admin/`;
+  let nextWeekday = (weekday) => {
+    switch (weekday) {
+      case "MON":
+        return "TUE";
+      case "TUE":
+        return "WED";
+      case "WED":
+        return "THU";
+      case "THU":
+        return "FRI";
+      case "FRI":
+        return "SAT";
+      case "SAT":
+        return "MON";
+      default:
+        break;
+    }
+  };
   db.collection("teams")
     .doc(`${signInToken.team.id}`)
     .set(
       { settings: { language: lang, category: cat, weekday, hour } },
       { merge: true }
     )
-    .then(function (docRef) {
-      if (isWizard)
-        fetch(
-          `https://www.easycron.com/rest/add?token=${process.env.EASYCRON_API_KEY}&cron_expression=0 ${hour} * * ${weekday} *&url=${url}&cron_job_name=${signInToken.team.id}`
-        )
-          .then((response) => response.json())
-          .then((data) => {
-            logger.info({ message: "cron-created", data });
-            res.status(200).json(JSON.stringify({ ok: "ok" }));
-          });
+    .then(function () {
+      if (isWizard) {
+        let urls = [
+          { route: "sendquestions", hour, weekday },
+          { route: "clearooms", hour: hour - 1, weekday: nextWeekday(weekday) },
+          { route: "addtorooms", hour: hour, weekday: nextWeekday(weekday) },
+        ];
+        urls.forEach((element) => {
+          await fetch(
+            `https://www.easycron.com/rest/add?token=${
+              process.env.EASYCRON_API_KEY
+            }
+          &cron_expression=0 ${element.hour} * * ${element.weekday} *
+          &url=${baseurl + element.url}
+          &cron_job_name=${signInToken.team.id}`
+          )
+            .then((response) => response.json())
+            .then((data) => {
+              logger.info({ message: "cron-created", data });
+            });
+        });
+        res.status(200).json(JSON.stringify({ ok: "ok" }));
+      }
     })
     .catch(function (error) {
       console.error("Error writing: ", error);
