d53e4d507916fbcd6285a4788437f3f10c428890
estebandalel-p44 <esteban.dalel@project44.com>
2021-06-17 22:36:43 -0500

    Restore some code for test

--- pages/wizard.tsx
+++ pages/wizard.tsx
@@ -106,7 +106,36 @@ const Wizard = ({  }) => {
   return (
     <>
       <PageTitle pageTitle="The finishing touches" />
+      <PagePadder>
+      <div className="flex justify-start items-start h-screen w-full flex-col flex-wrap">
+        <div className="flex hover:bg-gray-50 border-gray-200 border-t-2 w-full mt-2">
+          <div className="rounded mx-2" style={{width: "3em", height: "3em"}}>
+          <img src="/wmslack.png" />
+          </div>
+          <div>
+            <div className="flex">
 
+          <p className="font-bold text-lg">Watermelon</p>
+          <div className="flex-col flex justify-center">
+
+          <p className="bg-gray-200 rounded-sm text-xs ml-2 px-1 text-gray-500 leading-1">APP</p>
+          </div>
+            </div>
+          <p className="font-semibold">{questions[exampleQuestion].question}</p>
+          <div className="flex justify-start my-1">
+          <p className="border border-gray-200 rounded px-2 py-1 mr-2 hover:bg-gray-100">{questions[exampleQuestion].ansA}</p>
+          <p className="border border-gray-200 rounded px-2 py-1 hover:bg-gray-100">{questions[exampleQuestion].ansB}</p>
+          </div>
+          <p>{questions[exampleQuestion].icebreaker}</p>
+          </div>
+        </div>
+        <div className="my-2 flex justify-end w-full">
+         <button onClick={(e)=> saveSettings()} className="text-white font-semibold bg-green-400 rounded shadow-sm py-2 px-3">
+           Finish
+         </button>
+       </div>
+      </div>
+      </PagePadder>
     </>
   )
 }
@@ -126,7 +155,6 @@ export async function getServerSideProps(context) {
     }app.watermelon.tools/wizard`
   );
   let token = await f.json();
-  console.log(token)
   //createInitialGroups({token})
   if(token.ok){
     let db = admin.firestore();
