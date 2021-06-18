36bfa36373e3b28e8d6b4f1aacb1fa41a0cab34e
estebandalel-p44 <esteban.dalel@project44.com>
2021-06-17 22:08:49 -0500

    Delete all code for debug

--- pages/wizard.tsx
+++ pages/wizard.tsx
@@ -106,66 +106,7 @@ const Wizard = ({  }) => {
   return (
     <>
       <PageTitle pageTitle="The finishing touches" />
-      <PagePadder>
-      <div className="flex justify-start items-start h-screen w-full flex-col flex-wrap">
-        <div className="flex sm:flex-col md:flex-row">
-          <div className="card-style flex flex-col justify-between w-80">
-            <h2 className="font-bold text-xl">Language</h2>
-            <p>Select the language in which the questions will be sent:</p>
-            <p>This dashboard will stay in English</p>
-            <Select onChange={e => setLang(e.value)} value={langOpts.find(el=> el.value=== lang)} options={langOpts} />
-          </div>
-          <div className="card-style flex flex-col justify-between w-80">
-            <div>
-            <h2 className="font-bold text-xl">Question Type</h2>
-            <p>Select the category of the questions to be shown</p>
-            </div>
-            <Select onChange={e => setCat(e.value)} value={catOpts.find(el=> el.value=== cat)} options={catOpts} />
-          </div>
-          <div className="card-style flex flex-col justify-between w-80">
-            <div>
-            <h2 className="font-bold text-xl">Weekday to ask</h2>
-            <p>Select the day of the week to send the questions</p>
-            <Select onChange={e => setWeekday(e.value)} value={weekdayOpts.find(el=> el.value=== weekday)} options={weekdayOpts} />
-            </div>
-          </div>
-          <div className="card-style flex flex-col justify-between w-80">
-            <div>
-            <h2 className="font-bold text-xl">Hour to ask</h2>
-            <p>Select the hour of the day to send the questions</p>
-            <p>This will happen on {weekdayOpts.find(el=> el.value=== weekday)}</p>
-            <Select onChange={e => setHour(e.value)} value={hourOpts.find(el=> el.value=== hour)} options={hourOpts} />
-            </div>
-          </div>
-        </div>
-        <div className="flex hover:bg-gray-50 border-gray-200 border-t-2 w-full mt-2">
-          <div className="rounded mx-2" style={{width: "3em", height: "3em"}}>
-          <img src="/wmslack.png" />
-          </div>
-          <div>
-            <div className="flex">
 
-          <p className="font-bold text-lg">Watermelon</p>
-          <div className="flex-col flex justify-center">
-
-          <p className="bg-gray-200 rounded-sm text-xs ml-2 px-1 text-gray-500 leading-1">APP</p>
-          </div>
-            </div>
-          <p className="font-semibold">{questions[exampleQuestion].question}</p>
-          <div className="flex justify-start my-1">
-          <p className="border border-gray-200 rounded px-2 py-1 mr-2 hover:bg-gray-100">{questions[exampleQuestion].ansA}</p>
-          <p className="border border-gray-200 rounded px-2 py-1 hover:bg-gray-100">{questions[exampleQuestion].ansB}</p>
-          </div>
-          <p>{questions[exampleQuestion].icebreaker}</p>
-          </div>
-        </div>
-        <div className="my-2 flex justify-end w-full">
-         <button onClick={(e)=> saveSettings()} className="text-white font-semibold bg-green-400 rounded shadow-sm py-2 px-3">
-           Finish
-         </button>
-       </div>
-      </div>
-      </PagePadder>
     </>
   )
 }
