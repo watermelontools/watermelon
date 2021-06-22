5d9800b07e2b474508f2f26dfc17a359849c7789
estebandalel-p44 <esteban.dalel@project44.com>
2021-06-21 21:04:42 -0500

    Send message on response

--- pages/api/slack/interaction.js
+++ pages/api/slack/interaction.js
@@ -1,5 +1,16 @@
 export default function handler(req, res) {
     let {body} = req
     console.log(body)
+    fetch(body.response_url,{
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify(
+            { 
+                text: "Thanks for your request, we'll process it and get back to you."
+        }
+        )
+    })
     res.status(200).json({status: "ok"})
 }
