25b63a0887903d479c400eaa9d26e88aeec9c586
estebandalel-p44 <esteban.dalel@project44.com>
2021-06-21 22:59:51 -0500

    log only response url

--- pages/api/slack/interaction.js
+++ pages/api/slack/interaction.js
@@ -1,17 +1,17 @@
 export default async function handler(req, res) {
     let {payload} = req.body
     let slackResponse= await JSON.parse(payload)
-    Object.keys(slackResponse).map(key=> console.log(key))
-    fetch(slackResponse.response_url,{
+    console.log(slackResponse.response_url)
+    fetch(slackResponse.response_url,
+        {
         method: "POST",
         headers: {
             'Content-Type': 'application/json'
           },
         body: JSON.stringify(
-            { 
-                text: "Thanks for your request, we'll process it and get back to you."
-        }
-        )
+        { 
+            text: "Thanks for your request, we'll process it and get back to you."
+        })
     })
     .then(response => response.json())
   .then(data => console.log(data))
