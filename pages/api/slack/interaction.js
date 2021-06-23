fa47a9e89c793eacf1f0cabfb2571f5f3aadc270
estebandalel-p44 <esteban.dalel@project44.com>
2021-06-22 20:44:42 -0500

    Log other params in payload

--- pages/api/slack/interaction.js
+++ pages/api/slack/interaction.js
@@ -1,21 +1,21 @@
 export default async function handler(req, res) {
-    let {payload} = req.body
-    let slackResponse= await JSON.parse(payload)
-    console.log(slackResponse.response_url)
-    await fetch(slackResponse.response_url,
-        {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-          },
-        body: JSON.stringify(
-        { 
-            text: "Thanks for your request, we'll process it and get back to you."
-        })
+  let { payload } = req.body;
+  let slackResponse = await JSON.parse(payload);
+  console.log("msg", slackResponse.message);
+  console.log("acts", slackResponse.actions);
+  await fetch(slackResponse.response_url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      text: "Watermelon question answered.",
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      res.status(200).json({ status: "ok" });
+      console.log(data);
     })
-    .then(response => response.json())
-  .then(data => {
-      res.status(200).json({status: "ok"})
-      console.log(data)})
-  .catch(error=> console.error(error))
+    .catch((error) => console.error(error));
 }
