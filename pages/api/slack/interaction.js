b413ef839624e7d57090a3a5d69ae7c33d569582
estebandalel-p44 <esteban.dalel@project44.com>
2021-06-22 21:31:27 -0500

    Save responses on click

--- pages/api/slack/interaction.js
+++ pages/api/slack/interaction.js
@@ -1,8 +1,29 @@
 export default async function handler(req, res) {
   let { payload } = req.body;
   let slackResponse = await JSON.parse(payload);
-  console.log("msg", slackResponse.message);
-  console.log("acts", slackResponse.actions);
+  async function savePickedBy({ teamId, questionName, answerTitle, userId }) {
+    let testWeeklyQuestionsRef = db
+      .collection("teams")
+      .doc(teamId)
+      .collection("weekly_questions")
+      .doc(questionName)
+      .collection(answerTitle)
+      .doc("picked_by");
+    const res = await testWeeklyQuestionsRef.update({
+      picked_by: admin.firestore.FieldValue.arrayUnion(userId),
+    });
+  }
+  savePickedBy({
+    teamId: slackResponse.message.team,
+    questionName:
+      slackResponse.message.blocks[
+        slackResponse.message.blocks.findIndex(
+          (block) => block.block_id == slackResponse.actions[0].block_id
+        ) - 1
+      ].elements[0].text,
+    answerTitle: slackResponse.actions[0].value,
+    userId: slackResponse.user.id,
+  });
   await fetch(slackResponse.response_url, {
     method: "POST",
     headers: {
