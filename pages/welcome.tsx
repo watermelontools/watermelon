0ba286274e1cf526ed54fb2aebc93c31ae30a961
estebandalel-p44 <esteban.dalel@project44.com>
2021-07-07 22:10:08 -0500

    Remove gifs

--- pages/welcome.tsx
+++ pages/welcome.tsx
@@ -15,31 +15,10 @@ const Welcome = ({ firebaseApp, token }) => {
         <div className="rounded shadow p-4">
           <p>✅You are ready to go!</p>
           <br />
-          <h3>In any Slack channel:</h3>
-          <ol>
-            <li>
-              Type <strong>/watermelon-ask</strong> to send questions
-              <img src="/watermelon-ask.gif" />
-            </li>
-            <br />
-            <li>
-              Have people answer them (you do it too, don’t miss the fun!)
-                <img src="/watermelon-respond.gif" />
-            </li>
-            <br />
-            <li>
-              Type <strong>/watermelon-create</strong> and let the fun begin!
-              <img src="/watermelon-create.gif" />
-            </li>
-          </ol>
+          <p>We'll post the questions soon, stay tuned!</p>
+
           <br />
-          <div className="my-2">
-            <a
-              className="py-2 px-4 text-base rounded shadow-sm text-white bg-green-400"
-              href={`slack://app?team=${loginToken?.team?.id}&id=${loginToken?.app_id}`}>
-              Start using Watermelon🍉
-          </a>
-          </div>
+          <p>Rememeber you can change your <Link href="/settings"><a>settings</a></Link></p>
         </div>
       </PagePadder>
     </>
