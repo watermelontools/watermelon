0949c0c37c00c280a53798fa6595479ee896cacb
estebandalel-p44 <esteban.dalel@project44.com>
2021-07-08 19:51:23 -0500

    Extend instructions

--- pages/welcome.tsx
+++ pages/welcome.tsx
@@ -16,6 +16,8 @@ const Welcome = ({ firebaseApp, token }) => {
           <p>✅You are ready to go!</p>
           <br />
           <p>We'll post the questions soon, stay tuned!</p>
+          <p>Remember that your team has 24 hours to answer the questions, so we suggest pinning the message.</p>
+          <p>When the time has expired, the groups will be created and we'll send the icebreakers</p>
 
           <br />
           <p>Rememeber you can change your <Link href="/settings"><a>settings</a></Link></p>
