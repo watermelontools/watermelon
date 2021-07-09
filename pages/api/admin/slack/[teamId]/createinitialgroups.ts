200ea78d9b3db4648cff853611b55b1ce62af27b
estebandalel-p44 <esteban.dalel@project44.com>
2021-07-08 22:42:32 -0500

    Remove logging

--- pages/api/admin/slack/[teamId]/createinitialgroups.ts
+++ pages/api/admin/slack/[teamId]/createinitialgroups.ts
@@ -3,12 +3,12 @@ import logger from "../../../../../logger/logger";
 import admin from "../../../../../utils/firebase/backend";
 
 let db = admin.firestore();
-async function getTeam(teamId:string) {
+async function getTeam(teamId: string) {
   let fbRes = await db.collection("teams").doc(teamId).get();
-  return fbRes
+  return fbRes;
 }
 
- const createInitialGroups = ({ token }) => {
+const createInitialGroups = ({ token }) => {
   const createPromiseArray = [];
   for (let index = 0; index < 8; index++) {
     createPromiseArray.push(
@@ -23,29 +23,35 @@ async function getTeam(teamId:string) {
   }
   return createPromiseArray;
 };
-export const createAndSave = async ({access_token,teamId}:{access_token:string, teamId: string})=>{   
-  console.log("func called")
-     let createPromiseArray = createInitialGroups({
-  token: access_token,
-});
-let finishedProms = await Promise.all(createPromiseArray);
-console.log(finishedProms)
-await db.collection("teams")
-.doc(teamId)
-.set(
-  {
-    room_ids:  finishedProms
-  },
-  { merge: true }
-)
-.then(async function () {
-  logger.info({ message: "rooms-created", data:  finishedProms })
-})
-.catch(function (error) {
-  console.error("Error adding document: ", error);
-})
-return finishedProms
-}
+export const createAndSave = async ({
+  access_token,
+  teamId,
+}: {
+  access_token: string;
+  teamId: string;
+}) => {
+  console.log("func called");
+  let createPromiseArray = createInitialGroups({
+    token: access_token,
+  });
+  let finishedProms = await Promise.all(createPromiseArray);
+  await db
+    .collection("teams")
+    .doc(teamId)
+    .set(
+      {
+        room_ids: finishedProms,
+      },
+      { merge: true }
+    )
+    .then(async function () {
+      logger.info({ message: "rooms-created", data: finishedProms });
+    })
+    .catch(function (error) {
+      console.error("Error adding document: ", error);
+    });
+  return finishedProms;
+};
 export default async function handler(req, res) {
   const {
     query: { teamId },
@@ -56,12 +62,11 @@ export default async function handler(req, res) {
     });
     res.status(400).json({ status: "error", error: "no team id" });
   }
-  let fbRes = await getTeam(teamId)
+  let fbRes = await getTeam(teamId);
   if (fbRes.exists) {
     let data = fbRes.data();
     if (data?.add_to_slack_token?.access_token) {
-
-       createAndSave (data.token.access_token)
+      createAndSave(data.token.access_token)
         .then((settled) => {
           for (let index = 0; index < settled.length; index++) {
             const element = settled[index];
@@ -76,9 +81,8 @@ export default async function handler(req, res) {
           logger.error(error);
           res.status(500).send(error);
         });
-    }
-    else{
-      logger.error({message: "no data", data})
+    } else {
+      logger.error({ message: "no data", data });
     }
   }
 }
