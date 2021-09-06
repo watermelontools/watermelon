945b4a4f1cdf209228edff9f04a5874507a53a26
estebandalel-p44 <esteban.dalel@project44.com>
2021-09-05 20:19:34 -0500

    Upgrade savesettings

--- pages/api/saveSettings.ts
+++ pages/api/saveSettings.ts
@@ -1,66 +1,46 @@
 import admin from "../../utils/firebase/backend";
 import logger from "../../logger/logger";
-export default function handler(req, res) {
+import { nextWeekday } from "../../utils/cronUtils";
+import { createSettings } from "../../utils/airtable/backend";
+export default async function handler(req, res) {
   let db = admin.firestore();
   let body = JSON.parse(req.body);
   let { lang, cat, signInToken, weekday, hour, isWizard, timezone } = body;
   const baseurl = `https://${process.env.VERCEL_URL}/api/admin/`;
-  let nextWeekday = (weekday) => {
-    switch (weekday) {
-      case "MON":
-        return "TUE";
-      case "TUE":
-        return "WED";
-      case "WED":
-        return "THU";
-      case "THU":
-        return "FRI";
-      case "FRI":
-        return "SAT";
-      case "SAT":
-        return "MON";
-      default:
-        break;
-    }
-  };
-  db.collection("teams")
-    .doc(`${signInToken.team.id}`)
-    .set(
-      { settings: { language: lang, category: cat, weekday, hour, timezone } },
-      { merge: true }
-    )
-    .then(function () {
-      if (isWizard) {
-        let urls = [
-          { route: "sendquestions", hour, weekday },
-          { route: "clearooms", hour: hour - 1, weekday: nextWeekday(weekday) },
-          { route: "addtorooms", hour: hour, weekday: nextWeekday(weekday) },
-        ];
-        urls.forEach(async (element) => {
-          await fetch(
-            `https://www.easycron.com/rest/add?token=${
-              process.env.EASYCRON_API_KEY
-            }
-          &cron_expression=0 ${element.hour} * * ${element.weekday} *
-          &url=${baseurl + element.route}
-          &cron_job_name=${signInToken.team.id}
-          ${
-            timezone
-              ? `&timezone_from=2
-                &timezone=${timezone}`
-              : ""
-          }`
-          )
-            .then((response) => response.json())
-            .then((data) => {
-              logger.info({ message: "cron-created", data });
-            });
+  await createSettings({
+    settings: {
+      Language: lang,
+      Day: weekday,
+      Hour: hour,
+      Timezone: timezone,
+      Category: cat,
+    },
+    workspaceId: signInToken.team.id,
+  });
+  if (isWizard) {
+    let urls = [
+      { route: "sendquestions", hour, weekday },
+      { route: "clearooms", hour: hour - 1, weekday: nextWeekday(weekday) },
+      { route: "addtorooms", hour: hour, weekday: nextWeekday(weekday) },
+    ];
+    urls.forEach(async (element) => {
+      await fetch(
+        `https://www.easycron.com/rest/add?token=${process.env.EASYCRON_API_KEY}
+      &cron_expression=0 ${element.hour} * * ${element.weekday} *
+      &url=${baseurl + element.route}
+      &cron_job_name=${signInToken.team.id}
+      ${
+        timezone
+          ? `&timezone_from=2
+            &timezone=${timezone}`
+          : ""
+      }`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          logger.info({ message: "cron-created", data });
         });
-        res.status(200).json(JSON.stringify({ ok: "ok" }));
-      }
-    })
-    .catch(function (error) {
-      console.error("Error writing: ", error);
-      res.status(500).json(JSON.stringify({ ok: false }));
     });
+  }
+  res.status(200).json(JSON.stringify({ ok: "ok" }));
 }
