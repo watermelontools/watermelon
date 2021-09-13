1eaed4f063d5d11ccad2a4d84a514ca7b8582afb
estebandalel-p44 <esteban.dalel@project44.com>
2021-09-13 16:20:11 -0500

    respond 200 to slack

--- pages/api/slack/interaction.ts
+++ pages/api/slack/interaction.ts
@@ -8,6 +8,7 @@ export default async function handler(req, res) {
   let { payload } = req.body;
   let slackResponse = await JSON.parse(payload);
 console.log(slackResponse)
+  res.status(200).json({ status: "ok" });
   const ephimeralMessageData = {
     attachments:
       "[{'text': 'This response is anonymous.', 'color': '#75b855'}]",
@@ -58,5 +59,5 @@ console.log(slackResponse)
       logger.info({ message: "question-answered", data });
     })
     .catch((error) => console.error(error));
-  res.status(200).json({ slackResponse });
+  res.status(200).json({ status: "ok" });
 }
