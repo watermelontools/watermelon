5a399ed5b62ae4b5d96f7111df3f919383e69e8c
EstebanDalelR <EstebanDalelR@users.noreply.github.com>
2021-09-26 21:29:24 +0000

    Prettified

--- pages/api/admin/slack/[teamId]/addtorooms.ts
+++ pages/api/admin/slack/[teamId]/addtorooms.ts
@@ -1,11 +1,21 @@
 import logger from "../../../../../logger/logger";
-import { findWorkspaceRecord, getLastWeekAnswerers, getRooms } from "../../../../../utils/airtable/backend";
-import { inviteToRoom, kickFromRoom, listRoomMembers, sendDM, sendIcebreaker } from "../../../../../utils/slack/backend";
+import {
+  findWorkspaceRecord,
+  getLastWeekAnswerers,
+  getRooms,
+} from "../../../../../utils/airtable/backend";
+import {
+  inviteToRoom,
+  kickFromRoom,
+  listRoomMembers,
+  sendDM,
+  sendIcebreaker,
+} from "../../../../../utils/slack/backend";
 const axios = require("axios").default;
 
 async function getInstallationToken({ workspaceId }) {
   let workspaceRecord = await findWorkspaceRecord({ workspaceId });
-  return workspaceRecord.fields.AccessToken
+  return workspaceRecord.fields.AccessToken;
 }
 export default async function handler(req, res) {
   const {
@@ -17,68 +27,88 @@ export default async function handler(req, res) {
     });
     res.status(400).json({ status: "error", error: "no team id" });
   }
-  let responses = await getLastWeekAnswerers({ workspaceId: teamId })
+  let responses = await getLastWeekAnswerers({ workspaceId: teamId });
   if (responses.length > 0) {
     let accessToken = await getInstallationToken({ workspaceId: teamId });
-    let roomIds = await getRooms({ workspaceId: teamId })
-    let questions = {}
-    responses.map(response => {
+    let roomIds = await getRooms({ workspaceId: teamId });
+    let questions = {};
+    responses.map((response) => {
       if (questions[response.fields.Question]) {
-        let foundAnswer = questions[response.fields.Question].answers.findIndex(q =>
-          q.answerRecord.includes(response.fields.Answer[0])
-        )
+        let foundAnswer = questions[response.fields.Question].answers.findIndex(
+          (q) => q.answerRecord.includes(response.fields.Answer[0])
+        );
         if (foundAnswer > -1) {
           questions[response.fields.Question].answers[foundAnswer].users.push(
-            ...response.fields.SlackId)
-          questions[response.fields.Question].answers[foundAnswer].icebreaker = response.fields.IcebreakerToSend
-        }
-        else {
+            ...response.fields.SlackId
+          );
+          questions[response.fields.Question].answers[foundAnswer].icebreaker =
+            response.fields.IcebreakerToSend;
+        } else {
           questions[response.fields.Question].answers.push({
             answerRecord: response.fields.Answer,
             icebreaker: response.fields.IcebreakerToSend,
             answerText: response.fields.AnswerText,
-            users: response.fields.SlackId
-          }
-          )
+            users: response.fields.SlackId,
+          });
         }
-      }
-      else {
+      } else {
         questions[response.fields.Question] = {
           questionText: response.fields.QuestionText,
-          answers: [{
-            answerRecord: response.fields.Answer,
-            answerText: response.fields.AnswerText,
-            users: response.fields.SlackId
-          }]
-        }
+          answers: [
+            {
+              answerRecord: response.fields.Answer,
+              answerText: response.fields.AnswerText,
+              users: response.fields.SlackId,
+            },
+          ],
+        };
       }
-    })
+    });
     for (let index = 0; index < roomIds.length; index++) {
       const element = roomIds[index];
-      let roomMembers = await listRoomMembers({ accessToken, channel: element.fields.RoomId })
+      let roomMembers = await listRoomMembers({
+        accessToken,
+        channel: element.fields.RoomId,
+      });
       for (let j = 0; j < roomMembers.members.length; j++) {
         const member = roomMembers.members[j];
-        await kickFromRoom({ accessToken, channel: element.fields.RoomId, user: member })
-        await sendDM({ accessToken, channel: member, text: "You have been removed from last's week :watermelon: room, we're starting a new round." })
+        await kickFromRoom({
+          accessToken,
+          channel: element.fields.RoomId,
+          user: member,
+        });
+        await sendDM({
+          accessToken,
+          channel: member,
+          text: "You have been removed from last's week :watermelon: room, we're starting a new round.",
+        });
       }
     }
-    let questionNames = Object.keys(questions)
+    let questionNames = Object.keys(questions);
     for (let index = 0; index < questionNames.length; index++) {
-      let element = questions[questionNames[index]]
+      let element = questions[questionNames[index]];
       for (let j = 0; j < element.answers.length; j++) {
         const answer = element.answers[j];
-        let room = roomIds.shift()
+        let room = roomIds.shift();
         let icebreakerData = {
           text: `
-          Welcome to this :watermelon: room, you answered the question "${element.questionText[0]}".
-          ${answer.icebreaker[0].replace("${answer}", `*${answer.answerText[0]}*`)}
-        `}
-        let watermelonRoomData = { channel: room.fields.RoomId, users: answer.users }
-        await inviteToRoom({ accessToken, watermelonRoomData })
-        await sendIcebreaker({ accessToken, icebreakerData })
+          Welcome to this :watermelon: room, you answered the question "${
+            element.questionText[0]
+          }".
+          ${answer.icebreaker[0].replace(
+            "${answer}",
+            `*${answer.answerText[0]}*`
+          )}
+        `,
+        };
+        let watermelonRoomData = {
+          channel: room.fields.RoomId,
+          users: answer.users,
+        };
+        await inviteToRoom({ accessToken, watermelonRoomData });
+        await sendIcebreaker({ accessToken, icebreakerData });
       }
     }
     res.status(200).send({ ok: "ok" });
-  }
-  else res.status(500).send({ error: "No responses last week" })
+  } else res.status(500).send({ error: "No responses last week" });
 }
