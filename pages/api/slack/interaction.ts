a41e498fd3f86275cb0473d929ba7466ffe765c9
Esteban Dalel R <e.dalel10@uniandes.edu.co>
2021-09-29 21:25:35 -0500

    Debug logging

--- pages/api/slack/interaction.ts
+++ pages/api/slack/interaction.ts
@@ -7,8 +7,9 @@ import {
 export default async function handler(req, res) {
   let { payload } = req.body;
   let slackResponse = await JSON.parse(payload);
+  console.log(slackResponse)
 
-  logger.info({message: "NEW_INTERACTION", payload})
+  logger.info({message: "NEW_INTERACTION", slackResponse})
   
   const ephimeralMessageData = {
     attachments:
