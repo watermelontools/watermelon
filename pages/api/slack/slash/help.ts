199ad42aabcadda1de29ba6fa12c30957f1730fe
Esteban Dalel R <e.dalel10@uniandes.edu.co>
2021-09-29 22:00:36 -0500

    Update help.ts

--- pages/api/slack/slash/help.ts
+++ pages/api/slack/slash/help.ts
@@ -1,7 +1,7 @@
 import logger from "../../../../logger/logger";
 
 export default async function handler(req, res) {
-    logger.info({source: "SLASH_COMMAND-HELP", req.body})
+    logger.info({source: "SLASH_COMMAND-HELP", params:req.body})
     res.status(200).json({
 	"blocks": [
 		{
