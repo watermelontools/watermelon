ad7b0bc496de3c5b5115d5f9e8f6fe7ba657f828
Esteban Dalel R <e.dalel10@uniandes.edu.co>
2021-09-29 21:42:57 -0500

    Update help.ts

--- pages/api/slack/slash/help.ts
+++ pages/api/slack/slash/help.ts
@@ -1,12 +1,7 @@
 import logger from "../../../../logger/logger";
 
 export default async function handler(req, res) {
-    let { payload } = req.body;
-    let slackResponse = await JSON.parse(payload);
-    console.log(slackResponse)
 
         console.log(req.body)
-logger.info({ message: "NEW_INTERACTION", slackResponse })
-
     res.status(200).json({ status: "ok" });
 }
