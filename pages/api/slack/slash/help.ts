3c5f7e982cfe45906e60a5a162d0791c89d14b9c
Esteban Dalel R <e.dalel10@uniandes.edu.co>
2021-09-29 21:57:14 -0500

    Update help.ts

--- pages/api/slack/slash/help.ts
+++ pages/api/slack/slash/help.ts
@@ -1,7 +1,24 @@
 import logger from "../../../../logger/logger";
 
 export default async function handler(req, res) {
-
-        console.log(req.body)
-    res.status(200).json({ status: "ok" });
+    logger.info({source: "SLASH_COMMAND-HELP", req.body})
+    res.status(200).json({
+	"blocks": [
+		{
+			"type": "header",
+			"text": {
+				"type": "plain_text",
+				"text": "Welcome to Watermelon :watermelon:!",
+				"emoji": true
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "Your questions will be sent weekly and 24 hours later groups will be created. For help email us at <mailto:support@watermelon.tools|support@watermelon.tools>"
+			}
+		}
+	]
+});
 }
