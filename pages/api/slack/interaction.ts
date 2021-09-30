efc9db76aba57bc410815f8ce2b388e411a3a2c7
EstebanDalelR <EstebanDalelR@users.noreply.github.com>
2021-09-30 02:35:42 +0000

    Prettified

--- pages/api/slack/interaction.ts
+++ pages/api/slack/interaction.ts
@@ -8,9 +8,9 @@ export default async function handler(req, res) {
   let { payload } = req.body;
   let slackResponse = await JSON.parse(payload);
 
-  console.log(slackResponse)
+  console.log(slackResponse);
 
-  logger.info({message: "NEW_INTERACTION", slackResponse})
+  logger.info({ message: "NEW_INTERACTION", slackResponse });
 
   const ephimeralMessageData = {
     attachments:
