2cfe810139fb47ae4a449533af9080aa0eca086a
estebandalel-p44 <esteban.dalel@project44.com>
2021-10-13 23:25:28 -0500

    Add small team functionality

--- pages/api/admin/slack/[teamId]/sendquestions.ts
+++ pages/api/admin/slack/[teamId]/sendquestions.ts
@@ -18,8 +18,9 @@ export default async function handler(req, res) {
   let workspaceRecord = await findWorkspaceRecord({ workspaceId: teamId });
   let questions = await getAllUnusedQuestions({ workspaceId: teamId });
   let questionsToSend = [];
-
-  if (questions.length >= 1) {
+  let questionNumber = 1
+  if (workspaceRecord.fields.IsSmallTeam > 0) questionNumber = 2
+  if (questions.length >= questionNumber) {
     while (questionsToSend.length < 1) {
       let item = questions[Math.floor(Math.random() * questions.length)];
       if (!questionsToSend.includes(item)) questionsToSend.push(item);
