b2cf5fe4beaa62ff0638ba6d8a12bfdc5cfea3a7
Esteban Dalel R <estebandalelr@gmail.com>
2022-04-01 15:37:30 -0500

    Chore/fix env files (#56)
    * Remove env file
    
    * Create env local template
    
    * Update .gitignore
    
    * Added owner and repo to analytics
    
    * Prettified
    
    Co-authored-by: EstebanDalelR <EstebanDalelR@users.noreply.github.com>

--- pages/api/analytics/github/search.ts
+++ pages/api/analytics/github/search.ts
@@ -6,7 +6,11 @@ Airtable.configure({
 const base = require("airtable").base("appDpKitgxjDIUwZ3");
 
 export default async function handler(req, res) {
-  const searchType = req.body.searchType;
-  let createdRecord = await base("Searches").create({ Type: searchType });
+  const { searchType, owner, repo } = req.body;
+  let createdRecord = await base("Searches").create({
+    Type: searchType,
+    Owner: owner,
+    Repo: repo,
+  });
   res.status(200).json(createdRecord.fields);
 }
