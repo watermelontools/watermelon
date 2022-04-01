c04d23a78f600d5b9bf8041e7605b2fd21c7117e
estebandalelr <estebandalelr@gmail.com>
2022-04-01 15:50:13 -0600

    Added localuser to track WAUs

--- pages/api/analytics/github/search.ts
+++ pages/api/analytics/github/search.ts
@@ -6,11 +6,12 @@ Airtable.configure({
 const base = require("airtable").base("appDpKitgxjDIUwZ3");
 
 export default async function handler(req, res) {
-  const { searchType, owner, repo } = req.body;
+  const { searchType, owner, repo, localUser } = req.body;
   let createdRecord = await base("Searches").create({
     Type: searchType,
     Owner: owner,
     Repo: repo,
+    Username: localUser
   });
   res.status(200).json(createdRecord.fields);
 }
