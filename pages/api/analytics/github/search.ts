62f3b0d995610042be26ee0fe611688293be5941
Esteban Dalel R <estebandalelr@gmail.com>
2022-04-07 12:27:21 -0500

    Add email to analytics (#58)
    * Add email to analytics
    
    * Prettified
    
    Co-authored-by: EstebanDalelR <EstebanDalelR@users.noreply.github.com>

--- pages/api/analytics/github/search.ts
+++ pages/api/analytics/github/search.ts
@@ -6,12 +6,13 @@ Airtable.configure({
 const base = require("airtable").base("appDpKitgxjDIUwZ3");
 
 export default async function handler(req, res) {
-  const { searchType, owner, repo, localUser } = req.body;
+  const { searchType, owner, repo, localUser, userEmail } = req.body;
   let createdRecord = await base("Searches").create({
     Type: searchType,
     Owner: owner,
     Repo: repo,
     Username: localUser,
+    Email: userEmail,
   });
   res.status(200).json(createdRecord.fields);
 }
