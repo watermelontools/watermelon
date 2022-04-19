3089275e0d5365b6ae22db451d7634eb8b5ebcd8
estebandalelr <estebandalelr@gmail.com>
2022-04-19 09:35:40 -0500

    Add analytics for clicking the button

---
+++ pages/api/analytics/slack/slackhelp.ts
@@ -0,0 +1,13 @@
+import Airtable from "airtable";
+Airtable.configure({
+  endpointUrl: "https://api.airtable.com",
+  apiKey: process.env.AIRTABLE_API_KEY,
+});
+const base = require("airtable").base("appDpKitgxjDIUwZ3");
+
+export default async function handler(req, res) {
+  let createdRecord = await base("Slackhelp").create({
+    Click:"click",
+  });
+  res.status(200).json(createdRecord.fields);
+}
