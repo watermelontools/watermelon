bce4d6e2e4d8d9bd242507e84def0109e1f27ce2
baristaGeek <baristaGeek@users.noreply.github.com>
2022-05-03 00:36:29 +0000

    Prettified

--- pages/api/gpt3/explainCode.ts
+++ pages/api/gpt3/explainCode.ts
@@ -1,10 +1,9 @@
 import Airtable from "airtable";
-import PostHog from 'posthog-node'
+import PostHog from "posthog-node";
 
-const client = new PostHog(
-    process.env.POSTHOG_API_KEY,
-    { host: 'https://app.posthog.com' }
-)
+const client = new PostHog(process.env.POSTHOG_API_KEY, {
+  host: "https://app.posthog.com",
+});
 
 Airtable.configure({
   endpointUrl: "https://api.airtable.com",
@@ -15,33 +14,31 @@ const base = require("airtable").base("appDpKitgxjDIUwZ3");
 const { Configuration, OpenAIApi } = require("openai");
 
 export default async function handler(req, res) {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
 
-    const configuration = new Configuration({
-        apiKey: process.env.OPENAI_API_KEY,
-      });
-      
-    const openai = new OpenAIApi(configuration);
+  const openai = new OpenAIApi(configuration);
 
-    const response = await openai.createCompletion("text-davinci-002", {
-        prompt: "Explain code: "+req.body.codeBlock,
-        temperature: 0.7,
-        max_tokens: 64,
-        top_p: 1.0,
-    });
-      
-    // Analytics
-    const { owner, repo, localUser, userEmail } = req.body;
-    client.capture({
-      distinctId: 'run-watermelon',
-      event: `${owner} search`
-    })
-    let createdRecord = await base("Searches").create({
-      Type: "Explain code",
-      Owner: owner,
-      Repo: repo,
-      Username: localUser,
-      Email: userEmail,
-    });
-    res.status(200).json(response.data.choices[0].text);
-  }
-  
+  const response = await openai.createCompletion("text-davinci-002", {
+    prompt: "Explain code: " + req.body.codeBlock,
+    temperature: 0.7,
+    max_tokens: 64,
+    top_p: 1.0,
+  });
+
+  // Analytics
+  const { owner, repo, localUser, userEmail } = req.body;
+  client.capture({
+    distinctId: "run-watermelon",
+    event: `${owner} search`,
+  });
+  let createdRecord = await base("Searches").create({
+    Type: "Explain code",
+    Owner: owner,
+    Repo: repo,
+    Username: localUser,
+    Email: userEmail,
+  });
+  res.status(200).json(response.data.choices[0].text);
+}
