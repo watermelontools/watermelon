438ad3c54f1f105b8c35ee716afc56ec7845cf62
Esteban Dalel R <estebandalelr@gmail.com>
2022-07-25 18:08:47 -0500

    Added fetch for jira oauth

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,3 +3,39 @@ function HomePage() {
 }
 
 export default HomePage;
+export async function getServerSideProps(context) {
+  let f;
+  if (context.query.code)
+  console.log(context.query.code);
+    f = await fetch(`https://auth.atlassian.com/oauth/token`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        grant_type: "authorization_code",
+        code: context.query.code,
+        redirect_uri: "https://app.watermelon.tools",
+        client_id: process.env.NEXT_PUBLIC_JIRA_CLIENT_ID,
+        client_secret: process.env.NEXT_PUBLIC_JIRA_CLIENT_SECRET,
+      }),
+    });
+  else
+    return {
+      props: {
+        error: "no code",
+      },
+    };
+  const json = await f.json();
+  if (json.error)
+    return {
+      props: {
+        error: json.error,
+      },
+    };
+  return {
+    props: {
+      accessToken: json.access_token,
+    },
+  };
+}
