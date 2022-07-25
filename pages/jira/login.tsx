734119473d0edaea7526da8aef4c8d90bebc96fd
Esteban Dalel R <estebandalelr@gmail.com>
2022-07-25 17:44:31 -0500

    Fix double closing tag

---
+++ pages/jira/login.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default function LoginPage() {
+  return (
+    <Link
+      href={`https://auth.atlassian.com/authorize?
+    audience=api.atlassian.com&
+    client_id=VUngRAClu8ZE56vxXCFBocTxCTLEUQTT&
+    scope=read:jira-user%read:jira-work&
+    redirect_uri=https://app.watermelon.tools&
+    state=something&
+    response_type=code&
+    prompt=consent
+  `}
+    >
+      Login
+    </Link>
+  );
+}
