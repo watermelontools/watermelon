044b01c5aba5c9d74078da787fdd4017348d13c2
Esteban Dalel R <estebandalelr@gmail.com>
2022-07-25 17:56:32 -0500

    Added write scope

--- pages/jira/login.tsx
+++ pages/jira/login.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function LoginPage() {
   return (
     <Link
-      href={`https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=VUngRAClu8ZE56vxXCFBocTxCTLEUQTT&scope=read:jira-user%20read:jira-work&redirect_uri=https://app.watermelon.tools&state=something&response_type=code&prompt=consent`}
+      href={`https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=VUngRAClu8ZE56vxXCFBocTxCTLEUQTT&scope=read:jira-user%20read:jira-work%20write:jira-work&redirect_uri=https://app.watermelon.tools&state=something&response_type=code&prompt=consent`}
     >
       Login
     </Link>
