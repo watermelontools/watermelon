437602c7ae25970cf882ead73eb78903215c81c9
Esteban Dalel R <estebandalelr@gmail.com>
2022-07-26 15:34:36 -0500

    Create _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <link
+            rel="shortcut icon"
+            href="/favicon/favicon.ico"
+            type="image/x-icon"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
