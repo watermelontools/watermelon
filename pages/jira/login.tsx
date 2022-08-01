5173ec21906432d096f73c5fe94b8d75551e7243
estebandalelr <estebandalelr@gmail.com>
2022-08-01 16:06:34 -0500

    pass session id as state

--- pages/jira/login.tsx
+++ pages/jira/login.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
+import { supabase } from "../../utils/supabase";
 
 export default function LoginPage() {
   return (
     <Link
-      href={`https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=VUngRAClu8ZE56vxXCFBocTxCTLEUQTT&scope=read:jira-user%20read:jira-work%20write:jira-work%20offline_access&redirect_uri=https://app.watermelon.tools&state=something&response_type=code&prompt=consent`}
+      href={`https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=VUngRAClu8ZE56vxXCFBocTxCTLEUQTT&scope=read:jira-user%20read:jira-work%20write:jira-work%20offline_access&redirect_uri=https://app.watermelon.tools&state=${
+        supabase.auth.user().id
+      }&response_type=code&prompt=consent`}
     >
       Login
     </Link>
