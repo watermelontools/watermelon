4ff7287430f70fbf0f3e22e5e20e1cfc5b8b7402
estebandalelr <estebandalelr@gmail.com>
2022-08-01 13:20:13 -0500

    Use example for login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,27 @@
 import { supabase } from "../utils/supabase";
-
+import Auth from "./components/Auth";
+import Account from "./components/Account";
+import { useState, useEffect } from "react";
 function HomePage() {
-  return <div>Home</div>;
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    setSession(supabase.auth.session());
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  return (
+    <div className="container" style={{ padding: "50px 0 100px 0" }}>
+      {!session ? (
+        <Auth />
+      ) : (
+        <Account key={session.user.id} session={session} />
+      )}
+    </div>
+  );
 }
 
 export default HomePage;
