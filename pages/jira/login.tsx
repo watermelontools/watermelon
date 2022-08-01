d2fab291fe0c7d5730c3eb13166dafcb84ef4f3c
estebandalelr <estebandalelr@gmail.com>
2022-08-01 16:10:43 -0500

    make id optional

--- pages/jira/login.tsx
+++ pages/jira/login.tsx
@@ -5,7 +5,7 @@ export default function LoginPage() {
   return (
     <Link
       href={`https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=VUngRAClu8ZE56vxXCFBocTxCTLEUQTT&scope=read:jira-user%20read:jira-work%20write:jira-work%20offline_access&redirect_uri=https://app.watermelon.tools&state=${
-        supabase.auth.user().id
+        supabase.auth.user()?.id
       }&response_type=code&prompt=consent`}
     >
       Login
