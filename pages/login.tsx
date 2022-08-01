b3a03c298c3e9bda282b7474f4c0a6e32f83aa4e
estebandalelr <estebandalelr@gmail.com>
2022-08-01 12:00:18 -0500

    Add method

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,7 +2,7 @@ export default function Login() {
   return (
     <div>
       <h1>Login</h1>
-      <form action="/api/login">
+      <form action="/api/login" method="post">
         <label htmlFor="email">
           Email:
           <input type="email" name="email" required id="email" />
