c713b931c58987f9e6c4dc4c13697e14189cbb28
estebandalelr <estebandalelr@gmail.com>
2022-08-01 11:56:43 -0500

    Add login flow with supabase

---
+++ pages/login.tsx
@@ -0,0 +1,24 @@
+export default function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form action="/api/login">
+        <label htmlFor="email">
+          Email:
+          <input type="email" name="email" required id="email" />
+        </label>
+        <label htmlFor="password">
+          Password:
+          <input
+            type="password"
+            name="password"
+            required
+            id="password"
+            minLength={6}
+          />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
