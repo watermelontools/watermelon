8ebc2467d5889f9223aae2d8a36855f9af40637d
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-02 13:32:39 -0500

    Debug logging

--- pages/api/jira/getOrganization.ts
+++ pages/api/jira/getOrganization.ts
@@ -2,13 +2,15 @@ import { supabase } from "../../../utils/supabase";
 
 export default async function handler(req, res) {
   let { user } = req.body;
-  console.log(user);
-  if (!user) res.send({ error: "no user" });
+  console.log("u", user);
+  if (!user) {
+    return res.send({ error: "no user" });
+  }
   let { data, error, status } = await supabase
     .from("Jira")
     .select("organization")
     .eq("user", user);
   if (error) res.send(error);
-  console.log(data);
+  console.log("supadata", data);
   res.send(data[0]?.organization || { error: "no results" });
 }
