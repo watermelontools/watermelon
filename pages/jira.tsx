32fcd44626127cab9885da2f1faaecb5e4a5020a
estebandalelr <estebandalelr@gmail.com>
2022-08-02 14:55:14 -0500

    Add org image

--- pages/jira.tsx
+++ pages/jira.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { supabase } from "../utils/supabase";
 import { useRouter } from "next/router";
-export default function Jira({ organization }) {
+export default function Jira({ organization, url }) {
   const [timeToRedirect, setTimeToRedirect] = useState(5);
   const router = useRouter();
   useEffect(() => {
@@ -17,6 +17,7 @@ export default function Jira({ organization }) {
   return (
     <div>
       <h1>You have logged in with Jira to {organization}</h1>
+      <img src={url} alt="jira organization image" />
       <div>
         <p>You will be redirected in {timeToRedirect}...</p>
         <p>
@@ -87,6 +88,7 @@ export async function getServerSideProps(context) {
     return {
       props: {
         organization: orgInfoJson[0]?.name,
+        url: orgInfoJson[0]?.url,
       },
     };
   }
