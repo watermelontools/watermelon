d5fbe1da4838f97daf5cc199d78320fc8be4aa7d
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-02 13:06:13 -0500

    Create getOrganization.ts

---
+++ pages/api/jira/getOrganization.ts
@@ -0,0 +1,11 @@
+import { supabase } from "../../../utils/supabase";
+
+export default async function handler(req, res) {
+  let { user } = req.body;
+  let { data, error, status } = await supabase
+    .from("Jira")
+    .select("organization")
+    .eq("user", user);
+  if (error) res.send(error);
+  res.send(data[0]?.organization || { error: "no results" });
+}
