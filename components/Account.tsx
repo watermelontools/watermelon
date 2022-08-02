b86073872df91348237e0662ed506da99469185b
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-02 13:14:24 -0500

    Fetch jira org

--- components/Account.tsx
+++ components/Account.tsx
@@ -8,9 +8,10 @@ export default function Account({ session, jiraOrg }) {
   const [username, setUsername] = useState(null);
   const [website, setWebsite] = useState(null);
   const [avatar_url, setAvatarUrl] = useState(null);
-
+  const [DBJiraOrg, setDBJiraOrg] = useState(null);
   useEffect(() => {
     getProfile();
+    if (!jiraOrg) getJiraOrg();
   }, [session]);
   const [userId, setUserId] = useState(null);
   async function getProfile() {
@@ -40,7 +41,21 @@ export default function Account({ session, jiraOrg }) {
       setLoading(false);
     }
   }
-
+  async function getJiraOrg() {
+    try {
+      fetch("/api/jira/getOrganization")
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.error) {
+            setDBJiraOrg(null);
+          } else {
+            setDBJiraOrg(data);
+          }
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  }
   async function updateProfile({ username, website, avatar_url }) {
     try {
       setLoading(true);
@@ -112,13 +127,15 @@ export default function Account({ session, jiraOrg }) {
       <div>
         {jiraOrg ? (
           <p>logged in to {jiraOrg} with Jira</p>
+        ) : DBJiraOrg ? (
+          <p>logged in to {DBJiraOrg} with Jira</p>
         ) : (
           <Link
             href={`https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=VUngRAClu8ZE56vxXCFBocTxCTLEUQTT&scope=read:jira-user%20read:jira-work%20write:jira-work%20offline_access&redirect_uri=https://app.watermelon.tools&state=${
               userId ? userId : ""
             }&response_type=code&prompt=consent`}
           >
-            <a className="button block">login to Jira</a>
+            <a className="button block">Login to Jira</a>
           </Link>
         )}
       </div>
