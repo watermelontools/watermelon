d14ea6dd70e42e5e527373b2221c3a88f1ae4e7c
estebandalelr <estebandalelr@gmail.com>
2022-08-04 13:54:11 -0500

    fix returns with types

--- utils/db/jira/getOrganization.ts
+++ utils/db/jira/getOrganization.ts
@@ -1,6 +1,7 @@
+import { Organization } from "../../../types/jira/Organization";
 import { supabase } from "../../supabase";
 
-export default async function getJiraOrganization(user) {
+export default async function getJiraOrganization(user): Promise<Organization> {
   let { data, error, status } = await supabase
     .from("Jira")
     .select("organization")
@@ -8,5 +9,36 @@ export default async function getJiraOrganization(user) {
   if (error && status !== 406) {
     throw error;
   }
-  return { organization: data[0]?.organization } || { error: "no results" };
+  let organization: Organization = {
+    id: "",
+    name: "",
+    description: "",
+    url: "",
+    avatarUrl: "",
+    createdAt: "",
+    updatedAt: "",
+    organizationType: "",
+    hasPaidPlan: false,
+    access_token: "",
+    refresh_token: "",
+    scopes: [],
+  };
+  if (data) {
+    organization = {
+      id: data[0].id,
+      name: data[0].name,
+      description: data[0].description,
+      url: data[0].url,
+      avatarUrl: data[0].avatarUrl,
+      createdAt: data[0].createdAt,
+      updatedAt: data[0].updatedAt,
+      organizationType: data[0].organizationType,
+      hasPaidPlan: data[0].hasPaidPlan,
+      access_token: data[0].access_token,
+      refresh_token: data[0].refresh_token,
+      scopes: data[0].scopes,
+    };
+  }
+
+  return organization;
 }
