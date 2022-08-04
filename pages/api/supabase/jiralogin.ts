fabae4c1352fd5a0ea0f3e85dc32adef455123df
estebandalelr <estebandalelr@gmail.com>
2022-08-04 10:29:35 -0500

    Delete jiralogin.ts

--- pages/api/supabase/jiralogin.ts
+++
@@ -1,6 +0,0 @@
-import { supabase } from "../../../utils/supabase";
-export default async function handler(req, res) {
-  let { data, error, status } = await supabase.from("test").insert({});
-  if (error) res.send(error);
-  res.send(data);
-}
