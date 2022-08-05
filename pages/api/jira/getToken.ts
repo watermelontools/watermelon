25d79013c0e951fb8533ad3cf1d70befa8066bd8
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-05 13:04:18 -0500

    Extract access tokens

--- pages/api/jira/getToken.ts
+++ pages/api/jira/getToken.ts
@@ -1,4 +1,5 @@
 import updateTokens from "../../../utils/db/jira/updateTokens";
+import updateTokensFromJira from "../../../utils/jira/updateTokens";
 import getAPIAccessInfo from "../../../utils/db/jira/getAPIAccessInfo";
 
 export default async function handler(req, res) {
@@ -14,20 +15,9 @@ export default async function handler(req, res) {
     console.error(error);
     return res.send(error);
   }
-
-  console.log("data", data);
-  let newAccessTokens = await fetch("https://auth.atlassian.com/oauth/token", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      grant_type: "refresh_token",
-      client_id: process.env.NEXT_PUBLIC_JIRA_CLIENT_ID,
-      client_secret: process.env.JIRA_CLIENT_SECRET,
-      refresh_token: data.refresh_token,
-    }),
-  }).then((response) => response.json());
+  let newAccessTokens = await updateTokensFromJira({
+    refresh_token: data.refresh_token,
+  });
   const { access_token, refresh_token } = newAccessTokens;
   await updateTokens({ access_token, refresh_token, user });
   res.send({ access_token, cloudId: data.jira_id });
