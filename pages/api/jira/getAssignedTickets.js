90437f60398437d0f3d85dc1967bbafe4a0e0fce
baristaGeek <baristaGeek@users.noreply.github.com>
2022-08-05 00:07:33 +0000

    Prettified

--- pages/api/jira/getAssignedTickets.js
+++ pages/api/jira/getAssignedTickets.js
@@ -1,13 +1,19 @@
-const axios = require('axios');
+const axios = require("axios");
 
-const response =  axios.get('https://watermelontools.atlassian.net/rest/api/2/search?jql=assignee=6205b6df506317006b092e68', {
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': `Basic ${Buffer.from(
-            'evargas@watermelon.tools:Ws4UXtbhS2WZ0dnaBYGf9963'
-          ).toString('base64')}`,
+const response = axios
+  .get(
+    "https://watermelontools.atlassian.net/rest/api/2/search?jql=assignee=6205b6df506317006b092e68",
+    {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Basic ${Buffer.from(
+          "evargas@watermelon.tools:Ws4UXtbhS2WZ0dnaBYGf9963"
+        ).toString("base64")}`,
+      },
     }
-}).then(res => {
+  )
+  .then((res) => {
     res.send(res.data.issues);
-}).catch(err => console.error(err.response.data));;
+  })
+  .catch((err) => console.error(err.response.data));
