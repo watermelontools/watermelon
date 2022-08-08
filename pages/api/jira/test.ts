51e238aefc6feecbcf449dc28e48a9cad1755f59
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-08 09:43:21 -0500

    update query

--- pages/api/jira/test.ts
+++ pages/api/jira/test.ts
@@ -8,13 +8,17 @@ export default async function handler(req, res) {
     res.send({ error: "no access_token" });
   }
   await fetch(
-    `https://api.atlassian.com/ex/jira/${cloudId}/rest/api/3/field/search?query=assignee:6205b6df506317006b092e68'`,
+    `https://api.atlassian.com/ex/jira/${cloudId}/rest/api/3/search`,
     {
-      method: "GET",
+      method: "POST",
       headers: {
         Accept: "application/json",
         Authorization: `Bearer ${access_token}`,
       },
+      body: JSON.stringify({
+        jql: "assignee = 6205b6df506317006b092e68",
+        fields: ["summary", "status", "assignee", "created", "updated"],
+      }),
     }
   )
     .then((res) => res.json())
