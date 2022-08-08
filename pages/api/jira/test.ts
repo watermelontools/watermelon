f765ef12fa0e3677f85adb45c275f7edfdf97f14
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-08 09:50:16 -0500

    print body

--- pages/api/jira/test.ts
+++ pages/api/jira/test.ts
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
     }
   )
     .then((res) => {
-      console.log(res);
+      console.log(res.body);
       res.json();
     })
     .then((resJson) => {
