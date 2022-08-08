6686b797756669c356cc88111c395e5ca146a440
baristaGeek <estebanvargas94@gmail.com>
2022-08-08 16:14:33 -0500

    test getting self user

---
+++ utils/jira/getUserId.js
@@ -0,0 +1,31 @@
+export default async function handler(req, res) {
+    let { cloudId, access_token } = req.body;
+    let returnVal;
+    if (!cloudId) {
+      res.send({ error: "no cloudId" });
+    }
+    if (!access_token) {
+      res.send({ error: "no access_token" });
+    }
+    await fetch(
+      `https://api.atlassian.com/ex/jira/${cloudId}/rest/api/3/myself`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: `Bearer ${access_token}`,
+        },
+      }
+    )
+      .then((res) => {
+        console.log(res.body);
+        res.json();
+      })
+      .then((resJson) => {
+        console.log(resJson);
+        returnVal = resJson;
+      });
+    return res.send(returnVal);
+  }
+  
