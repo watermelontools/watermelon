e9c06167f97887072480b5676b5b63e5ef69492a
baristaGeek <estebanvargas94@gmail.com>
2022-08-08 16:13:21 -0500

    test getting self user

--- pages/api/jira/test.ts
+++ pages/api/jira/test.ts
@@ -9,18 +9,14 @@ export default async function handler(req, res) {
     res.send({ error: "no access_token" });
   }
   await fetch(
-    `https://api.atlassian.com/ex/jira/${cloudId}/rest/api/3/search`,
+    `https://api.atlassian.com/ex/jira/${cloudId}/rest/api/3/myself`,
     {
-      method: "POST",
+      method: "GET",
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
         Authorization: `Bearer ${access_token}`,
       },
-      body: JSON.stringify({
-        jql: "assignee = 6205b6df506317006b092e68",
-        fields: ["summary", "status", "assignee", "created", "updated"],
-      }),
     }
   )
     .then((res) => {
