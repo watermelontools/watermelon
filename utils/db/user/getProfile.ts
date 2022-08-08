117389c4a86b77108c98bfe02b50181ee5490512
estebandalelr <estebandalelr@gmail.com>
2022-08-08 13:12:18 -0500

    Dont close connection

--- utils/db/user/getProfile.ts
+++ utils/db/user/getProfile.ts
@@ -54,7 +54,6 @@ export default async function getUserProfile(
     });
 
     connection.execSql(request);
-    connection.close();
   }
   let { data, error, status } = await supabase
     .from("profiles")
