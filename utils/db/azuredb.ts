5e29006837de7ca2ee3f062a1fa9820d41dd8ea0
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-15 16:50:42 -0700

    Remove logging

--- utils/db/azuredb.ts
+++ utils/db/azuredb.ts
@@ -1,6 +1,5 @@
 const executeRequest = async (query) => {
   const queryString = JSON.stringify({ query: query });
-  console.log(queryString);
   let resp = await fetch("https://watermelonbackend.azurewebsites.net/users", {
     method: "POST",
     headers: {
@@ -9,14 +8,12 @@ const executeRequest = async (query) => {
     body: queryString,
   })
     .then((response) => {
-      console.log(response);
       return response.json();
     })
     .catch((error) => {
       console.error(error);
       return error;
     });
-  console.log(resp);
   return resp;
 };
 export default executeRequest;
