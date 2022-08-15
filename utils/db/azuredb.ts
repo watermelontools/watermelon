482fc48ee91ca981c9503556686f88de5ef0c922
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-15 12:03:14 -0700

    debug logging

--- utils/db/azuredb.ts
+++ utils/db/azuredb.ts
@@ -1,11 +1,14 @@
 const executeRequest = async (query) => {
+  const queryString = JSON.stringify({ query: query });
+  console.log(queryString);
   let resp = await fetch("https://watermelonbackend.azurewebsites.net/users", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ query: query }),
+    body: queryString,
   }).then((response) => response.json());
+  console.log(resp);
   return resp;
 };
 export default executeRequest;
