91df3070820506de93facd14a47a043f2bae94d7
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-16 15:02:05 -0700

    Fix query

--- utils/db/jira/saveUserInfo.ts
+++ utils/db/jira/saveUserInfo.ts
@@ -14,7 +14,31 @@ export default async ({
   user_id,
   user_displayname,
 }) => {
-  let query = `INSERT INTO [dbo].[jira] (access_token, refresh_token, jira_id, organization, url, org_avatar_url, scopes, user, user_email, user_avatar_url, user_id, user_displayname) VALUES ('${access_token}', '${refresh_token}', '${jira_id}', '${organization}', '${url}', '${org_avatar_url}', '${scopes}', '${user}', '${user_email}', '${user_avatar_url}', '${user_id}', '${user_displayname}')`;
+  let query = `INSERT INTO watermelon.dbo.jira (
+access_token,
+refresh_token,
+jira_id,
+organization,
+url,
+org_avatar_url,
+scopes,
+[user],
+user_email,
+user_avatar_url,
+user_id,
+user_displayname) VALUES (
+'${access_token}',
+'${refresh_token}',
+'${jira_id}',
+'${organization}',
+'${url}',
+'${org_avatar_url}',
+'${scopes}',
+'${user}',
+'${user_email}',
+'${user_avatar_url}',
+'${user_id}',
+'${user_displayname}')`;
   console.log(query);
   const queryString = JSON.stringify({ query: query });
   let resp = await executeRequest(queryString);
