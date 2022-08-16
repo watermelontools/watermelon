2a0a6b438c0c410a9c76ee18ecdbf4418c4aef95
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-16 15:37:36 -0700

    fix response await

--- utils/db/azuredb.ts
+++ utils/db/azuredb.ts
@@ -1,6 +1,5 @@
 const executeRequest = async (query) => {
   const queryString = JSON.stringify({ query: query });
-  console.log(queryString);
   let resp = await fetch("https://watermelonbackend.azurewebsites.net/users", {
     method: "POST",
     headers: {
@@ -9,11 +8,11 @@ const executeRequest = async (query) => {
     body: queryString,
   })
     .then((response) => {
-      console.log(response.status);
-      // print the response body to the console
-      let resp = response.json();
-      console.log(resp);
-      return resp;
+      response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      return data;
     })
     .catch((error) => {
       console.error(error);
