1fa35a683252f595fa8d26487d191be87251f0b9
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-16 16:51:35 -0700

    Create db.ts

---
+++ pages/api/db.ts
@@ -0,0 +1,23 @@
+export default async function handler(req, res) {
+  let { query } = req.body;
+  let queryString = JSON.stringify({ query: query });
+  let resp = await fetch("https://watermelonbackend.azurewebsites.net/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: queryString,
+  })
+    .then((response) => {
+      response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      return data;
+    })
+    .catch((error) => {
+      console.error(error);
+      return error;
+    });
+  return res.send(resp);
+}
