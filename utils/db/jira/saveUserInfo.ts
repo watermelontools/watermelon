030c0e2ce0a342c9c07a39d6ebcf2729bbddda70
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-17 12:21:18 -0700

    Use stored procedure to save

--- utils/db/jira/saveUserInfo.ts
+++ utils/db/jira/saveUserInfo.ts
@@ -14,7 +14,7 @@ export default async ({
   user_id,
   user_displayname,
 }) => {
-  let query = `INSERT INTO watermelon.dbo.jira (access_token,refresh_token,jira_id,organization,url,org_avatar_url,scopes,[user],user_email,user_avatar_url,user_id,user_displayname) OUTPUT INSERTED.* VALUES ('${access_token}','${refresh_token}','${jira_id}','${organization}','${url}','${org_avatar_url}','${scopes}','${user}','${user_email}','${user_avatar_url}','${user_id}','${user_displayname}') FOR JSON PATH`;
+  let query = `EXEC create_jira @organization=${organization}, @jira_id=${jira_id}, @url=${url}, @org_avatar_url=${org_avatar_url}, @scopes=${scopes}, @user=${user}, @user_email=${user_email}, @user_avatar_url=${user_avatar_url}, @user_id=${user_id}, @user_displayname=${user_displayname}, @access_token=${access_token}, @refresh_token=${refresh_token}`;
   let resp = await executeRequest(query);
   return resp;
 };
