c657ad66b3734433790c3e55a351903499a987c0
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-17 12:34:13 -0700

    Update saveUserInfo.ts

--- utils/db/jira/saveUserInfo.ts
+++ utils/db/jira/saveUserInfo.ts
@@ -14,7 +14,7 @@ export default async ({
   user_id,
   user_displayname,
 }) => {
-  let query = `EXEC dbo.create_jira @organization=${organization}, @jira_id=${jira_id}, @url=${url}, @org_avatar_url=${org_avatar_url}, @scopes=${scopes}, @user=${user}, @user_email=${user_email}, @user_avatar_url=${user_avatar_url}, @user_id=${user_id}, @user_displayname=${user_displayname}, @access_token=${access_token}, @refresh_token=${refresh_token}`;
+  let query = `EXEC dbo.create_jira @organization='${organization}', @jira_id='${jira_id}', @url='${url}', @org_avatar_url='${org_avatar_url}', @scopes='${scopes}', @user='${user}', @user_email='${user_email}', @user_avatar_url='${user_avatar_url}', @user_id='${user_id}', @user_displayname='${user_displayname}', @access_token='${access_token}', @refresh_token='${refresh_token}'`;
   let resp = await executeRequest(query);
   return resp;
 };
