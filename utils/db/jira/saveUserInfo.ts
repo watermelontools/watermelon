0d5c02294281d2cfcaabebb4f448b7a76cf51d00
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-17 10:19:18 -0700

    Return inserted

--- utils/db/jira/saveUserInfo.ts
+++ utils/db/jira/saveUserInfo.ts
@@ -14,7 +14,7 @@ export default async ({
   user_id,
   user_displayname,
 }) => {
-  let query = `INSERT INTO watermelon.dbo.jira (access_token,refresh_token,jira_id,organization,url,org_avatar_url,scopes,[user],user_email,user_avatar_url,user_id,user_displayname) VALUES ('${access_token}','${refresh_token}','${jira_id}','${organization}','${url}','${org_avatar_url}','${scopes}','${user}','${user_email}','${user_avatar_url}','${user_id}','${user_displayname}')`;
+  let query = `INSERT INTO watermelon.dbo.jira (access_token,refresh_token,jira_id,organization,url,org_avatar_url,scopes,[user],user_email,user_avatar_url,user_id,user_displayname) VALUES ('${access_token}','${refresh_token}','${jira_id}','${organization}','${url}','${org_avatar_url}','${scopes}','${user}','${user_email}','${user_avatar_url}','${user_id}','${user_displayname}') OUTPUT INSERTED.* FOR JSON PATH`;
   let resp = await executeRequest(query);
   return resp;
 };
