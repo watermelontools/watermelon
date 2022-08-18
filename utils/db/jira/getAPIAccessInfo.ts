89a0b583a808f2bd2b1f5b970aa0c375c844a850
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-18 11:24:38 -0700

    use azure to get tokens

--- utils/db/jira/getAPIAccessInfo.ts
+++ utils/db/jira/getAPIAccessInfo.ts
@@ -1,9 +1,14 @@
 import { supabase } from "../../supabase";
+import executeRequest from "../azuredb";
 export default async function getAPIAccessInfo(user: string): Promise<{
   access_token: string;
   refresh_token: string;
   cloudId: string;
 }> {
+  let query = `EXEC dbo.update_jira_tokens  @user='${user}'`;
+  let resp = await executeRequest(query);
+  console.log("updateTokens", resp);
+  //return resp;
   let { data, error, status } = await supabase
     .from("Jira")
     .select("access_token, refresh_token, jira_id")
