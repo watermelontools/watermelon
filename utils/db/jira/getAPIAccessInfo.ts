023de58fac5e42d56f255b6d218efc32d86c3dce
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-18 11:39:21 -0700

    Make azure source

--- utils/db/jira/getAPIAccessInfo.ts
+++ utils/db/jira/getAPIAccessInfo.ts
@@ -8,16 +8,5 @@ export default async function getAPIAccessInfo(user: string): Promise<{
   let query = `EXEC dbo.get_jira_tokens  @user='${user}'`;
   let resp = await executeRequest(query);
   console.log("updateTokens", resp);
-  //return resp;
-  let { data, error, status } = await supabase
-    .from("Jira")
-    .select("access_token, refresh_token, jira_id")
-    .eq("user", user);
-  if (error && status !== 406) {
-    throw error;
-  }
-  if (!data || !data[0]) {
-    throw new Error("no data");
-  }
-  return data[0];
+  return resp;
 }
