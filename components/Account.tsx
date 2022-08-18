b8905e79291eb2da1af13284fb84753b1e6cebdb
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-18 14:17:33 -0700

    remove github

--- components/Account.tsx
+++ components/Account.tsx
@@ -74,15 +74,7 @@ export default function Account({ session }) {
           </Link>
         )}
       </div>
-      <div>
-        <Link
-          href={`https://github.com/login/oauth/authorize?client_id=8543242e428085df968c&redirect_uri=https://app.watermelon.tools/github&state=${
-            userId ? userId : ""
-          }&scope=repo%20user%20notifications`}
-        >
-          <a>Sign in with GitHub</a>
-        </Link>
-      </div>
+
       <p>{userId}</p>
       <div>
         <button
