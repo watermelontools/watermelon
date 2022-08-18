fb0781b70ff49ad329c1f29841507f0d0457f9fb
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-18 11:55:47 -0700

    Remove suupabase

--- utils/db/jira/updateTokens.ts
+++ utils/db/jira/updateTokens.ts
@@ -1,4 +1,3 @@
-import { supabase } from "../../supabase";
 import executeRequest from "../azuredb";
 
 export default async function updateTokens({
@@ -10,16 +9,6 @@ export default async function updateTokens({
   access_token: string;
   refresh_token: string;
 }): Promise<void> {
-  let { data, error, status } = await supabase
-    .from("Jira")
-    .update({
-      access_token,
-      refresh_token,
-    })
-    .eq("user", user);
-  if (error && status !== 406) {
-    throw error;
-  }
   let query = `EXEC dbo.update_jira_tokens  @user='${user}', @access_token='${access_token}', @refresh_token='${refresh_token}'`;
   let resp = await executeRequest(query);
   console.log("updateTokens", resp);
