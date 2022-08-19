eb9885baf82ebbf5baa6868ef339e4f755fd3277
baristaGeek <estebanvargas94@gmail.com>
2022-08-19 11:19:27 -0700

    New JQL query

--- pages/api/jira/assignedTicketsTest.ts
+++ pages/api/jira/assignedTicketsTest.ts
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
       },
       body: JSON.stringify({
         // jql: `assignee="${user}" AND status="Open" OR status="In Progress"`,
-        jql: `assignee="${user}"`,
+        jql: `assignee="${user} AND status="Open"`,
       }),
     }
   )
