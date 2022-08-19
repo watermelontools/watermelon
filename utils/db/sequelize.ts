df746c504420458a8b86fe4ae07dde21937b1eab
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-19 10:46:40 -0700

    add tedious manually

--- utils/db/sequelize.ts
+++ utils/db/sequelize.ts
@@ -1,10 +1,12 @@
 const Sequelize = require("sequelize");
+import * as tedious from "tedious";
 const sequelize = new Sequelize(
   process.env.AZURE_DATABASE_DBNAME,
   process.env.AZURE_DATABASE_USERNAME,
   process.env.AZURE_DATABASE_PASSWORD,
   {
     host: process.env.AZURE_DATABASE_HOST,
+    dialectModule: tedious,
     dialect: "mssql",
     pool: {
       max: 5,
