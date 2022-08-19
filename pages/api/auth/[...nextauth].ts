4b7a2b334c0e35cdf4d28bfd0e53ccffd226322e
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-18 18:21:31 -0700

    add next auth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+import EmailProvider from "next-auth/providers/email";
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+});
