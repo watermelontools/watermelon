8a7ac6f55e270f064de14a34e3b422e7ab4272bf
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-19 12:06:53 -0700

    Test coannection

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,6 +3,16 @@ import EmailProvider from "next-auth/providers/email";
 import GithubProvider from "next-auth/providers/github";
 import SequelizeAdapter from "@next-auth/sequelize-adapter";
 import sequelize from "../../../utils/db/sequelize";
+
+const test = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+test();
 export default NextAuth({
   adapter: SequelizeAdapter(sequelize),
   debug: true,
