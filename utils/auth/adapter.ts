be53706e70f37936b95bbb2214ef1611514eca2d
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-19 15:44:42 -0700

    Fix queries

--- utils/auth/adapter.ts
+++ utils/auth/adapter.ts
@@ -18,28 +18,28 @@ export default function MyAdapter(): Adapter {
     async getUser(id) {
       console.log("getUser", id);
       return await executeRequest(
-        `SELECT id, name, email, email_verified, [image], created_at, updated_at FROM watermelon.dbo.users;
+        `SELECT id, name, email, email_verified, [image], created_at, updated_at FROM watermelon.dbo.users WHERE id = '${id}';
         `
       );
     },
     async getUserByEmail(email) {
       console.log("getUserByEmail", email);
       return await executeRequest(
-        `SELECT id, name, email, email_verified, [image], created_at, updated_at FROM watermelon.dbo.users;
+        `SELECT id, name, email, email_verified, [image], created_at, updated_at FROM watermelon.dbo.users WHERE email = '${email}';
         `
       );
     },
     async getUserByAccount({ providerAccountId, provider }) {
       console.log("getUserByAccount", providerAccountId, provider);
       return await executeRequest(
-        `SELECT id, compound_id, user_id, provider_type, provider_id, provider_account_id, refresh_token, access_token, access_token_expires, created_at, updated_at FROM watermelon.dbo.accounts;
+        `SELECT id, compound_id, user_id, provider_type, provider_id, provider_account_id, refresh_token, access_token, access_token_expires, created_at, updated_at FROM watermelon.dbo.accounts WHERE provider_account_id = '${providerAccountId}' AND provider_type = '${provider}';
         `
       );
     },
     async updateUser(user) {
       console.log("updateUser", user);
       return await executeRequest(
-        `UPDATE watermelon.dbo.users SET id='', name='', email='', email_verified='', [image]='', created_at=getdate(), updated_at=getdate();
+        `UPDATE watermelon.dbo.users SET id='', name='', email='', email_verified='', [image]='', created_at=getdate(), updated_at=getdate() WHERE id = '${user.id}';
         `
       );
     },
@@ -68,7 +68,7 @@ export default function MyAdapter(): Adapter {
     async getSessionAndUser(sessionToken) {
       console.log("getSessionAndUser", sessionToken);
       return await executeRequest(
-        `SELECT id, user_id, expires, session_token, token, created_at, updated_at FROM watermelon.dbo.sessions;
+        `SELECT id, user_id, expires, session_token, token, created_at, updated_at FROM watermelon.dbo.sessions WHERE session_token = '${sessionToken}';
         `
       );
     },
@@ -93,7 +93,7 @@ export default function MyAdapter(): Adapter {
     async useVerificationToken({ identifier, token }) {
       console.log("useVerificationToken", identifier, token);
       return await executeRequest(
-        `SELECT id, identifier, token, expires, created_at, updated_at FROM watermelon.dbo.verification_tokens;
+        `SELECT id, identifier, token, expires, created_at, updated_at FROM watermelon.dbo.verification_tokens WHERE identifier = '${identifier}' AND token = '${token}';
         `
       );
     },
