1e6758481472789862db1a4c3cbd14161297c432
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-19 14:10:53 -0700

    add username

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -22,11 +22,14 @@ export default NextAuth({
       server: {
         host: process.env.EMAIL_SERVER_HOST,
         port: process.env.EMAIL_SERVER_PORT,
+        username: process.env.EMAIL_SERVER_USERNAME,
         auth: {
           user: process.env.EMAIL_SERVER_USER,
+          username: process.env.EMAIL_SERVER_USERNAME,
           pass: process.env.EMAIL_SERVER_PASSWORD,
         },
       },
+      username: process.env.EMAIL_SERVER_USERNAME,
       from: process.env.EMAIL_FROM,
     }),
     GithubProvider({
