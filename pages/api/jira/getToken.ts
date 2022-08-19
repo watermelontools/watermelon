61f2284b39961be659877cff1a9b00d96b373f45
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-19 13:46:53 -0700

    Update getToken.ts

--- pages/api/jira/getToken.ts
+++ pages/api/jira/getToken.ts
@@ -1,6 +1,7 @@
 import updateTokens from "../../../utils/db/jira/updateTokens";
 import updateTokensFromJira from "../../../utils/jira/updateTokens";
 import getAPIAccessInfo from "../../../utils/db/jira/getAPIAccessInfo";
+import sequelize from "../../../utils/db/sequelize";
 
 export default async function handler(req, res) {
   let { user } = req.body;
@@ -9,6 +10,8 @@ export default async function handler(req, res) {
     return res.send({ error: "no user" });
   }
   try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
     let { refresh_token, access_token, cloudId } = (
       await getAPIAccessInfo(user)
     )[0];
