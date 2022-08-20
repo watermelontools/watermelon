2e5cd67342902092a2bcce5d825ff7141ef65a56
baristaGeek <estebanvargas94@gmail.com>
2022-08-20 15:54:19 -0700

    Parametrize Jira cloud ID and user email

--- pages/api/jira/getAssignedTicketsInProgress.ts
+++ pages/api/jira/getAssignedTicketsInProgress.ts
@@ -1,26 +1,19 @@
 import getJiraOrganization from "../../../utils/db/jira/getOrganization";
 
 export default async function handler(req, res) {
-  let { cloudId, user, access_token } = req.body;
-  // get access_token from getToken.ts
-  // let access_token = await getToken(req.body.refresh_token);
-  // get cloudID and user from getJirgaOrganization.ts
+  let { user, access_token } = req.body;
 
-  // .user_id and user_email is what we need
-  // get metadata from getMetadata.ts
-  let getJira = await getJiraOrganization(user);
-  console.log("getJira - getAssignedTicketsInProgress.ts", getJira);
-
+  let {jira_id, user_email} = await getJiraOrganization(user);
 
   let returnVal;
-  if (!cloudId) {
-    res.send({ error: "no cloudId" });
+  if (!jira_id) {
+    res.send({ error: "no Jira cloudId" });
   }
   if (!access_token) {
     res.send({ error: "no access_token" });
   }
   await fetch(
-    `https://api.atlassian.com/ex/jira/${cloudId}/rest/api/3/search`,
+    `https://api.atlassian.com/ex/jira/${jira_id}/rest/api/3/search`,
     {
       method: "POST",
       headers: {
@@ -29,7 +22,7 @@ export default async function handler(req, res) {
         Authorization: `Bearer ${access_token}`,
       },
       body: JSON.stringify({
-        jql: `assignee="${user}" AND status not in (Backlog, Done)`,
+        jql: `assignee="${user_email}" AND status not in (Backlog, Done)`,
       }),
     }
   )
