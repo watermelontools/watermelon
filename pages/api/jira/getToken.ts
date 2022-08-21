cb167c771a28a8ed84b64444a8445507af033031
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-20 17:58:50 -0700

    Export function

--- pages/api/jira/getToken.ts
+++ pages/api/jira/getToken.ts
@@ -1,26 +1,9 @@
-import updateTokens from "../../../utils/db/jira/updateTokens";
-import updateTokensFromJira from "../../../utils/jira/updateTokens";
-import getAPIAccessInfo from "../../../utils/db/jira/getAPIAccessInfo";
-
+import refreshTokens from "../../../utils/jira/refreshTokens";
 export default async function handler(req, res) {
   let { user } = req.body;
 
   if (!user) {
     return res.send({ error: "getToken.ts - no user" });
   }
-  try {
-    let { refresh_token, access_token, cloudId } = (
-      await getAPIAccessInfo(user)
-    )[0];
-    let newAccessTokens = await updateTokensFromJira({
-      refresh_token: refresh_token,
-    });
-    refresh_token = newAccessTokens.refresh_token;
-    access_token = newAccessTokens.access_token;
-    await updateTokens({ access_token, refresh_token, user });
-    res.send({ access_token, cloudId });
-  } catch (error) {
-    console.error(error);
-    return res.send(error);
-  }
+  return await refreshTokens({ user });
 }
---
+++ utils/jira/refreshTokens.ts
@@ -0,0 +1,21 @@
+import updateTokens from "./updateTokens";
+import updateTokensFromJira from "./updateTokens";
+import getAPIAccessInfo from "../db/jira/getAPIAccessInfo";
+
+export default async ({ user }) => {
+  try {
+    let { refresh_token, access_token, cloudId } = (
+      await getAPIAccessInfo(user)
+    )[0];
+    let newAccessTokens = await updateTokensFromJira({
+      refresh_token: refresh_token,
+    });
+    refresh_token = newAccessTokens.refresh_token;
+    access_token = newAccessTokens.access_token;
+    await updateTokens({ refresh_token });
+    return { access_token, cloudId };
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+};
