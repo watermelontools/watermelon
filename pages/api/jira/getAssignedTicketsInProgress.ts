78bb512f31c9ef0971303f7c6aa7007ea04b29d3
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-20 18:13:53 -0700

    Update getAssignedTicketsInProgress.ts

--- pages/api/jira/getAssignedTicketsInProgress.ts
+++ pages/api/jira/getAssignedTicketsInProgress.ts
@@ -1,40 +1,24 @@
 import getJiraOrganization from "../../../utils/db/jira/getOrganization";
-import getToken from "./getToken";
+import getToken from "../../../utils/jira/refreshTokens";
 
 export default async function handler(req, res) {
   let { user } = req.body.user;
-  let access_token = "";
 
-    await fetch(
-      `https://app.watermelon.tools/api/jira/getToken`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-          // Authorization: `Bearer ${access_token}`,
-        }
-      }
-    ).then((res) => {
-        res.json();
-    }).then((data) => {
-      console.log("line 25 data: " + data);
-        this.access_token = data;
-    }).catch((error) => {
-        console.error(error);
-    })
+  if (!user) {
+    return res.send({ error: "getToken.ts - no user" });
+  }
 
-  console.log("retrieved access token - getAssignedTicketsInProgress", access_token);
-  let {jira_id, user_email} = await getJiraOrganization(user);
+  let { access_token, cloudId } = await getToken({ user });
 
-  let returnVal;
+  let { jira_id, user_email } = await getJiraOrganization(user);
+
   if (!jira_id) {
     res.send({ error: "no Jira cloudId" });
   }
   if (!access_token) {
     res.send({ error: "no access_token" });
   }
-  await fetch(
+  let returnVal = await fetch(
     `https://api.atlassian.com/ex/jira/${jira_id}/rest/api/3/search`,
     {
       method: "POST",
@@ -49,8 +33,6 @@ export default async function handler(req, res) {
     }
   )
     .then((res) => res.json())
-    .then((resJson) => {
-      returnVal = resJson.issues;
-    });
+    .then((resJson) => resJson.issues);
   return res.send(returnVal);
 }
