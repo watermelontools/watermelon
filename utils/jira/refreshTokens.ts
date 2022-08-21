a5d4ff28e3e8a91af62427b9fe11fb1f6c92466b
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-20 18:33:21 -0700

    Do not refresh tokens

--- utils/jira/refreshTokens.ts
+++ utils/jira/refreshTokens.ts
@@ -5,12 +5,12 @@ import getAPIAccessInfo from "../db/jira/getAPIAccessInfo";
 export default async ({ user }) => {
   try {
     let { refresh_token, access_token, cloudId } = await getAPIAccessInfo(user);
-    let newAccessTokens = await updateTokensFromJira({
+    /*     let newAccessTokens = await updateTokensFromJira({
       refresh_token: refresh_token,
     });
     refresh_token = newAccessTokens.refresh_token;
     access_token = newAccessTokens.access_token;
-    await updateTokens({ refresh_token });
+    await updateTokens({ refresh_token }); */
     return { access_token, cloudId };
   } catch (error) {
     console.error(error);
