b333a35ad517f31c96e9ccc8770efe3e4b7c66c9
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-22 14:57:55 -0700

    Move to stored proc

--- utils/db/jira/getOrganization.ts
+++ utils/db/jira/getOrganization.ts
@@ -18,25 +18,23 @@ export default async function getJiraOrganization(user): Promise<Organization> {
     jira_id: "",
     user_email: "",
   };
-  let azureData = await executeRequest(
-    `SELECT * FROM watermelon.dbo.jira WHERE [user]='${user}' FOR JSON PATH`
-  );
-  if (azureData) {
+  let data = await executeRequest(`EXEC dbo.get_jira @user = '${user}'`);
+  if (data) {
     organization = {
-      id: azureData[0].id,
-      name: azureData[0].name,
-      description: azureData[0].description,
-      url: azureData[0].url,
-      avatarUrl: azureData[0].avatarUrl,
-      createdAt: azureData[0].createdAt,
-      updatedAt: azureData[0].updatedAt,
-      organizationType: azureData[0].organizationType,
-      hasPaidPlan: azureData[0].hasPaidPlan,
-      access_token: azureData[0].access_token,
-      refresh_token: azureData[0].refresh_token,
-      scopes: azureData[0].scopes,
-      jira_id: azureData[0].jira_id,
-      user_email: azureData[0].user_email,
+      id: data.id,
+      name: data.name,
+      description: data.description,
+      url: data.url,
+      avatarUrl: data.avatarUrl,
+      createdAt: data.createdAt,
+      updatedAt: data.updatedAt,
+      organizationType: data.organizationType,
+      hasPaidPlan: data.hasPaidPlan,
+      access_token: data.access_token,
+      refresh_token: data.refresh_token,
+      scopes: data.scopes,
+      jira_id: data.jira_id,
+      user_email: data.user_email,
     };
   }
 
