99b6921d9b915d11d3df5746b676b9dd9a65c41b
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-22 12:07:00 -0700

    Fix saving of tickets

--- utils/jira/refreshTokens.ts
+++ utils/jira/refreshTokens.ts
@@ -1,16 +1,16 @@
-import updateTokens from "./updateTokens";
+import updateTokens from "../db/jira/updateTokens";
 import updateTokensFromJira from "./updateTokens";
 import getAPIAccessInfo from "../db/jira/getAPIAccessInfo";
 
 export default async ({ user }) => {
   try {
     let { refresh_token, access_token, cloudId } = await getAPIAccessInfo(user);
-    /*     let newAccessTokens = await updateTokensFromJira({
+    let newAccessTokens = await updateTokensFromJira({
       refresh_token: refresh_token,
     });
     refresh_token = newAccessTokens.refresh_token;
     access_token = newAccessTokens.access_token;
-    await updateTokens({ refresh_token }); */
+    await updateTokens({ access_token, refresh_token, user });
     return { access_token, cloudId };
   } catch (error) {
     console.error(error);
