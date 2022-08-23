0ef7685cfda5e84517ee5308971dfa872d56ff7d
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-23 14:47:59 -0700

    Conditionally update user

--- utils/auth/adapter.ts
+++ utils/auth/adapter.ts
@@ -55,6 +55,9 @@ export default function MyAdapter(): Adapter {
     },
     async updateUser(user) {
       console.log("updateUser", user);
+      if (!user.emailVerified || !user.email) {
+        return null;
+      }
       return await executeRequest(
         `EXEC [dbo].[update_user] @id = '${user.id}', @email = '${user.email}', @name = '${user.name}', @emailVerified = '${user.emailVerified}';
         `
