f2a44583047dda918328f555156c68001aee9c99
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-22 18:12:36 -0700

    Remove last traces of supabase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,9 @@
-import { supabase } from "../utils/supabase";
-import Auth from "../components/Auth";
-import Account from "../components/Account";
-import { useState, useEffect } from "react";
+import LogInBtn from "../components/login-btn";
+
 function HomePage({ organization }) {
-  const [session, setSession] = useState(null);
-
-  useEffect(() => {
-    setSession(supabase.auth.session());
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-  }, []);
   return (
     <div className="container" style={{ padding: "50px 0 100px 0" }}>
-      {!session ? (
-        <Auth />
-      ) : (
-        <Account key={session.user.id} session={session} />
-      )}
+      <LogInBtn />
     </div>
   );
 }
