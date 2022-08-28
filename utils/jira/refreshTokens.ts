75e853e7b2dcad92dab18702a4a87f9fdf5d5bb0
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-28 10:03:30 -0700

    code cleanup

--- utils/jira/refreshTokens.ts
+++ utils/jira/refreshTokens.ts
@@ -4,12 +4,10 @@ import getAPIAccessInfo from "../db/jira/getAPIAccessInfo";
 
 export default async ({ user }) => {
   try {
-    let { refresh_token, access_token, cloudId } = await getAPIAccessInfo(user);
-    let newAccessTokens = await updateTokensFromJira({
-      refresh_token: refresh_token,
-    });
+    let { refresh_token, cloudId } = await getAPIAccessInfo(user);
+    let newAccessTokens = await updateTokensFromJira({ refresh_token });
     refresh_token = newAccessTokens.refresh_token;
-    access_token = newAccessTokens.access_token;
+    let access_token = newAccessTokens.access_token;
     await updateTokens({ access_token, refresh_token, user });
     return { access_token, cloudId };
   } catch (error) {
