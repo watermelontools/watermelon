776df7f0affbe567ca2b98aaf44734cae5807668
Esteban Dalel R <estebandalelr@gmail.com>
2022-08-30 14:33:27 -0700

    Never overwrite tokens

--- utils/jira/refreshTokens.ts
+++ utils/jira/refreshTokens.ts
@@ -6,10 +6,12 @@ export default async ({ user }) => {
   try {
     let { refresh_token, cloudId } = await getAPIAccessInfo(user);
     let newAccessTokens = await updateTokensFromJira({ refresh_token });
-    refresh_token = newAccessTokens.refresh_token;
-    let access_token = newAccessTokens.access_token;
-    await updateTokens({ access_token, refresh_token, user });
-    return { access_token, cloudId };
+    await updateTokens({
+      access_token: newAccessTokens.access_token,
+      refresh_token: newAccessTokens.refresh_token,
+      user,
+    });
+    return { access_token: newAccessTokens.access_token, cloudId };
   } catch (error) {
     console.error(error);
     return error;
