ff762c3680ca42f37c8a1ef6aef56ce53e666e07
baristaGeek <baristaGeek@users.noreply.github.com>
2022-08-31 23:42:09 +0000

    Prettified

--- pages/api/jira/getMostRelevantJiraTicket.ts
+++ pages/api/jira/getMostRelevantJiraTicket.ts
@@ -2,13 +2,27 @@ import getJiraOrganization from "../../../utils/db/jira/getOrganization";
 import getToken from "../../../utils/jira/refreshTokens";
 
 export default async function handler(req, res) {
-  let { user, pr_title} = req.body;
+  let { user, pr_title } = req.body;
 
   // Remove stopwords to provide better search results
-  let stopwords = ['add', 'get', 'as', 'at', 'he', 'the', 'was', 'from', 'and', 'or'];
+  let stopwords = [
+    "add",
+    "get",
+    "as",
+    "at",
+    "he",
+    "the",
+    "was",
+    "from",
+    "and",
+    "or",
+  ];
   // trim pr_title
   pr_title = pr_title.trim();
-  pr_title = pr_title.split(' ').filter(word => !stopwords.includes(word.toLowerCase())).join(' ');
+  pr_title = pr_title
+    .split(" ")
+    .filter((word) => !stopwords.includes(word.toLowerCase()))
+    .join(" ");
   // Add an OR clause to make the search query more flexible
   let split_pr_title = pr_title.split(" ");
   let parsed_pr_title = split_pr_title.join(" OR ");
