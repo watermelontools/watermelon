f4e682092ed3a5940db2a4355617a41929c1f724
Esteban Dalel R <estebandalelr@gmail.com>
2022-09-02 11:56:53 -0700

    Update vscode-insiders.tsx

--- pages/vscode-insiders.tsx
+++ pages/vscode-insiders.tsx
@@ -1,13 +1,24 @@
 import Link from "next/link";
+import { useSession, signIn } from "next-auth/react";
 
 function VSCodeInsiders() {
+  const { status, data } = useSession({
+    required: true,
+    onUnauthenticated() {
+      signIn();
+    },
+  });
   return (
     <div className="w-full flex flex-col justify-center items-center">
-      <p>
-        <Link href="vscode-insiders://watermelontools.watermelon-tools">
-          <a>vscode</a>
+      {status !== "loading" && (
+        <Link
+          href={`vscode-insiders://watermelontools.watermelon-tools?email=${
+            data?.user?.email ?? ""
+          }`}
+        >
+          <a>click to login with vscode</a>
         </Link>
-      </p>
+      )}
     </div>
   );
 }
