fed6f83b6feb402aa66bbb90eae41ae9507e7d9c
Esteban Dalel R <estebandalelr@gmail.com>
2022-09-01 17:17:54 -0700

    test making the route protected

--- pages/vscode.tsx
+++ pages/vscode.tsx
@@ -1,6 +1,15 @@
 import Link from "next/link";
+import { useSession, signIn } from "next-auth/react";
+import Router from "next/router";
 
 function VSCode() {
+  const { status, data } = useSession({
+    required: true,
+    onUnauthenticated() {
+      signIn();
+    },
+  });
+  console.log(status, data);
   return (
     <div className="w-full flex flex-col justify-center items-center">
       <p>
