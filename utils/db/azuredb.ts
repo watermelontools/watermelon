345fbfbd6fc7cc00218f2a02e0fcf9c6c98ce8e9
Esteban Dalel R <estebandalelr@gmail.com>
2022-09-02 18:18:36 -0700

    Feature/try catch awaits (#78)
    * remove unused code
    
    * Add try catch
    
    * Delete unused file
    
    * rename response object
    
    * code cleanup
    
    * Add try catch
    
    * Add catch
    
    * Add try catch
    
    * Never overwrite tokens
    
    * Add try catchs

--- utils/db/azuredb.ts
+++ utils/db/azuredb.ts
@@ -1,21 +1,26 @@
 const executeRequest = async (query) => {
   const queryString = JSON.stringify({ query: query });
-  let resp = await fetch(process.env.AZURE_WEBAPP_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: queryString,
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      return data;
+  try {
+    let resp = await fetch(process.env.AZURE_WEBAPP_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: queryString,
     })
-    .catch((error) => {
-      console.error(error);
-      return error;
-    });
-  console.log("resp", resp);
-  return resp;
+      .then((response) => response.json())
+      .then((data) => {
+        return data;
+      })
+      .catch((error) => {
+        console.error(error);
+        return error;
+      });
+    console.log("resp", resp);
+    return resp;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
 };
 export default executeRequest;
--- utils/jira/updateTokens.ts
+++ utils/jira/updateTokens.ts
@@ -3,24 +3,32 @@ export default async function updateTokens({
 }: {
   refresh_token: string;
 }): Promise<{ access_token: string; refresh_token: string }> {
-  let newAccessTokens = await fetch("https://auth.atlassian.com/oauth/token", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      grant_type: "refresh_token",
-      client_id: process.env.NEXT_PUBLIC_JIRA_CLIENT_ID,
-      client_secret: process.env.JIRA_CLIENT_SECRET,
-      refresh_token: refresh_token,
-    }),
-  })
-    .then((response) => response.json())
-    .catch((error) => {
-      console.error(error);
-    });
-  return {
-    access_token: newAccessTokens.access_token,
-    refresh_token: newAccessTokens.refresh_token,
-  };
+  try {
+    let newAccessTokens = await fetch(
+      "https://auth.atlassian.com/oauth/token",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          grant_type: "refresh_token",
+          client_id: process.env.NEXT_PUBLIC_JIRA_CLIENT_ID,
+          client_secret: process.env.JIRA_CLIENT_SECRET,
+          refresh_token: refresh_token,
+        }),
+      }
+    )
+      .then((response) => response.json())
+      .catch((error) => {
+        console.error(error);
+      });
+    return {
+      access_token: newAccessTokens.access_token,
+      refresh_token: newAccessTokens.refresh_token,
+    };
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
 }
