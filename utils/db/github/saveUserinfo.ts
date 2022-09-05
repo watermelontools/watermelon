9e9c3d59c65d4f28ea89aac2ff466dfa7acaa940
Esteban Dalel R <estebandalelr@gmail.com>
2022-09-04 19:52:47 -0700

    Partial github integration

---
+++ utils/db/github/saveUserinfo.ts
@@ -0,0 +1,20 @@
+import executeRequest from "../azuredb";
+
+export default async ({
+  access_token,
+  refresh_token,
+  jira_id,
+  organization,
+  url,
+  org_avatar_url,
+  scopes,
+  watermelon_user,
+  user_email,
+  user_avatar_url,
+  user_id,
+  user_displayname,
+}) => {
+  let query = `EXEC dbo.create_github @organization='${organization}', @jira_id='${jira_id}', @url='${url}', @org_avatar_url='${org_avatar_url}', @watermelon_user='${watermelon_user}', @user_email='${user_email}', @user_avatar_url='${user_avatar_url}', @user_id='${user_id}', @user_displayname='${user_displayname}', @access_token='${access_token}', @refresh_token='${refresh_token}', @scopes='${scopes}'`;
+  let resp = await executeRequest(query);
+  return resp;
+};
