9f14fea491e7b54238779110082181d12ee26af3
Esteban Dalel R <estebandalelr@gmail.com>
2022-09-06 16:51:03 -0700

    Stop logging azure

--- utils/db/azuredb.ts
+++ utils/db/azuredb.ts
@@ -16,7 +16,7 @@ const executeRequest = async (query) => {
         console.error(error);
         return error;
       });
-    console.log("resp", resp);
+    // console.log("resp", resp);
     return resp;
   } catch (error) {
     console.error(error);
