d833ebe044cdb933e9e128ac668852dd6f62d346
Esteban Dalel R <estebandalelr@gmail.com>
2022-09-07 16:20:40 -0700

    Display github user

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,15 @@ import LogInBtn from "../components/login-btn";
 
 function HomePage({}) {
   const [userEmail, setUserEmail] = useState(null);
-  const [userData, setUserData] = useState(null);
+  const [jiraUserData, setJiraUserData] = useState(null);
+  const [githubUserData, setGithubUserData] = useState(null);
   const { data: session } = useSession();
   useEffect(() => {
     setUserEmail(session?.user?.email);
   }, [session]);
   useEffect(() => {
     getJiraInfo();
+    getGitHubInfo();
   }, [userEmail]);
   async function getJiraInfo() {
     const res = await fetch("/api/jira/getUser", {
@@ -24,9 +26,21 @@ function HomePage({}) {
       }),
     });
     const data = await res.json();
-    setUserData(data);
+    setJiraUserData(data);
   }
-
+  async function getGitHubInfo() {
+    const res = await fetch("/api/github/getUser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: userEmail,
+      }),
+    });
+    const data = await res.json();
+    setGithubUserData(data);
+  }
   return (
     <div className="container" style={{ padding: "50px 0 100px 0" }}>
       <LogInBtn />
@@ -34,21 +48,30 @@ function HomePage({}) {
       {userEmail && (
         <div>
           <div>
-            <Link
-              href={`https://github.com/login/oauth/authorize?client_id=8543242e428085df968c&redirect_uri=https://app.watermelontools.com/github&state=${userEmail}&scope=repo%20user%20notifications`}
-            >
-              <a>Sign in with GitHub</a>
-            </Link>
+            {githubUserData.name ? (
+              <div>
+                <h1>GitHub</h1>
+                <h2>{githubUserData.name}</h2>
+                <h3>{githubUserData.login}</h3>
+                <img src={githubUserData.avatar_url} />
+              </div>
+            ) : (
+              <Link
+                href={`https://github.com/login/oauth/authorize?client_id=8543242e428085df968c&redirect_uri=https://app.watermelontools.com/github&state=${userEmail}&scope=repo%20user%20notifications`}
+              >
+                <a>Sign in with GitHub</a>
+              </Link>
+            )}
           </div>
           <div>
-            {userData?.organization ? (
+            {jiraUserData?.organization ? (
               <div>
-                <p> Logged into {userData.organization}</p>
-                <img src={userData.org_avatar_url} />
+                <p> Logged into {jiraUserData.organization}</p>
+                <img src={jiraUserData.org_avatar_url} />
                 <div>
-                  <img src={userData.user_avatar_url} />
-                  <p>Your Jira email:{userData.user_email} </p>
-                  <p>Your Jira name: {userData.user_displayname}</p>
+                  <img src={jiraUserData.user_avatar_url} />
+                  <p>Your Jira email:{jiraUserData.user_email} </p>
+                  <p>Your Jira name: {jiraUserData.user_displayname}</p>
                 </div>
               </div>
             ) : (
