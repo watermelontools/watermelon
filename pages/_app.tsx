c7e7db2c0c4292c3ea117f9574808c1b2dbcce2d
Esteban Dalel R <estebandalelr@gmail.com>
2022-09-10 15:03:23 -0700

    Add theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import { SessionProvider } from "next-auth/react";
-
+import { ThemeProvider } from "@primer/react";
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ThemeProvider colorMode="night">
+        <Component {...pageProps} />
+      </ThemeProvider>
     </SessionProvider>
   );
 }
