02af943895aefa0a073e40a39607d53345d132a3
Esteban Dalel R <estebandalelr@gmail.com>
2022-09-11 12:08:30 -0700

    Extract info getters

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import { useSession } from "next-auth/react";
-import Link from "next/link";
 import { useEffect, useState } from "react";
 import LogOutBtn from "../components/logout-btn";
 import LogInBtn from "../components/login-btn";
@@ -7,7 +6,8 @@ import GitHubInfo from "../components/githubInfo";
 import JiraInfo from "../components/jiraInfo";
 import JiraLoginLink from "../components/JiraLoginLink";
 import GitHubLoginLink from "../components/GitHubLoginLink";
-
+import getGitHubInfo from "../utils/api/getGitHubInfo";
+import getJiraInfo from "../utils/api/getJiraInfo";
 function HomePage({}) {
   const [userEmail, setUserEmail] = useState(null);
   const [jiraUserData, setJiraUserData] = useState(null);
@@ -17,35 +17,14 @@ function HomePage({}) {
     setUserEmail(session?.user?.email);
   }, [session]);
   useEffect(() => {
-    getJiraInfo();
-    getGitHubInfo();
+    getJiraInfo(userEmail).then((data) => {
+      setJiraUserData(data);
+    });
+    getGitHubInfo(userEmail).then((data) => {
+      setGithubUserData(data);
+    });
   }, [userEmail]);
-  async function getJiraInfo() {
-    const res = await fetch("/api/jira/getUser", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        user: userEmail,
-      }),
-    });
-    const data = await res.json();
-    setJiraUserData(data);
-  }
-  async function getGitHubInfo() {
-    const res = await fetch("/api/github/getUser", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        user: userEmail,
-      }),
-    });
-    const data = await res.json();
-    setGithubUserData(data);
-  }
+
   return (
     <div className="container" style={{ padding: "50px 0 100px 0" }}>
       {session ? <LogOutBtn /> : <LogInBtn />}
