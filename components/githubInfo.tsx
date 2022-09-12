4bdd8fa14cd3d40ed993f60f31716389add2f42e
Esteban Dalel R <estebandalelr@gmail.com>
2022-09-11 17:30:14 -0700

    Add styling to Main page

--- components/githubInfo.tsx
+++ components/githubInfo.tsx
@@ -1,9 +1,16 @@
 const GitHubInfo = (githubUserData) => (
-  <div>
-    <h1>GitHub</h1>
+  <div className="Box">
+    <div className="Subhead">
+      <h2 className="Subhead-heading">GitHub</h2>
+    </div>
     <h2>{githubUserData.name}</h2>
     <h3>{githubUserData.login}</h3>
-    <img src={githubUserData.avatar_url} />
+    <img
+      className="avatar"
+      src={githubUserData.avatar_url}
+      width="48"
+      height="48"
+    />
   </div>
 );
 export default GitHubInfo;
--- components/jiraInfo.tsx
+++ components/jiraInfo.tsx
@@ -1,9 +1,22 @@
 const JiraInfo = (jiraUserData) => (
-  <div>
+  <div className="Box">
+    <div className="Subhead">
+      <h2 className="Subhead-heading">Jira</h2>
+    </div>
     <p> Logged into {jiraUserData.organization}</p>
-    <img src={jiraUserData.org_avatar_url} />
+    <img
+      className="avatar"
+      src={jiraUserData.org_avatar_url}
+      width="48"
+      height="48"
+    />
     <div>
-      <img src={jiraUserData.user_avatar_url} />
+      <img
+        className="avatar"
+        src={jiraUserData.user_avatar_url}
+        width="48"
+        height="48"
+      />
       <p>Your Jira email:{jiraUserData.user_email} </p>
       <p>Your Jira name: {jiraUserData.user_displayname}</p>
     </div>
