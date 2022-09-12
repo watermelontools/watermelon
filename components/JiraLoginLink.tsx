f453037e51f4390dee33b23e47b81eab7c780a5d
Esteban Dalel R <estebandalelr@gmail.com>
2022-09-12 12:53:29 -0700

    Add box to login links

--- components/JiraLoginLink.tsx
+++ components/JiraLoginLink.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 const JiraLoginLink = (userEmail) => (
-  <div>
+  <div className="Box">
     <Link
       href={`https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=VUngRAClu8ZE56vxXCFBocTxCTLEUQTT&scope=read:jira-user%20read:jira-work%20write:jira-work%20offline_access&redirect_uri=https://app.watermelontools.com/jira&state=${userEmail}&response_type=code&prompt=consent`}
     >
