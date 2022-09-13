f86ac11d706bdf92de0cf23eb868c6996ee8e1e0
Esteban Dalel R <estebandalelr@gmail.com>
2022-09-13 14:16:27 -0700

    Make non available services grayscale

--- components/dashboard/ComingSoonService.tsx
+++ components/dashboard/ComingSoonService.tsx
@@ -1,6 +1,12 @@
 const JiraLoginLink = ({ name }) => (
   <div className="Box d-flex flex-items-center flex-justify-start p-2">
-    <img className="avatar avatar-8" src={`/logos/${name.toLowerCase()}.svg`} />
+    <img
+      className="avatar avatar-8"
+      src={`/logos/${name.toLowerCase()}.svg`}
+      style={{
+        filter: "grayscale(100%)",
+      }}
+    />
     <div className="p-2">
       <h2>{name}</h2>
       <p>Coming soon!</p>
