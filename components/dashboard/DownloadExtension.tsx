530d704c41b24ddee2f0a6daa8a8cdf20dac5e8d
Esteban Dalel R <estebandalelr@gmail.com>
2022-09-17 15:43:03 -0500

    Fix linking

--- components/dashboard/DownloadExtension.tsx
+++ components/dashboard/DownloadExtension.tsx
@@ -1,18 +1,21 @@
 import Link from "next/link";
 
 const DownloadExtension = ({ name, email, accessToken }) => (
-  <div className="Box d-flex flex-items-center flex-justify-start p-2">
-    <img className="avatar avatar-8" src={`/logos/${name.toLowerCase()}.svg`} />
-    <Link
-      href={`vscode://watermelontools.watermelon-tools?email=${
-        email ?? ""
-      }&token=${accessToken ?? ""}`}
-    >
+  <Link
+    href={`vscode://watermelontools.watermelon-tools?email=${
+      email ?? ""
+    }&token=${accessToken ?? ""}`}
+  >
+    <a className="Box d-flex flex-items-center flex-justify-start p-2">
+      <img
+        className="avatar avatar-8"
+        src={`/logos/${name.toLowerCase()}.svg`}
+      />
       <div className="p-2">
         <h2>{name}</h2>
         <p>Download Watermelon</p>
       </div>
-    </Link>
-  </div>
+    </a>
+  </Link>
 );
 export default DownloadExtension;
