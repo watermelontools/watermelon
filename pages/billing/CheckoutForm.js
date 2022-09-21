73ed52e4e6cc07a9e000b3c7ec5fea6f221160de
baristaGeek <estebanvargas94@gmail.com>
2022-09-21 17:53:59 -0500

    Build styled billing page and make it work with a hardcoded payment intent client secret

---
+++ pages/billing/CheckoutForm.js
@@ -0,0 +1,79 @@
+import React, {useEffect, useState} from 'react';
+import {useStripe, useElements, PaymentElement} from '@stripe/react-stripe-js';
+
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const [numberOfSeats, setNumberOfSeats] = useState(5);
+  const [subscriptionPrice, setSubscriptionPrice] = useState(50);
+  const [interval, setInterval] = useState('Monthly');
+
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    if (interval === 'Monthly') {
+      const calculatedPrice = numberOfSeats * 10;
+      setSubscriptionPrice(calculatedPrice);
+    } else if (interval === 'Yearly') {
+      const calculatedPrice = numberOfSeats * 10 * 12 * 0.8;
+      setSubscriptionPrice(calculatedPrice);
+    }
+  }, [numberOfSeats, interval]);
+
+  const handleSubmit = async (event) => {
+    // We don't want to let default form submission happen here,
+    // which would refresh the page.
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      // Stripe.js has not yet loaded.
+      // Make sure to disable form submission until Stripe.js has loaded.
+      return;
+    }
+
+    const {error} = await stripe.confirmPayment({
+      //`Elements` instance that was used to create the Payment Element
+      elements,
+      confirmParams: {
+        return_url: 'https://example.com/order/123/complete',
+      },
+    });
+
+
+    if (error) {
+      // This point will only be reached if there is an immediate error when
+      // confirming the payment. Show error to your customer (for example, payment
+      // details incomplete)
+      setErrorMessage(error.message);
+    } else {
+      // Your customer will be redirected to your `return_url`. For some payment
+      // methods like iDEAL, your customer will be redirected to an intermediate
+      // site first to authorize the payment, then redirected to the `return_url`.
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="number-of-seats">Number of seats</label>
+      <br />
+      <input required type="number" className="form-control mb-2 mr-2" id="exampleFormControlSelect1" placeholder="Number of seats" value={numberOfSeats} onChange={(e) => {setNumberOfSeats(e.target.value)}}/>
+      <select required className="form-control mb-2" id="exampleFormControlSelect1" value={interval} onChange={(e) => {setInterval(e.target.value)}}>
+        <option value="Monthly">Monthly</option>
+        <option value="Yearly">Yearly</option>
+      </select>
+      <br />
+      <p className="text-danger">{interval} subscription price: ${subscriptionPrice}</p>
+      <input required label="Number of seats" type="text" className="form-control mb-2 mr-2" id="exampleFormControlSelect1" placeholder="Admin First Name"/>
+      <input required label="Number of seats" type="text" className="form-control mb-2" id="exampleFormControlSelect1" placeholder="Admin Last Name"/>
+      <PaymentElement />
+      <br />
+      <div className="d-flex flex-items-center flex-justify-center">
+        <button className='btn btn-primary' type="submit" disabled={!stripe}>Purchase</button>
+      </div>
+      {errorMessage && <div>{errorMessage}</div>}
+    </form>
+  )
+};
+
+export default CheckoutForm;
---
+++ pages/billing/index.js
@@ -0,0 +1,61 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+// import CheckoutForm
+import CheckoutForm from "./CheckoutForm";
+// import paymentIntent from utils
+
+const stripePromise = loadStripe(
+  process.env.STRIPE_TEST_PUBLIC_KEY
+);
+
+function BillingPage() {
+  // const paymentIntentClientSecret = await getPaymentIntent("1000");
+
+  const options = {
+    // passing the client secret obtained from the server
+    clientSecret:
+      // TODO: get the client secret from the server,
+      clientSecretFromServer,
+
+    appearance: {
+      theme: "night",
+      labels: "floating",
+
+      variables: {
+        colorPrimary: "#79c0ff",
+        colorBackground: "#0d1117",
+        colorText: "#c9d1d9",
+        colorDanger: "#df1b41",
+        fontFamily: "Ideal Sans, system-ui, sans-serif",
+        fontSize: "14-px",
+        spacingUnit: "2px",
+        borderRadius: "4px",
+      },
+    },
+  };
+
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(350px, 1fr))",
+      }}
+    >
+      <div className="d-flex flex-items-center flex-justify-center flex-column">
+        <div
+          className="Box d-flex flex-items-center flex-justify-center flex-column p-4 p-6 m-6"
+          style={{ maxWidth: "80ch" }}
+        >
+          <h1 className="h3 mb-3 f4 text-normal">
+            Purchase your Watermelon subscription
+          </h1>
+          <Elements stripe={stripePromise} options={options}>
+            <CheckoutForm />
+          </Elements>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BillingPage;
