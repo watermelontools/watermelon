001b5235a4681321a4d573ee6d87202c0aefbeb4
baristaGeek <estebanvargas94@gmail.com>
2022-09-30 19:39:21 -0500

    Fix email destructuring

--- pages/api/payments/getByEmail.ts
+++ pages/api/payments/getByEmail.ts
@@ -2,9 +2,10 @@ import getByEmail from "../../../utils/db/payments/getByEmail";
 
 export default async function handler(req, res) {
   let { email } = req.body;
+  console.log("req.body: ", req.body)
   if (!email) {
     return res.send({ error: "no email" });
   }
-  let hasPaid = await getByEmail(email);
+  let hasPaid = await getByEmail({email});
   return res.send(hasPaid);
 }
