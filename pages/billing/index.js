1a973ff7c8382b697aaa3b243f3329f77e912e3e
baristaGeek <estebanvargas94@gmail.com>
2022-10-18 20:00:02 +0700

    Revert to correct behavior

--- pages/billing/index.js
+++ pages/billing/index.js
@@ -57,23 +57,22 @@ function BillingPage() {
         gridTemplateColumns: "repeat(auto-fit, minmax(350px, 1fr))",
       }}
     >
-      {/* render if component already mounted, if not app will crash*/}
-      {retrievedClientSecret && (
-        <Elements stripe={stripePromise} options={options}>
-          <div className="d-flex flex-items-center flex-justify-center flex-column">
-            <div
-              className="Box d-flex flex-items-center flex-justify-center flex-column p-4 p-6 m-6"
-              style={{ maxWidth: "80ch" }}
-            >
-              <h1 className="h3 mb-3 f4 text-normal">
-                Purchase your Watermelon subscription
-              </h1>
-
+      <div className="d-flex flex-items-center flex-justify-center flex-column">
+        <div
+          className="Box d-flex flex-items-center flex-justify-center flex-column p-4 p-6 m-6"
+          style={{ maxWidth: "80ch" }}
+        >
+          <h1 className="h3 mb-3 f4 text-normal">
+            Purchase your Watermelon subscription
+          </h1>
+          {/* render if component already mounted */}
+          {retrievedClientSecret && (
+            <Elements stripe={stripePromise} options={options}>
               <CheckoutForm />
-            </div>
-          </div>
-        </Elements>
-      )}
+            </Elements>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
