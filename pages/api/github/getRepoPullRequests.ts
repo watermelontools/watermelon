920d9ef60ae965bfc883ad5d298351698eec0afc
Esteban Dalel R <estebandalelr@gmail.com>
2022-10-20 15:46:00 -0500

    Create getRepoPullRequests.ts

---
+++ pages/api/github/getRepoPullRequests.ts
@@ -0,0 +1,24 @@
+import { Octokit } from "octokit";
+import getToken from "../../../utils/db/github/getToken";
+
+export default async function handler(req, res) {
+  let { user, owner, repo } = req.body;
+  if (!user) {
+    return res.send({ error: "no user" });
+  }
+  if (!repo) {
+    return res.send({ error: "no repo" });
+  }
+  if (!owner) {
+    return res.send({ error: "no owner" });
+  }
+  let { access_token } = await getToken(user);
+  const octokit = new Octokit({
+    auth: access_token,
+  });
+  let pullRequests = await octokit.rest.pulls.list({
+    owner,
+    repo,
+  });
+  return res.send(pullRequests.data);
+}
