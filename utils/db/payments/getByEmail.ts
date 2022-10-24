edec72789b447a97699b305127ed838bd9852a89
baristaGeek <estebanvargas94@gmail.com>
2022-10-24 15:54:55 +0700

    Remove console log

--- utils/db/payments/getByEmail.ts
+++ utils/db/payments/getByEmail.ts
@@ -8,7 +8,6 @@ export default async function getByEmail({
   try {
     let query = `EXEC dbo.get_payment_by_email @email='${email}'`;
     let resp = await executeRequest(query);
-    console.log("resp", resp);
     return resp;
   } catch (error) {
     console.error(error);
