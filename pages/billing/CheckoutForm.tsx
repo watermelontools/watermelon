d0cc9148805c156569221dbfbfbdb867963808e9
baristaGeek <estebanvargas94@gmail.com>
2022-10-26 17:56:36 +0700

    Remove comma

--- pages/billing/CheckoutForm.tsx
+++ pages/billing/CheckoutForm.tsx
@@ -20,7 +20,7 @@ const CheckoutForm = () => {
       //`Elements` instance that was used to create the Payment Element
       elements,
       confirmParams: {
-        return_url: "https://example.com/order/123/complete",
+        return_url: "https://example.com/order/123/complete"
       },
     });
 
