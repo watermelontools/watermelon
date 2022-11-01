c1fd553a1083deba304cb59958f22e6ea621bc88
Esteban Dalel R <estebandalelr@gmail.com>
2022-11-01 15:20:16 -0500

    Feature/comment on jira (#95)
    * Fix name of procedure
    
    * Added code to post comment to jira
    
    * Fixes to post on jira tickets
    
    * Update commentOnJiraTicket.ts
    
    * Remove returnval
    
    * Prettified
    
    * Fix error with Jira request
    
    * Remove unnecessary logging
    
    Co-authored-by: EstebanDalelR <EstebanDalelR@users.noreply.github.com>

---
+++ utils/jira/commentOnJiraTicket.ts
@@ -0,0 +1,53 @@
+export default async function handler({
+  issueIdOrKey,
+  text,
+  access_token,
+  cloudId,
+}: {
+  issueIdOrKey: string;
+  text: string;
+  access_token: string;
+  cloudId: string;
+}) {
+  if (!cloudId) {
+    return { error: "no cloudId" };
+  }
+  if (!access_token) {
+    return { error: "no access_token" };
+  }
+  let bodyToSend = JSON.stringify({
+    body: {
+      type: "doc",
+      version: 1,
+      content: [
+        {
+          type: "paragraph",
+          content: [
+            {
+              text: "🍉" + text,
+              type: "text",
+            },
+          ],
+        },
+      ],
+    },
+  });
+  try {
+    return await fetch(
+      `https://api.atlassian.com/ex/jira/${cloudId}/rest/api/3/issue/${issueIdOrKey}/comment`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept-Encoding": "deflate",
+          Accept: "application/json",
+          Authorization: `Bearer ${access_token}`,
+        },
+        body: bodyToSend,
+      }
+    ).then((res) => res.json());
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+}
