b2a035bfb7c0028cce267b6d30863e79c72579b0
baristaGeek <estebanvargas94@gmail.com>
2022-11-04 16:17:23 +0700

    Remove scopes

--- components/GitHubLoginLink.tsx
+++ components/GitHubLoginLink.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const GitHubLoginLink = ({ userEmail }) => (
   <div className="Box">
     <Link
-      href={`https://github.com/login/oauth/authorize?client_id=8543242e428085df968c&redirect_uri=https://app.watermelontools.com/github&state=${userEmail}&scope=repo%20user%20notifications`}
+      href={`https://github.com/login/oauth/authorize?client_id=8543242e428085df968c&redirect_uri=https://app.watermelontools.com/github&state=${userEmail}&scope=repo`}
     >
       <a className="button block">
         <div className="Box d-flex flex-items-center flex-justify-start p-2">
