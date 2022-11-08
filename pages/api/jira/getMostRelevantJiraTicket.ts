06a5265ba4d50fc74eebfebad1faef68af122286
baristaGeek <estebanvargas94@gmail.com>
2022-11-08 15:25:37 +0700

    Enhance JQL Query to find most relevant Jira ticket

--- pages/api/jira/getMostRelevantJiraTicket.ts
+++ pages/api/jira/getMostRelevantJiraTicket.ts
@@ -57,7 +57,9 @@ export default async function handler(req, res) {
         Authorization: `Bearer ${access_token}`,
       },
       body: JSON.stringify({
-        jql: `text ~ "${parsedPRTitle}"`,
+        // ORDER BY issuetype ASC gives priority to bug tickets. If there are no bug tickets, it will still return stories
+        // Sorting by description might be better than completely filtering out tickets without a description
+        jql: `text ~ "${parsedPRTitle}" AND issuetype in (Bug, Story) ORDER BY issuetype ASC, "Story Points" DESC, description DESC`,
         expand: ["renderedFields"],
       }),
     }
