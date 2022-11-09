ccba12d36984647da6dea052a2032f6c51a44b78
Esteban Dalel R <estebandalelr@gmail.com>
2022-11-09 18:21:15 -0500

    Create getToken.ts

---
+++ utils/db/slack/getToken.ts
@@ -0,0 +1,13 @@
+import executeRequest from "../azuredb";
+
+export default async function getToken({ user }): Promise<any> {
+  try {
+    let data = await executeRequest(
+      `EXEC dbo.get_slack_token @watermelon_user = '${user}'`
+    );
+    return data;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+}
