e3243a418791b302cccff4b0c9930b0b30c6616f
Esteban Dalel R <estebandalelr@gmail.com>
2022-11-10 10:55:36 -0500

    Add search scope

--- pages/index.tsx
+++ pages/index.tsx
@@ -112,7 +112,7 @@ function HomePage({}) {
               ))}
 
               <a
-                href={`https://slack.com/oauth/v2/authorize?client_id=2258283433764.3516691319939&scope=incoming-webhook,chat:write,chat:write.customize&user_scope=chat:write,users:read&state=${userEmail}`}
+                href={`https://slack.com/oauth/v2/authorize?client_id=2258283433764.3516691319939&scope=incoming-webhook,chat:write,chat:write.customize&user_scope=chat:write,users:read,search:read&state=${userEmail}`}
               >
                 Add to Slack
               </a>
