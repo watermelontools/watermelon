55824019073ed59d1c09baf4753d70428b56e7a2
baristaGeek <estebanvargas94@gmail.com>
2022-11-15 12:18:25 +0700

    Build getServerSideProps function

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,8 @@ import Header from "../components/Header";
 import DownloadExtension from "../components/dashboard/DownloadExtension";
 import getSlackInfo from "../utils/api/getSlackInfo";
 import SlackLoginLink from "../components/SlackLoginLink";
-function HomePage({}) {
+import getByEmail from "../utils/db/payments/getByEmail";
+function HomePage({hasPaidServerSide}) {
   const [userEmail, setUserEmail] = useState(null);
   const [jiraUserData, setJiraUserData] = useState(null);
   const [githubUserData, setGithubUserData] = useState(null);
@@ -92,7 +93,7 @@ function HomePage({}) {
                 {jiraUserData?.organization ? (
                   <JiraInfo {...jiraUserData} />
                 ) : (
-                  <JiraLoginLink userEmail={userEmail} hasPaid={hasPaid} />
+                  <JiraLoginLink userEmail={userEmail} hasPaid={hasPaidServerSide} />
                 )}
               </div>
               <div className="p-3">
@@ -107,7 +108,7 @@ function HomePage({}) {
                     }}
                   />
                 ) : (
-                  <SlackLoginLink userEmail={userEmail} hasPaid={hasPaid} />
+                  <SlackLoginLink userEmail={userEmail} hasPaid={hasPaidServerSide} />
                 )}
               </div>
               <div className="p-3">
@@ -135,6 +136,18 @@ function HomePage({}) {
       )}
     </div>
   );
+}
+
+// This gets called on every request
+export async function getServerSideProps() {
+  // Fetch data from external API
+  let hasPaidServerSide = await getByEmail({ email: "evargas@watermelon.tools"}).then((data) => {
+    if (data["email"]) {
+      return true;
+    }
+    return false;
+  });
+  return { props: { hasPaidServerSide } }
 }
 
 export default HomePage;
