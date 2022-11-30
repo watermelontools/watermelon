ba3f8d2013a5d83e849ee4e9215cbabac4f361fd
baristaGeek <estebanvargas94@gmail.com>
2022-11-30 19:07:13 +0700

    Modify stored procedures

--- utils/db/github/addEmailToGitHubQueryCountTable.ts
+++ utils/db/github/addEmailToGitHubQueryCountTable.ts
@@ -1,9 +1,9 @@
 import executeRequest from "../azuredb";
 
-export default async function addToCodeContextQueryCounter(userEmail): Promise<any> {
+export default async function addEmailToGitHubQueryCountTable(userEmail): Promise<any> {
   try {
     let data = await executeRequest(
-      `EXEC dbo.add_to_query_count @email = '${userEmail}'`
+      `EXEC dbo.add_email_to_github_query_count_table @email = '${userEmail}'`
     );
     return data;
   } catch (err) {
