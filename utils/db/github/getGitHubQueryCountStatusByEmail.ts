c98d87eed1cb49bf4f7405932203d47283c3b112
baristaGeek <estebanvargas94@gmail.com>
2022-12-05 13:37:26 +0700

    1. Limit usage or not based on query count and payment status

---
+++ utils/db/github/getGitHubQueryCountStatusByEmail.ts
@@ -0,0 +1,13 @@
+import executeRequest from "../azuredb";
+
+export default async function getGitHubQueryCountStatusByEmail(userEmail): Promise<any> {
+  try {
+    let data = await executeRequest(
+      `EXEC dbo.get_github_query_count_status_by_email @email = '${userEmail}'`
+    );
+    return data;
+  } catch (err) {
+    console.error(err);
+    return err;
+  }
+}
