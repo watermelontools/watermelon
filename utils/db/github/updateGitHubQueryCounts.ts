164af3dee89a272c676416db7bc8e9daddbc9642
baristaGeek <baristaGeek@users.noreply.github.com>
2022-12-06 05:11:06 +0000

    Prettified

--- utils/db/github/updateGitHubQueryCounts.ts
+++ utils/db/github/updateGitHubQueryCounts.ts
@@ -2,10 +2,8 @@ import executeRequest from "../azuredb";
 
 export default async function updateGitHubQueryCounts(): Promise<any> {
   try {
-    let data = await executeRequest(
-      `EXEC dbo.update_github_query_counts`
-    );
-    return {data};
+    let data = await executeRequest(`EXEC dbo.update_github_query_counts`);
+    return { data };
   } catch (err) {
     console.error(err);
     return err;
