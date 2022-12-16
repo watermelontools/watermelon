5cdf8d78eb13961816e36131ad384209af7396d2
baristaGeek <estebanvargas94@gmail.com>
2022-12-16 18:09:17 +0800

    Move Bitbucket auth logic from server side to front-end due to technical restrictions

--- pages/bitbucket.tsx
+++ pages/bitbucket.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import Router, { useRouter } from "next/router";
 import Link from "next/link";
 import saveUserInfo from "../utils/db/bitbucket/saveUser";
 import BitbucketLoginLink from "../components/BitbucketLoginLink";
@@ -13,6 +13,46 @@ export default function Bitbucket({ login, avatar_url, userEmail, error }) {
     const interval = setInterval(() => {
       setTimeToRedirect(timeToRedirect - 1);
       if (timeToRedirect === 0) {
+        // before redirecting, add user to DB
+
+        // First get access-token from window.location.hash
+        let retrievedAccessToken = window.location.hash.split("=")[1].split("&")[0];
+
+        // Second, fetch user data from the Bitbucket API
+        fetch('https://api.bitbucket.org/2.0/user', {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${retrievedAccessToken}`,
+            'Accept': 'application/json'
+          }
+        })
+          .then(response => {
+            console.log(
+              `Response: ${response.status} ${response.statusText}`
+            );
+            return response.text();
+          })
+          .then(text => console.log(text))
+          .catch(err => console.error(err));
+
+        // Third, call the create_bitbucket stored procedure
+        // await saveUserInfo({
+        //   access_token: "DNKhmncVIHChqVY5YG9mAQgpvLfbt2KLjS7xWfXyrt0MiXBbye8phEkvgGNVYIgcBS03qNMp4HgKOHGpdCXe-WG0vWwZwrcgWV6eWRfA1tFLYyquM1PAA5FuTCFv0muq0DJavb2n0Ia3DYgeNxXU5b4U5rw",
+        //   // access_token: json.access_token,
+        //   scope: json.scope,
+        //   login: userJson.login,
+        //   id: userJson.id,
+        //   avatar_url: userJson.avatar_url,
+        //   watermelon_user: context.query.state,
+        //   name: userJson.name,
+        //   company: userJson.company,
+        //   blog: userJson.blog,
+        //   email: userJson.email,
+        //   location: userJson.location,
+        //   bio: userJson.bio,
+        //   twitter_username: userJson.twitter_username,
+        // });
+
         router.push("/");
       }
     }, 1000);
@@ -66,76 +106,46 @@ export default function Bitbucket({ login, avatar_url, userEmail, error }) {
   );
 }
 
-export async function getServerSideProps(context) {
-  let f;
-  if (context.query.code) {
-    // This wouldn't be needed if we get the access_token from the url param
-    f = await fetch(`https://bitbucket.org/site/oauth2/access_token`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify({
-        grant_type: "authorization_code",
-        code: context.query.code,
-        redirect_uri: "https://app.watermelontools.com/bitbucket",
-        client_id: process.env.BITBUCKET_CLIENT_ID,
-        client_secret: process.env.BITBUCKET_CLIENT_SECRET,
-      }),
-    });
-    console.log("bitbucket access_token: ", f)
-  } else
-    return {
-      props: {
-        error: "no code",
-      },
-    };
-  const json = await f.json();
-  if (json.error) {
-    return {
-      props: {
-        error: json.error,
-      },
-    };
-  } else {
-    let user = await fetch(`https://api.bitbucket.org/2.0/user`, {
-      headers: {
-        Authorization: `Bearer ${json.access_token}`,
-        'Accept': 'application/json'
-      },
-    });
-    let userJson = await user.json();
-    console.log("bitbucket userJson: ", userJson);
+export async function getServerSideProps() {
+  // Here we had the old code that retrieved the access_token from the API of other services, but Bitbucket doesn't work like that. So we're using the code below instead.
 
-    // If this works, we would use location to get the access_token from the url param here
-    console.log("await save user info with hardcoded token called.")
-    // Via something like this 
-    // const searchParams = new URLSearchParams(window.location.hash);
-    // const accessToken = searchParams.get('access_token');
-    await saveUserInfo({
-      access_token: "DNKhmncVIHChqVY5YG9mAQgpvLfbt2KLjS7xWfXyrt0MiXBbye8phEkvgGNVYIgcBS03qNMp4HgKOHGpdCXe-WG0vWwZwrcgWV6eWRfA1tFLYyquM1PAA5FuTCFv0muq0DJavb2n0Ia3DYgeNxXU5b4U5rw",
-      // access_token: json.access_token,
-      scope: json.scope,
-      login: userJson.login,
-      id: userJson.id,
-      avatar_url: userJson.avatar_url,
-      watermelon_user: context.query.state,
-      name: userJson.name,
-      company: userJson.company,
-      blog: userJson.blog,
-      email: userJson.email,
-      location: userJson.location,
-      bio: userJson.bio,
-      twitter_username: userJson.twitter_username,
-    });
-    return {
-      props: {
-        loggedIn: true,
-        userEmail: context.query.state,
-        login: userJson.login,
-        avatar_url: userJson.avatar_url,
-      },
-    };
-  }
+    // Then, we should fetch the user data to get avatar_url, login, etc, and then send that to the create_bitbucket stored procedure
+    
+    // Call endpoint to retrieve user
+    // let user = await fetch(`https://api.bitbucket.org/2.0/user`, {
+    //   headers: {
+    //     Authorization: `Bearer ${json.access_token}`,
+    //     'Accept': 'application/json'
+    //   },
+    // });
+
+    // call user.json();
+    // let userJson = await user.json();
+
+    // Send it to the DB
+    // await saveUserInfo({
+    //   access_token: "DNKhmncVIHChqVY5YG9mAQgpvLfbt2KLjS7xWfXyrt0MiXBbye8phEkvgGNVYIgcBS03qNMp4HgKOHGpdCXe-WG0vWwZwrcgWV6eWRfA1tFLYyquM1PAA5FuTCFv0muq0DJavb2n0Ia3DYgeNxXU5b4U5rw",
+    //   // access_token: json.access_token,
+    //   scope: json.scope,
+    //   login: userJson.login,
+    //   id: userJson.id,
+    //   avatar_url: userJson.avatar_url,
+    //   watermelon_user: context.query.state,
+    //   name: userJson.name,
+    //   company: userJson.company,
+    //   blog: userJson.blog,
+    //   email: userJson.email,
+    //   location: userJson.location,
+    //   bio: userJson.bio,
+    //   twitter_username: userJson.twitter_username,
+    // });
+    // return {
+    //   props: {
+    //     loggedIn: true,
+    //     userEmail: context.query.state,
+    //     login: userJson.login,
+    //     avatar_url: userJson.avatar_url,
+    //   },
+    // };
+  
 }
