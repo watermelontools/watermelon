e07579f547ddd06bc9d96e9e6263c9f785bf0d46
Esteban Dalel R <estebandalelr@gmail.com>
2022-12-23 19:05:09 +0100

    Update GitLabLoginLink.tsx

--- components/GitLabLoginLink.tsx
+++ components/GitLabLoginLink.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const GitLabLoginLink = ({ userEmail }) => (
   <div>
     <Link
-      href={`https://gitlab.com/oauth/authorize?client_id=399843bda5477334e51f2aa88d96dfff694ce2da3130dabc685e34e1aa55b8f3&redirect_uri=https://app.watermelontools.com/gitlab&response_type=code&state=${userEmail}&scope=api,read_api,read_user,read_repository,write_repository,read_registry,openid,profile,email`}
+      href={`https://gitlab.com/oauth/authorize?client_id=399843bda5477334e51f2aa88d96dfff694ce2da3130dabc685e34e1aa55b8f3&redirect_uri=https://app.watermelontools.com/gitlab&response_type=code&state=${userEmail}&scope=api%20read_api%20read_user%20read_repository%20write_repository%20read_registry%20openid%20profile%20email`}
       className="button block"
     >
       <div className="Box d-flex flex-items-center flex-justify-start p-2">
