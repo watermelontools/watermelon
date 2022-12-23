ce808c736e93de8298b2c45803a6adcea24faad1
baristaGeek <baristaGeek@users.noreply.github.com>
2022-12-23 15:29:10 +0000

    Prettified

--- pages/api/bitbucket/getAllIssues.ts
+++ pages/api/bitbucket/getAllIssues.ts
@@ -1,4 +1,4 @@
-import  updateBitbucketAccessToken  from "../../../utils/bitbucket/updateBitbucketAccessToken";
+import updateBitbucketAccessToken from "../../../utils/bitbucket/updateBitbucketAccessToken";
 
 export default async function handler(req, res) {
   let { workspace, repo_slug, userEmail } = req.body;
