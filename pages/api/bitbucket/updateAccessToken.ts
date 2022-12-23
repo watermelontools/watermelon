174348a049baee8f781a4c03b85a0081d72e2d17
baristaGeek <estebanvargas94@gmail.com>
2022-12-23 18:44:47 +0800

    Build utils and endpoint to update bitbucket access token, all together

---
+++ pages/api/bitbucket/updateAccessToken.ts
@@ -0,0 +1,10 @@
+import updateBitbucketAccessToken from "../../../utils/db/bitbucket/updateBitbucketAccessToken";
+
+export default async function handler(req, res) {
+  let { userEmail } = req.body;
+  if (!userEmail) {
+    return res.send({ error: "no user email" });
+  }
+  let newAccessToken = await updateBitbucketAccessToken(userEmail);
+  return res.send(newAccessToken);
+}
