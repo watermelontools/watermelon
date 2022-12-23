c5dab7189d7b06e0eca20af4b49d26f8dbf46628
baristaGeek <baristaGeek@users.noreply.github.com>
2022-12-23 05:25:29 +0000

    Prettified

--- utils/bitbucket/getRefreshToken.ts
+++ utils/bitbucket/getRefreshToken.ts
@@ -1,19 +1,19 @@
 export default async ({ code }) => {
-    try {
-        let response = await fetch(
-        "https://bitbucket.org/site/oauth2/access_token",
-        {
-            method: "POST",
-            headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            },
-            body: `grant_type=authorization_code&code=${code}`,
-        }
-        );
-        let data = await response.json();
-        return data;
-    } catch (error) {
-        console.error(error);
-        return error;
-    }
+  try {
+    let response = await fetch(
+      "https://bitbucket.org/site/oauth2/access_token",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `grant_type=authorization_code&code=${code}`,
+      }
+    );
+    let data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
 };
