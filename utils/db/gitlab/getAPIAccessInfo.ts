772d814d490fe8ddd166364c430901ae217712c7
Esteban Dalel R <estebandalelr@gmail.com>
2022-12-26 17:14:45 -0500

    Remove logging

--- utils/db/gitlab/getAPIAccessInfo.ts
+++ utils/db/gitlab/getAPIAccessInfo.ts
@@ -4,11 +4,8 @@ export default async function getAPIAccessInfo(user: string): Promise<{
   refresh_token: string;
 }> {
   try {
-    console.log("getAPIAccessInfo");
     let query = `EXEC dbo.get_gitlab_tokens @user='${user}'`;
-    console.log("query", query);
     let resp = await executeRequest(query);
-    console.log("resp", resp);
     return resp;
   } catch (error) {
     console.error(error);
