7d490b01762082524bc6cab884b90fbb315f6876
baristaGeek <baristaGeek@users.noreply.github.com>
2022-12-27 14:03:43 +0000

    Prettified

--- pages/api/bitbucket/getPullRequestComments.ts
+++ pages/api/bitbucket/getPullRequestComments.ts
@@ -3,14 +3,14 @@ import updateBitbucketAccessToken from "../../../utils/bitbucket/updateBitbucket
 export default async function handler(req, res) {
   let { workspace, repo_slug, pull_request_id, userEmail } = req.body;
 
-  let access_token  = await updateBitbucketAccessToken(userEmail);
+  let access_token = await updateBitbucketAccessToken(userEmail);
 
   await fetch(
     `https://api.bitbucket.org/2.0/repositories/${workspace}/${repo_slug}/pullrequests/${pull_request_id}/comments`,
     {
       method: "GET",
       headers: {
-      Authorization: `Bearer ${access_token}`,
+        Authorization: `Bearer ${access_token}`,
         Accept: "application/json",
       },
     }
