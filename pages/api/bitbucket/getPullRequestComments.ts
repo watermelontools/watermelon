ec10a99108de5747134cce3f63eb0565b30a0e79
baristaGeek <estebanvargas94@gmail.com>
2022-12-28 17:38:20 +0800

    Refactor get pull request comments

--- pages/api/bitbucket/getPullRequestComments.ts
+++ pages/api/bitbucket/getPullRequestComments.ts
@@ -1,26 +1,33 @@
 import updateBitbucketAccessToken from "../../../utils/bitbucket/updateBitbucketAccessToken";
 
 export default async function handler(req, res) {
-  let { workspace, repo_slug, pull_request_id, userEmail } = req.body;
+  try {
+    const { workspace, repo_slug, pull_request_id, userEmail } = req.body;
 
-  let access_token = await updateBitbucketAccessToken(userEmail);
-
-  await fetch(
-    `https://api.bitbucket.org/2.0/repositories/${workspace}/${repo_slug}/pullrequests/${pull_request_id}/comments`,
-    {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${access_token}`,
-        Accept: "application/json",
-      },
+    if (!workspace) {
+      return res.send({ error: "no workspace" });
     }
-  )
-    .then((response) => {
-      response.json().then((data) => {
-        return res.send(data);
-      });
-    })
-    .catch((err) => {
-      return res.send(err);
-    });
+    if (!repo_slug) {
+      return res.send({ error: "no repo_slug" });
+    }
+    if (!pull_request_id) {
+      return res.send({ error: "no pull_request_id" });
+    }
+
+    const access_token = await updateBitbucketAccessToken(userEmail);
+    const response = await fetch(
+      `https://api.bitbucket.org/2.0/repositories/${workspace}/${repo_slug}/pullrequests/${pull_request_id}/comments`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+          Accept: "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    res.send(data);
+  } catch (err) {
+    res.send(err);
+  }
 }
