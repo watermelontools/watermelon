b85941c1eec774b82ec26495571748e0b29fbb72
Esteban Dalel R <estebandalelr@gmail.com>
2022-12-30 14:16:46 -0500

    Create getAssignedIssues.ts

---
+++ pages/api/gitlab/getAssignedIssues.ts
@@ -0,0 +1,27 @@
+import getAssignedIssues from "../../../utils/gitlab/getAssignedIssues";
+import getToken from "../../../utils/gitlab/refreshTokens";
+import getUser from "../../../utils/db/gitlab/getUser";
+export default async function handler(req, res) {
+  let { user, issue_iid, project_id } = req.body;
+  if (!user) {
+    return res.send({ error: "no user" });
+  }
+  if (!issue_iid) {
+    return res.send({ error: "no issue_number" });
+  }
+  if (!project_id) {
+    return res.send({ error: "no project_id" });
+  }
+  let { access_token } = await getToken({ user });
+  let { id } = await getUser(user);
+  try {
+    let issues = await getAssignedIssues({
+      access_token,
+      userId: id,
+      project_id,
+    });
+    return res.send(issues);
+  } catch (error) {
+    return res.send({ error });
+  }
+}
