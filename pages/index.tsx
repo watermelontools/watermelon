e021d35acf0a94ab2d787d99345c3d0254b22299
Esteban Dalel R <estebandalelr@gmail.com>
2023-01-04 11:17:16 -0500

    Use index to handle bitbucket

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,7 +42,7 @@ function HomePage({}) {
 
       getBitbucketInfo(userEmail).then((data) => {
         setBitbucketUserData(data);
-      })
+      });
       getGitLabInfo(userEmail).then((data) => {
         setGitlabUserData(data);
       });
@@ -121,7 +121,7 @@ function HomePage({}) {
                   <GitHubLoginLink userEmail={userEmail} />
                 )}
               </div>
-              
+
               <div className="p-3">
                 {bitbucketUserData?.name || bitbucketUserData?.email ? (
                   <BitbucketInfo {...bitbucketUserData} />
@@ -203,3 +203,55 @@ function HomePage({}) {
 }
 
 export default HomePage;
+
+export async function getServerSideProps(context) {
+  let f;
+  if (context.query.code) {
+    f = await fetch(`https://bitbucket.org/site/oauth2/access_token`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `grant_type=authorization_code&code=${context.query.code}&redirect_uri=https://app.watermelontools.com/bitbucket&client_id=${process.env.BITBUCKET_CLIENT_ID}&client_secret=${process.env.BITBUCKET_CLIENT_SECRET}`,
+    });
+  } else
+    return {
+      props: {
+        error: "no code",
+      },
+    };
+  const json = await f.json();
+  console.log(json);
+  if (json.error) {
+    return {
+      props: {
+        error: json.error,
+      },
+    };
+  } else {
+    let user = await fetch(`https://api.bitbucket.org/2.0/user`, {
+      headers: {
+        Authorization: `Bearer ${json.access_token}`,
+      },
+    });
+    console.log(user);
+    let userJson = await user.json();
+    /*     await saveUserInfo({
+      access_token: json.access_token,
+      refresh_token: json.refresh_token,
+      id: userJson.id,
+      avatar_url: userJson.avatar_url,
+      watermelon_user: context.query.state,
+      name: userJson.name,
+      location: userJson.location,
+    }); */
+    return {
+      props: {
+        loggedIn: true,
+        userEmail: context.query.state,
+        login: userJson.username,
+        avatar_url: userJson.avatar_url,
+      },
+    };
+  }
+}
