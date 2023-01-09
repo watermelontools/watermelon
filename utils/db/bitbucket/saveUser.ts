4c7ad69afe9f849f8260284a1dfe692cbc521736
Esteban Dalel R <estebandalelr@gmail.com>
2023-01-09 12:15:14 -0500

    fix query

--- utils/db/bitbucket/saveUser.ts
+++ utils/db/bitbucket/saveUser.ts
@@ -10,7 +10,7 @@ export default async ({
   refresh_token,
 }) => {
   let query = `EXEC dbo.create_bitbucket @watermelon_user='${watermelon_user}', @id='${id}', 
- @avatar_url='${avatar_url}', @name='${name}', @location='${location}', @access_token='${access_token}', @refresh_token='${refresh_token};
+ @avatar_url='${avatar_url}', @name='${name}', @location='${location}', @access_token='${access_token}', @refresh_token='${refresh_token}';
  `;
   let resp = await executeRequest(query);
   return resp;
