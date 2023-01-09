905aa81ea6ad84c8f1e168e8f5748c09f9ce30b2
Esteban Dalel R <estebandalelr@gmail.com>
2023-01-09 12:06:19 -0500

    pass state

--- components/BitbucketLoginLink.tsx
+++ components/BitbucketLoginLink.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 const BitbucketLoginLink = ({ userEmail }) => (
   <div className="Box">
     <Link
-      href={`https://bitbucket.org/site/oauth2/authorize?client_id=qvxNH4EGH4sjZysZAu&response_type=code`}
-      className="button block">
-
+      href={`https://bitbucket.org/site/oauth2/authorize?client_id=qvxNH4EGH4sjZysZAu&response_type=code/state=${userEmail}`}
+      className="button block"
+    >
       <div className="Box d-flex flex-items-center flex-justify-start p-2">
         <img className="avatar avatar-8" src="/logos/bitbucket.svg" />
         <div className="p-2">
@@ -13,7 +13,6 @@ const BitbucketLoginLink = ({ userEmail }) => (
           <p>View your Assigned Issues and Relevant Pull Requests</p>
         </div>
       </div>
-
     </Link>
   </div>
 );
