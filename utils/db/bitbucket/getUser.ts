87de8b452e97b7f8679d4ff73ed5bfcf9d223510
Esteban Dalel R <estebandalelr@gmail.com>
2023-01-09 18:33:01 -0500

    Fix SQL command

--- utils/db/bitbucket/getUser.ts
+++ utils/db/bitbucket/getUser.ts
@@ -2,7 +2,9 @@ import executeRequest from "../azuredb";
 
 export default async function getUser(email): Promise<any> {
   try {
-    let data = await executeRequest(`EXEC dbo.get_bitbucket_user @email = '${email}'`);
+    let data = await executeRequest(
+      `EXEC dbo.get_bitbucket_user @watermelon_user = '${email}'`
+    );
     return data;
   } catch (err) {
     console.error(err);
