d917c00ed950b83b82c65808321c52c766a91059
baristaGeek <estebanvargas94@gmail.com>
2023-01-11 15:29:12 -0500

    Update git query count on get issues by commits

--- pages/api/github/getIssuesByCommits.ts
+++ pages/api/github/getIssuesByCommits.ts
@@ -1,5 +1,7 @@
 import { Octokit } from "octokit";
 import getToken from "../../../utils/db/github/getToken";
+import addToGitHubQueryCount from "../../../utils/db/github/addToGitHubQueryCount";
+import getGitHubQueryCountStatusByEmail from "../../../utils/db/github/getGitHubQueryCountStatusByEmail";
 
 export default async function handler(req, res) {
   let { user, repo, owner, commitList } = req.body;
@@ -20,12 +22,23 @@ export default async function handler(req, res) {
     auth: access_token,
   });
   let q = `repo:${owner}/${repo}+hash:${commitList}`;
+
+  // if the github query count for the user with that email address is over 50 and the user hasn't paid, return an error
+  let { hasPaid, git_query_count } = await getGitHubQueryCountStatusByEmail(
+    user
+  );
+  if (git_query_count > 50 && !hasPaid) {
+    return res.send({ error: "GitHub query limit reached" });
+  }
+
   try {
     let issues = await octokit.rest.search.issuesAndPullRequests({
       q,
       is: "pr",
       type: "pr",
     });
+
+    addToGitHubQueryCount(user);
 
     return res.send(issues.data);
   } catch (error) {
