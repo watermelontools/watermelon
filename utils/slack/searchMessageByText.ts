e92a3842927ac7e3087cfcf02bc2bc254d7bf47e
Esteban Dalel R <estebandalelr@gmail.com>
2023-01-20 11:34:35 -0500

    Add user info to slack messages search

--- utils/slack/searchMessageByText.ts
+++ utils/slack/searchMessageByText.ts
@@ -13,15 +13,44 @@ export default async function handler({
   }
 
   try {
-    return await fetch(`https://slack.com/api/search.messages?query=${text}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json; charset=utf-8",
-        "Accept-Encoding": "deflate",
-        Accept: "application/json",
-        Authorization: `Bearer ${user_token}`,
-      },
-    }).then((res) => res.json());
+    const foundMessages = await fetch(
+      `https://slack.com/api/search.messages?query=${text}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          "Accept-Encoding": "deflate",
+          Accept: "application/json",
+          Authorization: `Bearer ${user_token}`,
+        },
+      }
+    ).then((res) => res.json());
+
+    //map to promises the matched messages
+    // and fetch the user info for each message
+    const promises = foundMessages.messages.matches.map(
+      async (element, index) => {
+        return await fetch(
+          `https://slack.com/api/users.info?user=${element.user}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json; charset=utf-8",
+              "Accept-Encoding": "deflate",
+              Accept: "application/json",
+              Authorization: `Bearer ${user_token}`,
+            },
+          }
+        )
+          .then((res) => res.json())
+          .then((json) => {
+            foundMessages.messages.matches[index].userInfo = json;
+          });
+      }
+    );
+    await Promise.allSettled(promises);
+
+    return foundMessages;
   } catch (error) {
     console.error(error);
     return error;
