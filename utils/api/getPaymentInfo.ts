642eb0be1b5f8bf52c6c9af2bf6c157f79c0311d
Esteban Dalel R <estebandalelr@gmail.com>
2023-01-26 17:12:50 -0500

    Fix getting email

--- utils/api/getPaymentInfo.ts
+++ utils/api/getPaymentInfo.ts
@@ -1,11 +1,11 @@
-const getPaymentInfo = async (userEmail: string) => {
+const getPaymentInfo = async (email: string) => {
   const data = await fetch("/api/payments/getByEmail", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      user: userEmail,
+      email,
     }),
   })
     .then((res) => res.json())
