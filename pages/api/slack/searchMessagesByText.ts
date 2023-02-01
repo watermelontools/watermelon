f02ac427665c386b514c3219ed11551e785e50f7
Esteban Dalel R <estebandalelr@gmail.com>
2023-02-01 09:02:23 -0500

    Feature/get slack replies  (#140)
    * Remove logging
    
    * get replies on backend
    
    * Fix naming

--- pages/api/slack/searchMessagesByText.ts
+++ pages/api/slack/searchMessagesByText.ts
@@ -1,5 +1,6 @@
 import searchMessageByText from "../../../utils/slack/searchMessageByText";
 import getToken from "../../../utils/db/slack/getToken";
+import getConversationReplies from "../../../utils/slack/getConversationReplies";
 export default async function handler(req, res) {
   let { user, text } = req.body;
 
@@ -17,6 +18,15 @@ export default async function handler(req, res) {
     text,
     user_token,
   });
-
+  let repliesPromises = response.messages.matches.map(async (match, index) => {
+    response.messages.matches[index].comments = [];
+    let replies = await getConversationReplies({
+      ts: match.ts,
+      user_token: match.user_token,
+      channelId: match.channel.id,
+    });
+    response.messages.matches[index].replies.push(...replies.messages);
+  });
+  await Promise.allSettled(repliesPromises);
   return res.send(response);
 }
--- utils/db/slack/saveUserInfo.ts
+++ utils/db/slack/saveUserInfo.ts
@@ -21,6 +21,5 @@ export default async ({
 }) => {
   let query = `EXEC dbo.create_slack @user_token = '${user_token}', @bot_token = '${bot_token}', @bot_user_id = '${bot_user_id}', @user_id = '${user_id}', @team_id = '${team_id}', @team_name = '${team_name}', @bot_scopes = '${bot_scopes}', @user_scopes = '${user_scopes}', @incoming_webhook_channel_id = '${incoming_webhook_channel_id}', @watermelon_user = '${watermelon_user}', @incoming_webhook_configuration_url = '${incoming_webhook_configuration_url}', @incoming_webhook_url = '${incoming_webhook_url}', @is_enterprise_install = '${is_enterprise_install}', @user_username = '${user_username}', @user_title = '${user_title}', @user_real_name = '${user_real_name}', @user_picture_url = '${user_picture_url}'`;
   let resp = await executeRequest(query);
-  console.log("resp", resp);
   return resp;
 };
