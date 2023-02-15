ae2442a4db110ca9243210e928711745414692d3
Esteban Dalel R <estebandalelr@gmail.com>
2023-02-15 12:07:51 -0500

    Extract URL

--- pages/vscodium.tsx
+++ pages/vscodium.tsx
@@ -3,36 +3,32 @@ import { useSession, signIn } from "next-auth/react";
 import { useEffect, useState } from "react";
 
 function VSCodeInsiders() {
-  const [timeToRedirect, setTimeToRedirect] = useState(10);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTimeToRedirect(timeToRedirect - 1);
-      if (timeToRedirect === 0) {
-        window.open(
-          `vscodium://watermelontools.watermelon-tools?email=${
-            data?.user?.email ?? ""
-          }&token=${data?.user.name ?? ""}`,
-          "_blank"
-        );
-      }
-    }, 1000);
-    return () => clearInterval(interval);
-  }, [timeToRedirect]);
   const { status, data } = useSession({
     required: true,
     onUnauthenticated() {
       signIn();
     },
   });
+  const [timeToRedirect, setTimeToRedirect] = useState(10);
+  let system = "vscode";
+  let url: string = `${system}://watermelontools.watermelon-tools?email=${
+    data?.user?.email ?? ""
+  }&token=${data?.user.name ?? ""}`;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeToRedirect(timeToRedirect - 1);
+      if (timeToRedirect === 0) {
+        window.open(url, "_blank");
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [timeToRedirect]);
+
   return (
     <div className="d-flex flex-items-center flex-justify-center">
       {status !== "loading" && (
-        <Link
-          href={`vscodium://watermelontools.watermelon-tools?email=${
-            data?.user?.email ?? ""
-          }&token=${data?.user.name ?? ""}`}
-        >
+        <Link href={url}>
           <div className="d-flex flex-items-center flex-justify-center flex-column">
             <div
               className="Box d-flex flex-items-center flex-justify-center flex-column p-4 p-4 m-2"
