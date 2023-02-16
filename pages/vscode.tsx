7f0eb2187524c9339c2dc29919dc5fa6f283ae84
Esteban Dalel R <estebandalelr@gmail.com>
2023-02-16 09:37:37 -0500

    Feature/automatic vs redirect (#145)
    * Add comments to issue fetching
    
    * Add redirect timer
    
    * Extract URL
    
    * Change redirect text
    
    * Fix negative times

--- pages/vscode.tsx
+++ pages/vscode.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useSession, signIn } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 function VSCode() {
   const { status, data } = useSession({
@@ -8,21 +9,35 @@ function VSCode() {
       signIn();
     },
   });
+  const [timeToRedirect, setTimeToRedirect] = useState(10);
+  let system = "vscode";
+  let url: string = `${system}://watermelontools.watermelon-tools?email=${
+    data?.user?.email ?? ""
+  }&token=${data?.user.name ?? ""}`;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeToRedirect(timeToRedirect - 1);
+      if (timeToRedirect === 0) {
+        window.open(url, "_blank");
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [timeToRedirect]);
 
   return (
     <div className="d-flex flex-items-center flex-justify-center">
       {status !== "loading" && (
-        <Link
-          href={`vscode://watermelontools.watermelon-tools?email=${
-            data?.user?.email ?? ""
-          }&token=${data?.user.name ?? ""}`}
-        >
+        <Link href={url}>
           <div className="d-flex flex-items-center flex-justify-center flex-column">
             <div
               className="Box d-flex flex-items-center flex-justify-center flex-column p-4 p-4 m-2"
               style={{ maxWidth: "80ch" }}
             >
               <h1 className="h3 mb-3 f4 text-normal">Open VSCode</h1>
+              {timeToRedirect > 0 ? (
+                <p>We will try opening it in {timeToRedirect}...</p>
+              ) : null}{" "}
             </div>
           </div>
         </Link>
