6cb53249282818a34bf26b4a64b133a4177acd71
Esteban Dalel R <estebandalelr@gmail.com>
2023-04-04 16:58:20 -0500

    Use title to search

--- utils/actions/getSlack.ts
+++ utils/actions/getSlack.ts
@@ -1,12 +1,12 @@
 import searchMessageByText from "../../utils/slack/searchMessageByText";
 async function getSlack({ title, body, slack_token, randomWords }) {
   let slackValue = {};
+
   if (slack_token) {
     let response = await searchMessageByText({
-      text: `${randomWords.toString()} OR ${title} OR  ${body}`,
+      text: `${title}`,
       user_token: slack_token,
     });
-
     slackValue = response?.messages?.matches.slice(0, 3);
   } else {
     slackValue = { error: "no slack token" };
