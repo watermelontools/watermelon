c8463410f6e6f05f5fae6ceb18d834ab89a60791
baristaGeek <estebanvargas94@gmail.com>
2023-04-13 14:23:18 -0500

    Setup tests

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+};
+
---
+++ server.ts
@@ -0,0 +1,18 @@
+import next from 'next';
+import http from 'http';
+import { parse } from 'url';
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+export default async function createTestServer() {
+  await app.prepare();
+  const server = http.createServer((req, res) => {
+    const parsedUrl = parse(req.url!, true);
+    handle(req, res, parsedUrl);
+  });
+
+  return server;
+}
+
