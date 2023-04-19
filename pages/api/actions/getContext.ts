bc06ed80648f235761a08963830dfb41d0c14081
Esteban Dalel R <estebandalelr@gmail.com>
2023-04-19 14:45:43 -0500

    Remove unused import

--- pages/api/actions/getContext.ts
+++ pages/api/actions/getContext.ts
@@ -5,12 +5,6 @@ import getJira from "../../../utils/actions/getJira";
 import getOpenAISummary from "../../../utils/actions/getOpenAISummary";
 import { trackEvent } from "../../../utils/analytics/azureAppInsights";
 
-const { Configuration, OpenAIApi } = require("openai");
-const configuration = new Configuration({
-  apiKey: process.env.OPEN_AI_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
 export default async function handler(req, res) {
   const { user, title, body, repo, owner, number, commitList } = req.body;
   trackEvent({
