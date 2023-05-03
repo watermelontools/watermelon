aee54d2c8c8e02c750dfd5ec999c32021713f207
Esteban Dalel R <estebandalelr@gmail.com>
2023-05-03 13:40:06 -0500

    test without webhooks package

--- pages/api/actions/github.ts
+++ pages/api/actions/github.ts
@@ -1,31 +1,8 @@
-import { Webhooks } from "@octokit/webhooks";
 import { Octokit } from "@octokit/rest";
-
-const webhooks = new Webhooks({
-  secret: process.env.WEBHOOK_SECRET,
-});
 
 const octokit = new Octokit({
   appId: process.env.GITHUB_APP_ID,
   privateKey: process.env.GITHUB_PRIVATE_KEY,
-});
-
-// Handle the "pull_request" event
-webhooks.on("pull_request", async ({ payload }) => {
-  console.log("pull_request", payload.repository);
-  if (payload.action === "opened") {
-    const { repository, pull_request } = payload;
-    const comment = {
-      owner: repository.owner.login,
-      repo: repository.name,
-      //@ts-ignore
-      issue_number: pull_request.number,
-      body: "Thank you for your pull request! We will review it shortly.",
-    };
-
-    // Add a comment to the pull request
-    await octokit.rest.issues.createComment(comment);
-  }
 });
 
 export default async (req, res) => {
@@ -33,11 +10,20 @@ export default async (req, res) => {
     try {
       // Verify and parse the webhook event
       const eventName = req.headers["x-github-event"];
-      await webhooks.receive({
-        id: req.headers["x-github-delivery"],
-        name: eventName,
-        payload: req.body,
-      });
+      let { payload } = req.body;
+      if (payload.action === "opened") {
+        const { repository, pull_request } = payload;
+        const comment = {
+          owner: repository.owner.login,
+          repo: repository.name,
+          //@ts-ignore
+          issue_number: pull_request.number,
+          body: "Thank you for your pull request! We will review it shortly.",
+        };
+
+        // Add a comment to the pull request
+        await octokit.rest.issues.createComment(comment);
+      }
 
       res.status(200).send("Webhook event processed");
     } catch (error) {
