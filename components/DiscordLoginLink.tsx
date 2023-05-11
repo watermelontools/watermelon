aaa9324f5eb01002c32cc86ef66c9c425bbabcc8
Esteban Dalel R <estebandalelr@gmail.com>
2023-05-11 11:42:06 -0400

    Update DiscordLoginLink.tsx

--- components/DiscordLoginLink.tsx
+++ components/DiscordLoginLink.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const DiscordLoginLink = ({ userEmail }) => (
   <div className="Box">
     <Link
-      href={`https://discord.com/api/oauth2/authorize?client_id=1105902302839189514&permissions=26783953042448&redirect_uri=https%3A%2F%2Fapp.watermelontools.com%2Fdiscord&response_type=code&scope=identify%20email%20webhook.incoming%20relationships.read%20messages.read%20bot%20connections%20dm_channels.read&state=${userEmail}`}
+      href={`https://discord.com/oauth2/authorize?client_id=1105902302839189514&redirect_uri=https%3A%2F%2Fapp.watermelontools.com%2Fdiscord&response_type=code&scope=identify%20email%20messages.read&state=${userEmail}`}
       className="button block"
     >
       <div className="Box d-flex flex-items-center flex-justify-start p-2">
