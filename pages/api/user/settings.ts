435cdc8de3173d62345b475ebc125efb3d048e80
Esteban Dalel R <estebandalelr@gmail.com>
2023-05-16 11:39:18 -0400

    patch settings on save

--- pages/api/user/settings.ts
+++ pages/api/user/settings.ts
@@ -1,9 +1,17 @@
 import getUserSettings from "../../../utils/db/user/settings";
+import patchUserSettings from "../../../utils/db/user/patchUserSettings";
 
 export default async function handler(req, res) {
   let { user } = req.body;
   if (!user) {
     return res.send({ error: "no user" });
+  }
+  if (res.method === "PATCH") {
+    let { settings } = req.body;
+    if (!settings) {
+      return res.send({ error: "no settings" });
+    }
+    return await patchUserSettings(user, settings);
   }
   let dbUser = await getUserSettings(user);
   return res.send(dbUser);
--- pages/settings.tsx
+++ pages/settings.tsx
@@ -20,6 +20,32 @@ function HomePage({}) {
     setUserEmail(session?.user?.email);
     setUserSettingsState(session?.user?.email);
   }, [session]);
+  const [formState, setFormState] = useState({
+    JiraTickets: userSettings?.JiraTickets || 3,
+    SlackMessages: userSettings?.SlackMessages || 3,
+    GitHubPRs: userSettings?.GitHubPRs || 3,
+    AISummary: userSettings?.AISummary || 3,
+  });
+
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch("/api/user/settings", {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formState),
+      });
+
+      if (response.ok) {
+        console.log("Form saved successfully");
+      } else {
+        console.error("Failed to save the form");
+      }
+    } catch (error) {
+      console.error("An error occurred while saving the form", error);
+    }
+  };
 
   return (
     <div>
@@ -28,7 +54,109 @@ function HomePage({}) {
       {status === "authenticated" && (
         <>
           {session ? <Header /> : <LogInBtn />}
-          {userEmail ? <></> : null}
+          {userEmail ? (
+            <div className="p-3">
+              <h2>Settings</h2>
+              <div className="">
+                <div className="Subhead">
+                  <span className="Subhead-heading">Action Settings</span>
+                  <div className="Subhead-description">
+                    This controls how the action behaves.
+                  </div>
+                </div>
+                //form goes here
+                <form>
+                  <div className="">
+                    <span>Jira Tickets: </span>
+                    <select
+                      className="form-select"
+                      aria-label="Amount of Jira Tickets"
+                      defaultValue={userSettings?.JiraTickets}
+                      onChange={(e) =>
+                        setFormState({
+                          ...formState,
+                          JiraTickets: e.target.value,
+                        })
+                      }
+                      value={formState.JiraTickets}
+                    >
+                      {Array.from(Array(11)).map((i, index) => {
+                        if (index === 0) {
+                          return null;
+                        }
+                        return <option value={index}>{index}</option>;
+                      })}
+                    </select>
+                  </div>
+                  <div className="">
+                    <span>Slack Messages: </span>
+                    <select
+                      className="form-select"
+                      aria-label="Amount of Slack Messages"
+                      defaultValue={userSettings?.SlackMessages}
+                      value={formState.SlackMessages}
+                      onChange={(e) =>
+                        setFormState({
+                          ...formState,
+                          SlackMessages: e.target.value,
+                        })
+                      }
+                    >
+                      {Array.from(Array(11)).map((i, index) => {
+                        if (index === 0) {
+                          return null;
+                        }
+                        return <option value={index}>{index}</option>;
+                      })}
+                    </select>
+                  </div>
+                  <div className="">
+                    <span>GitHub PRs: </span>
+                    <select
+                      className="form-select"
+                      aria-label="Amount of GitHub PRs"
+                      defaultValue={userSettings?.GitHubPRs}
+                      value={formState.GitHubPRs}
+                      onChange={(e) =>
+                        setFormState({
+                          ...formState,
+                          GitHubPRs: e.target.value,
+                        })
+                      }
+                    >
+                      {Array.from(Array(11)).map((i, index) => {
+                        if (index === 0) {
+                          return null;
+                        }
+                        return <option value={index}>{index}</option>;
+                      })}
+                    </select>
+                  </div>
+                  <div className="">
+                    <span>AI Summary: </span>
+                    <select
+                      className="form-select"
+                      aria-label="AI Summary"
+                      defaultValue={userSettings?.AISummary}
+                      value={formState.AISummary}
+                      onChange={(e) =>
+                        setFormState({
+                          ...formState,
+                          AISummary: e.target.value,
+                        })
+                      }
+                    >
+                      <option value={1}>Active</option>;
+                      <option value={0}>Inactive</option>;
+                    </select>
+                  </div>
+                  <button className="btn btn-primary" type="button">
+                    Save
+                  </button>
+                </form>
+              </div>
+            </div>
+          ) : null}
         </>
       )}
     </div>
