baaa63f72f5b9b0447e0c218efa3c5c4f2843808
Esteban Dalel R <estebandalelr@gmail.com>
2023-05-17 15:29:14 -0400

    reduce scopes

--- components/SlackLoginLink.tsx
+++ components/SlackLoginLink.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const SlackLoginLink = ({ userEmail }) => (
   <div>
     <Link
-      href={`https://slack.com/oauth/v2/authorize?client_id=2258283433764.3516691319939&scope=chat:write,chat:write.customize,incoming-webhook,channels:history,groups:history,im:history,mpim:history,users:read&user_scope=chat:write,channels:history,groups:history,im:history,mpim:history,identify,search:read,users:read&state=${userEmail}`}
+      href={`https://slack.com/oauth/v2/authorize?client_id=2258283433764.3516691319939&scope=chat:write,chat:write.customize,incoming-webhook,channels:history,groups:history,users:read&user_scope=chat:write,channels:history,groups:history,identify,search:read,users:read&state=${userEmail}`}
       className="button block"
     >
       <div className="Box d-flex flex-items-center flex-justify-start p-2">
