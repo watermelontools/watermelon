6a5394d1511b8f0c062c4ce664a6500e6431dc0c
Esteban Dalel R <estebandalelr@gmail.com>
2023-05-22 15:24:21 -0400

    Fix query

--- utils/db/teams/addActionCount.ts
+++ utils/db/teams/addActionCount.ts
@@ -2,7 +2,7 @@ import executeRequest from "../azuredb";
 
 export default async ({ watermelon_user }) => {
   let query = await executeRequest(
-    `EXEC dbo.increment_github_app_uses @watermelon_user = '${watermelon_user}`
+    `EXEC dbo.increment_github_app_uses @watermelon_user = '${watermelon_user}'`
   );
   return query;
 };
