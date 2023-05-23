17b927ae6bca160c06797e02db3d5fc15f42133d
Esteban Dalel R <estebandalelr@gmail.com>
2023-05-22 20:44:07 -0400

    Add logo to login

--- components/login-btn.tsx
+++ components/login-btn.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { signIn } from "next-auth/react";
 export default function LogInBtn() {
   return (
@@ -9,6 +10,12 @@ export default function LogInBtn() {
         className="Box d-flex flex-items-center flex-justify-center flex-column p-4 p-4 m-2"
         style={{ maxWidth: "80ch" }}
       >
+        <Image
+          src="/logos/watermelon.png"
+          alt="Watermelon Tools"
+          width="36"
+          height="23"
+        />
         <h1 className="h3 mb-3 f4 text-normal">Watermelon Auth</h1>
         <p className="text-gray mb-4">
           Sign in to Watermelon Auth to access all services
