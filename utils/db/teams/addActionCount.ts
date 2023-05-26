42c2f51c20bc0cc205a4f24acf0a24278dd5bea1
Esteban Dalel R <estebandalelr@gmail.com>
2023-05-26 16:52:51 -0600

    add logging

--- utils/db/teams/addActionCount.ts
+++ utils/db/teams/addActionCount.ts
@@ -1,6 +1,7 @@
 import executeRequest from "../azuredb";
 
 export default async ({ watermelon_user }) => {
+  console.log("watermelon_user", watermelon_user);
   let query = await executeRequest(
     `EXEC dbo.increment_github_app_uses @watermelon_user = '${watermelon_user}'`
   );
