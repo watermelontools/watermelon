92804dabfd3a6e23e04b968f650d7ef305897798
Esteban Dalel R <estebandalelr@gmail.com>
2023-05-25 18:50:43 -0600

    Add notion querying

---
+++ utils/actions/getNotion.ts
@@ -0,0 +1,38 @@
+function removeSpecialChars(inputString) {
+  const specialChars = '!"#$%&/()=-_"{}¨*[]'; // Edit this list to include or exclude characters
+  return inputString
+    .split("")
+    .filter((char) => !specialChars.includes(char))
+    .join("");
+}
+
+async function getNotion({ notion_token, randomWords, amount = 3 }) {
+  if (!notion_token) {
+    return { error: "no notion token" };
+  } else {
+    let cleanRW = Array.from(
+      new Set(randomWords.map((word) => removeSpecialChars(word)))
+    );
+
+    let returnVal = await fetch(`https://api.notion.com/v1/search`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${notion_token}`,
+      },
+      body: JSON.stringify({
+        query: cleanRW.join(" "),
+      }),
+    })
+      .then((res) => res.json())
+      .then((resJson) => resJson.issues)
+      .catch((error) => {
+        console.error(error);
+      });
+
+    console.log(returnVal);
+    return returnVal?.slice(0, amount);
+  }
+}
+export default getNotion;
