e27f9331064cb6a95825b35fc6fa881145fe4378
Esteban Dalel R <estebandalelr@gmail.com>
2023-06-07 12:33:15 -0500

    Scrub action for app

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -59,9 +59,9 @@ function HomePage({}) {
               <h2>Settings</h2>
               <div className="">
                 <div className="Subhead">
-                  <span className="Subhead-heading">Action Settings</span>
+                  <span className="Subhead-heading">App Settings</span>
                   <div className="Subhead-description">
-                    This controls how the action behaves.
+                    This controls how the GitHub App behaves.
                   </div>
                 </div>
                 //form goes here
