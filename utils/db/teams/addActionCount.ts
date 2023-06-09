305873d4ce9e9a81badb921b80e5c35ed18684e7
Esteban Dalel R <estebandalelr@gmail.com>
2023-06-09 16:01:11 -0500

    small fixes

--- utils/db/teams/addActionCount.ts
+++ utils/db/teams/addActionCount.ts
@@ -5,6 +5,5 @@ export default async ({ watermelon_user }) => {
   let query = await executeRequest(
     `EXEC dbo.increment_github_app_uses @watermelon_user = '${watermelon_user}'`
   );
-  console.log("query", query);
   return query;
 };
