5289cd9467cd40982f5e029108c4b5f7f36e5fff
Esteban Dalel R <estebandalelr@gmail.com>
2023-06-10 18:32:26 -0500

    Move text writers to helpers

---
+++ utils/actions/markdownHelpers/github.ts
@@ -0,0 +1,30 @@
+const githubMarkdown = ({
+  GitHubPRs,
+  ghValue,
+  userLogin,
+}: {
+  GitHubPRs: number;
+  ghValue: any;
+  userLogin: string;
+}) => {
+  let markdown = "";
+
+  markdown += `\n`;
+  markdown += "### GitHub PRs";
+  if (GitHubPRs) {
+    if (!Array.isArray(ghValue) && ghValue?.error === "no github token") {
+      markdown += `\n No results found :(`;
+    } else if (Array.isArray(ghValue) && ghValue?.length) {
+      for (let index = 0; index < ghValue?.length; index++) {
+        const element = ghValue[index];
+        markdown += `\n - [#${element.number} - ${element.title}](${element.html_url})`;
+        markdown += `\n`;
+      }
+    }
+  } else {
+    markdown += `GitHub PRs deactivated by ${userLogin}`;
+
+    markdown += `\n`;
+  }
+  return markdown;
+};
