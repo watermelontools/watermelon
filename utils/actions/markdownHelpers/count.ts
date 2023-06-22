121c944e051b0c8a78733498c8121ebc6397e602
Esteban Dalel R <estebandalelr@gmail.com>
2023-06-22 11:25:27 -0500

    Extract count markdown

---
+++ utils/actions/markdownHelpers/count.ts
@@ -0,0 +1,22 @@
+const countMarkdown = ({
+  count,
+  isPrivateRepo,
+  repoName,
+}: {
+  count: any;
+  isPrivateRepo: boolean;
+  repoName: string;
+}) => {
+  let textToWrite = "";
+  if (isPrivateRepo) {
+    if (count.error) {
+      textToWrite += `\n We're sorry, we ran into an error: ${count.error}`;
+    } else {
+      textToWrite += `\n Your team ${count.name} has used Watermelon ${count.github_app_uses} times.`;
+    }
+  } else {
+    textToWrite += `\n ${repoName} is an open repo and Watermelon will serve it for free.`;
+    textToWrite += `\n 🍉🫶`;
+  }
+  return textToWrite;
+};
