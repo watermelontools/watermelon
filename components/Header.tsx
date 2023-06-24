b8ff0e308e449387ce50e571637a47f1ff08c93b
Esteban Dalel R <estebandalelr@gmail.com>
2023-06-24 15:13:50 -0500

    fix typings

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { useSession, signOut } from "next-auth/react";
 import { useEffect, useState } from "react";
 export default function Header() {
-  const [userEmail, setUserEmail] = useState(null);
+  const [userEmail, setUserEmail] = useState<string | null | undefined>(null);
   const { data } = useSession();
 
   useEffect(() => {
@@ -48,7 +48,7 @@ export default function Header() {
                   className="dropdown-item"
                   href={`vscode://watermelontools.watermelon-tools?email=${
                     userEmail ?? ""
-                  }&token=${data?.user.name ? data.user.name : ""}`}
+                  }&token=${data?.user?.name ? data.user.name : ""}`}
                 >
                   VSCode Extension
                 </a>
