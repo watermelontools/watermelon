2631baf274880944068dd861448af5ff835caf89
Esteban Dalel R <estebandalelr@gmail.com>
2023-06-24 15:37:21 -0500

    Fix possible null

--- components/logout-btn.tsx
+++ components/logout-btn.tsx
@@ -3,7 +3,7 @@ export default function Component() {
   const { data: session } = useSession();
   return (
     <>
-      <p>Signed in as {session.user.email}</p>
+      <p>Signed in as {session?.user?.email}</p>
       <button className="btn" onClick={() => signOut()}>
         Sign out
       </button>
