3dd3ea3451aa52105550909635c6596dcbde9eb6
Esteban Dalel R <estebandalelr@gmail.com>
2023-06-27 14:54:10 -0500

    restore session provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import { SessionProvider } from "next-auth/react";
 import { Analytics } from "@vercel/analytics/react";
 import "@primer/css/index.scss";
-export default function App({ Component }) {
-  return <Component />;
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+      <Analytics />
+    </SessionProvider>
+  );
 }
