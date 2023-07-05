78ac9019d8d0c4bb738f12c74ec87eb02961fe10
Esteban Dalel R <estebandalelr@gmail.com>
2023-07-04 20:19:17 -0500

    Move layout to master layout

--- app/page.tsx
+++ app/page.tsx
@@ -12,62 +12,58 @@ async function HomePage({}) {
   let userEmail = session?.user?.email;
   let userName = session?.user?.name;
   // if not logged in, do not show anything
-  if (!session) return <LogInBtn />;
   const data = await getAllPublicUserData({ userEmail });
   return (
     <div>
       <>
-        <Header userEmail={userEmail} userToken={userName} />
-        <Navbar>
-          {userEmail ? (
-            <>
-              <LoginGrid userEmail={userEmail} data={data} />
-              <div
-                style={{
-                  display: "grid",
-                  gridTemplateColumns: "repeat(auto-fit, minmax(350px, 1fr))",
-                }}
-              >
-                <div className="p-3">
-                  <DownloadExtension
-                    name="VSCode"
-                    email={userEmail}
-                    urlStart="vscode"
-                    accessToken={userName}
-                  />
-                </div>
-                <div className="p-3">
-                  <DownloadExtension
-                    name="VSCode Insiders"
-                    urlStart="vscode-insiders"
-                    email={userEmail}
-                    accessToken={userName}
-                  />
-                </div>
-                <div className="p-3">
-                  <DownloadExtension
-                    name="VSCodium"
-                    urlStart="vscodium"
-                    email={userEmail}
-                    accessToken={userName}
-                  />
+        {userEmail ? (
+          <>
+            <LoginGrid userEmail={userEmail} data={data} />
+            <div
+              style={{
+                display: "grid",
+                gridTemplateColumns: "repeat(auto-fit, minmax(350px, 1fr))",
+              }}
+            >
+              <div className="p-3">
+                <DownloadExtension
+                  name="VSCode"
+                  email={userEmail}
+                  urlStart="vscode"
+                  accessToken={userName}
+                />
+              </div>
+              <div className="p-3">
+                <DownloadExtension
+                  name="VSCode Insiders"
+                  urlStart="vscode-insiders"
+                  email={userEmail}
+                  accessToken={userName}
+                />
+              </div>
+              <div className="p-3">
+                <DownloadExtension
+                  name="VSCodium"
+                  urlStart="vscodium"
+                  email={userEmail}
+                  accessToken={userName}
+                />
+              </div>
+            </div>
+            <a
+              href="https://github.com/apps/watermelon-context"
+              target="_blank"
+            >
+              <div className="Box d-flex flex-items-center flex-justify-start m-3 p-2">
+                <img className="avatar avatar-8" src="/logos/github.svg" />
+                <div className="p-2">
+                  <h2>Try our GitHub App</h2>
+                  <p>Context on each Pr</p>
                 </div>
               </div>
-              <a
-                href="https://github.com/apps/watermelon-context"
-                target="_blank"
-              >
-                <div className="Box d-flex flex-items-center flex-justify-start m-3 p-2">
-                  <img className="avatar avatar-8" src="/logos/github.svg" />
-                  <div className="p-2">
-                    <h2>Try our GitHub App</h2>
-                    <p>Context on each Pr</p>
-                  </div>
-                </div>
-              </a>
-            </>
-          ) : null}
-        </Navbar>
+            </a>
+          </>
+        ) : null}
       </>
     </div>
   );
