fd379c3ae14a90709c7985021c0af80903741558
Esteban Dalel R <estebandalelr@gmail.com>
2023-07-06 15:28:05 -0500

    manage empty results

--- utils/actions/markdownHelpers/linear.ts
+++ utils/actions/markdownHelpers/linear.ts
@@ -16,6 +16,15 @@ const linearMarkdown = ({
     if (linearValue?.error === "no linear token") {
       markdown += `\n [Click here to login to Linear](https://app.watermelontools.com)`;
     } else {
+      if (linearValue.length) {
+        for (let index = 0; index < linearValue.length; index++) {
+          const element = linearValue[index];
+          markdown += `\n - [${element.title} - ${element.number}](${element.url})`;
+          markdown += `\n`;
+        }
+      } else {
+        markdown += `\n No results found :(`;
+      }
     }
   } else {
     markdown += `Linear Tickets deactivated by ${userLogin}`;
