2cebdd1e0b16a34eb8e5e1fc7b14d551bf83ac83
Esteban Dalel R <estebandalelr@gmail.com>
2023-07-06 09:23:05 -0500

    Add search params to billing page

--- app/billing/page.tsx
+++ app/billing/page.tsx
@@ -2,7 +2,14 @@ import { authOptions } from "../api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
 import CardElement from "./cardElement";
 
-async function BillingPage() {
+async function BillingPage({
+  params,
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  console.log("BillingPage", { params, searchParams });
   const session = await getServerSession(authOptions);
   let userEmail = session?.user?.email;
 
