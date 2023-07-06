6a33798b177ff57297a6681cdf5baddbd43077a2
Esteban Dalel R <estebandalelr@gmail.com>
2023-07-06 16:03:18 -0500

    Fix undefined team count

--- utils/actions/markdownHelpers/count.ts
+++ utils/actions/markdownHelpers/count.ts
@@ -12,7 +12,9 @@ const countMarkdown = ({
     if (count.error) {
       textToWrite += `\n We're sorry, we ran into an error: ${count.error}`;
     } else {
-      textToWrite += `\n Your team ${count.name} has used Watermelon ${count.github_app_uses} times.`;
+      if (count.name && count.github_app_uses) {
+        textToWrite += `\n Your team ${count.name} has used Watermelon ${count.github_app_uses} times.`;
+      }
     }
   } else {
     textToWrite += `\n ${repoName} is an open repo and Watermelon will serve it for free.`;
