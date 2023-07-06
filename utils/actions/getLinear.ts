79b64549c34ea27fb27f66b8422ee2be603aadc4
Esteban Dalel R <estebandalelr@gmail.com>
2023-07-06 09:54:20 -0500

    Add linear fetching

--- utils/actions/getLinear.ts
+++ utils/actions/getLinear.ts
@@ -13,7 +13,24 @@ async function getLinear({
     ghValue = { error: "no linear token" };
     return ghValue;
   } else {
-    return [];
+    const graphql = JSON.stringify({
+      query:
+        "query SearchIssues($term: String!) {\n  searchIssues(term: $term) {\n    nodes {\n      title\n      number\n      url\n    }\n  }\n}",
+      constiables: { term: randomWords },
+    });
+
+    let linearTickets = await fetch("https://api.linear.app/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${linear_token}`,
+      },
+      body: graphql,
+    })
+      .then((response) => response.json())
+      .then((result) => result)
+      .catch((error) => console.log("error", error));
+    return linearTickets.data.searchIssues.nodes;
   }
 }
 export default getLinear;
