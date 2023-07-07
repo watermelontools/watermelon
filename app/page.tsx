f6294f37f47a5c4ae135c7f954e41126ba279935
Esteban Dalel R <estebandalelr@gmail.com>
2023-07-07 08:24:33 -0500

    Check for data before render

--- app/page.tsx
+++ app/page.tsx
@@ -12,51 +12,49 @@ async function HomePage({}) {
   const data = await getAllPublicUserData({ userEmail });
   return (
     <div>
-      <>
-        <>
-          <LoginGrid userEmail={userEmail} data={data} />
-          <div
-            style={{
-              display: "grid",
-              gridTemplateColumns: "repeat(auto-fit, minmax(350px, 1fr))",
-            }}
-          >
-            <div className="p-3">
-              <DownloadExtension
-                name="VSCode"
-                email={userEmail}
-                urlStart="vscode"
-                accessToken={userName}
-              />
-            </div>
-            <div className="p-3">
-              <DownloadExtension
-                name="VSCode Insiders"
-                urlStart="vscode-insiders"
-                email={userEmail}
-                accessToken={userName}
-              />
-            </div>
-            <div className="p-3">
-              <DownloadExtension
-                name="VSCodium"
-                urlStart="vscodium"
-                email={userEmail}
-                accessToken={userName}
-              />
-            </div>
+      {data && <LoginGrid userEmail={userEmail} data={data} />}
+      {userEmail && (
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fit, minmax(350px, 1fr))",
+          }}
+        >
+          <div className="p-3">
+            <DownloadExtension
+              name="VSCode"
+              email={userEmail}
+              urlStart="vscode"
+              accessToken={userName}
+            />
           </div>
-          <a href="https://github.com/apps/watermelon-context" target="_blank">
-            <div className="Box d-flex flex-items-center flex-justify-start m-3 p-2">
-              <img className="avatar avatar-8" src="/logos/github.svg" />
-              <div className="p-2">
-                <h2>Try our GitHub App</h2>
-                <p>Context on each Pr</p>
-              </div>
-            </div>
-          </a>
-        </>
-      </>
+          <div className="p-3">
+            <DownloadExtension
+              name="VSCode Insiders"
+              urlStart="vscode-insiders"
+              email={userEmail}
+              accessToken={userName}
+            />
+          </div>
+          <div className="p-3">
+            <DownloadExtension
+              name="VSCodium"
+              urlStart="vscodium"
+              email={userEmail}
+              accessToken={userName}
+            />
+          </div>
+        </div>
+      )}
+      <a href="https://github.com/apps/watermelon-context" target="_blank">
+        <div className="Box d-flex flex-items-center flex-justify-start m-3 p-2">
+          <img className="avatar avatar-8" src="/logos/github.svg" />
+          <div className="p-2">
+            <h2>Try our GitHub App</h2>
+            <p>Context on each Pr</p>
+          </div>
+        </div>
+      </a>
     </div>
   );
 }
