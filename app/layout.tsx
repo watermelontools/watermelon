3e09745cda53ceb4fdb02fe9b25dcc48e3878bd5
Esteban Dalel R <estebandalelr@gmail.com>
2023-07-07 08:36:15 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,18 +19,21 @@ export default async function RootLayout({
   const session = await getServerSession(authOptions);
   let userEmail = session?.user?.email;
   let userName = session?.user?.name;
-  if (!session) {
-    return <LogInBtn />;
-  } else {
-    return (
-      <html lang="en" data-color-mode="dark" data-dark-theme="dark">
-        <body>
-          <Header userEmail={userEmail} userToken={userName} />
-          <Navbar>
-            <AuthProvider>{children}</AuthProvider>
-          </Navbar>
-        </body>
-      </html>
-    );
-  }
+
+  return (
+    <html lang="en" data-color-mode="dark" data-dark-theme="dark">
+      <body>
+        {userEmail ? (
+          <>
+            <Header userEmail={userEmail} userToken={userName} />
+            <Navbar>
+              <AuthProvider>{children}</AuthProvider>
+            </Navbar>
+          </>
+        ) : (
+          <LogInBtn />
+        )}
+      </body>
+    </html>
+  );
 }
