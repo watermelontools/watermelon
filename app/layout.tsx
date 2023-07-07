1595f26e80e978d5ae85d02a06ce6db3922c76e1
Esteban Dalel R <estebandalelr@gmail.com>
2023-07-07 08:32:52 -0500

    Fix return

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,16 +19,18 @@ export default async function RootLayout({
   const session = await getServerSession(authOptions);
   let userEmail = session?.user?.email;
   let userName = session?.user?.name;
-  if (!session) return <LogInBtn />;
-
-  return (
-    <html lang="en" data-color-mode="dark" data-dark-theme="dark">
-      <body>
-        <Header userEmail={userEmail} userToken={userName} />
-        <Navbar>
-          <AuthProvider>{children}</AuthProvider>
-        </Navbar>
-      </body>
-    </html>
-  );
+  if (!session) {
+    return <LogInBtn />;
+  } else {
+    return (
+      <html lang="en" data-color-mode="dark" data-dark-theme="dark">
+        <body>
+          <Header userEmail={userEmail} userToken={userName} />
+          <Navbar>
+            <AuthProvider>{children}</AuthProvider>
+          </Navbar>
+        </body>
+      </html>
+    );
+  }
 }
