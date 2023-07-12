2b8a4f370a8a49acd5eb84ea8e0594947e2e0e89
Esteban Dalel R <estebandalelr@gmail.com>
2023-07-12 12:43:01 -0500

    Rename tracker

--- lib/track/posthogTracker.ts
+++ lib/track/posthogTracker.ts
@@ -2,7 +2,7 @@ import "server-only";
 
 import { PostHog } from "posthog-node";
 
-export default function PostHogClient() {
+export default function PostHogTracker() {
   const posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
   });
