084e714d53c39f21a64a46a23ad7c076bb232557
Esteban Dalel R <estebandalelr@gmail.com>
2023-07-15 10:41:54 -0500

    Fix link

--- components/JiraLoginLink.tsx
+++ components/JiraLoginLink.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const JiraLoginLink = ({ userEmail }) => (
   <div>
     <Link
-      href={`https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=VUngRAClu8ZE56vxXCFBocTxCTLEUQTT&scope=read%3Ajira-work%20read%3Ajira-user%20write%3Ajira-work&redirect_uri=https%3A%2F%2Fapp.watermelontools.com%2Fjira&state=${userEmail}&response_type=code&prompt=consent`}
+      href={`https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=VUngRAClu8ZE56vxXCFBocTxCTLEUQTT&scope=read%3Ajira-work%20read%3Ajira-user%20write%3Ajira-work&redirect_uri=https%3A%2F%2Fapp.watermelontools.com%2Fatlassian&state=${userEmail}&response_type=code&prompt=consent`}
       className="button block"
     >
       <div className="Box d-flex flex-items-center flex-justify-start p-2">
