2c7288f6c202d9b85f07052cb2d5190f855fb743
Esteban Dalel R <estebandalelr@gmail.com>
2023-07-15 10:40:41 -0500

    change link

--- components/JiraLoginLink.tsx
+++ components/JiraLoginLink.tsx
@@ -2,18 +2,18 @@ import Link from "next/link";
 
 const JiraLoginLink = ({ userEmail }) => (
   <div>
-      <Link
-        href={`https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=VUngRAClu8ZE56vxXCFBocTxCTLEUQTT&scope=read:jira-user%20read:jira-work%20write:jira-work%20offline_access&redirect_uri=https://app.watermelontools.com/jira&state=${userEmail}&response_type=code&prompt=consent`}
-        className="button block"
-      >
-        <div className="Box d-flex flex-items-center flex-justify-start p-2">
-          <img className="avatar avatar-8" src="/logos/jira.svg" />
-          <div className="p-2">
-            <h2>Login to Jira</h2>
-            <p>View your Most Relevant Ticket and Active Tickets</p>
-          </div>
+    <Link
+      href={`https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=VUngRAClu8ZE56vxXCFBocTxCTLEUQTT&scope=read%3Ajira-work%20read%3Ajira-user%20write%3Ajira-work&redirect_uri=https%3A%2F%2Fapp.watermelontools.com%2Fjira&state=${userEmail}&response_type=code&prompt=consent`}
+      className="button block"
+    >
+      <div className="Box d-flex flex-items-center flex-justify-start p-2">
+        <img className="avatar avatar-8" src="/logos/jira.svg" />
+        <div className="p-2">
+          <h2>Login to Jira</h2>
+          <p>View your Most Relevant Ticket and Active Tickets</p>
         </div>
-      </Link>
+      </div>
+    </Link>
   </div>
 );
 export default JiraLoginLink;
