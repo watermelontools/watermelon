21602b2d5dbd234d911e729849fdb045e8c0dd67
Esteban Dalel R <estebandalelr@gmail.com>
2023-07-17 16:35:49 -0500

    Fix query

--- utils/db/confluence/saveUserInfo.ts
+++ utils/db/confluence/saveUserInfo.ts
@@ -14,7 +14,7 @@ export default async ({
   user_id,
   user_displayname,
 }) => {
-  let query = `EXEC dbo.create_jira @organization='${organization}', @confluence_id='${confluence_id}', @url='${url}', @org_avatar_url='${org_avatar_url}', @watermelon_user='${watermelon_user}', @user_email='${user_email}', @user_avatar_url='${user_avatar_url}', @user_id='${user_id}', @user_displayname='${user_displayname}', @access_token='${access_token}', @refresh_token='${refresh_token}', @scopes='${scopes}'`;
+  let query = `EXEC dbo.create_confluence @organization='${organization}', @confluence_id='${confluence_id}', @url='${url}', @org_avatar_url='${org_avatar_url}', @watermelon_user='${watermelon_user}', @user_email='${user_email}', @user_avatar_url='${user_avatar_url}', @user_id='${user_id}', @user_displayname='${user_displayname}', @access_token='${access_token}', @refresh_token='${refresh_token}', @scopes='${scopes}'`;
   let resp = await executeRequest(query);
   return resp;
 };
