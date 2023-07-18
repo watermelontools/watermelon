5732171d8482da7edc6b2411a28b538c30556548
Esteban Dalel R <estebandalelr@gmail.com>
2023-07-18 12:50:14 -0500

    Make sidebar sticky, add logout button

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,18 +27,21 @@ export default async function RootLayout({
   const session = await getServerSession(authOptions);
   let userEmail = session?.user?.email;
   let userName = session?.user?.name;
-  if (!session) {
-    return <LogInBtn />;
-  } else {
-    return (
-      <html lang="en" data-color-mode="dark" data-dark-theme="dark">
-        <body>
-          <Header userEmail={userEmail} userToken={userName} />
-          <Navbar>
-            <AuthProvider>{children}</AuthProvider>
-          </Navbar>
-        </body>
-      </html>
-    );
-  }
+
+  return (
+    <html lang="en" data-color-mode="dark" data-dark-theme="dark">
+      <body style={{ minHeight: "100vh" }}>
+        {userEmail ? (
+          <>
+            <Header userEmail={userEmail} userToken={userName} />
+            <Navbar>
+              <AuthProvider>{children}</AuthProvider>
+            </Navbar>
+          </>
+        ) : (
+          <LogInBtn />
+        )}
+      </body>
+    </html>
+  );
 }
