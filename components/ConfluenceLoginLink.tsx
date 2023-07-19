72973e5a38ac3950373687e7536c9b333c6fd0a7
Esteban Dalel R <estebandalelr@gmail.com>
2023-07-19 13:42:37 -0500

    Get refresh token on login

--- components/ConfluenceLoginLink.tsx
+++ components/ConfluenceLoginLink.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const ConfluenceLoginLink = ({ userEmail }) => (
   <div>
     <Link
-      href={`https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=VUngRAClu8ZE56vxXCFBocTxCTLEUQTT&scope=read%3Acontent%3Aconfluence%20read%3Acontent-details%3Aconfluence%20read%3Ablogpost%3Aconfluence%20read%3Acomment%3Aconfluence%20read%3Auser%3Aconfluence%20read%3Auser.property%3Aconfluence%20read%3Ainlinetask%3Aconfluence&redirect_uri=https%3A%2F%2Fapp.watermelontools.com%2Fatlassian&state=${
+      href={`https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=VUngRAClu8ZE56vxXCFBocTxCTLEUQTT&scope=offline_access%20read%3Acontent%3Aconfluence%20read%3Acontent-details%3Aconfluence%20read%3Ablogpost%3Aconfluence%20read%3Acomment%3Aconfluence%20read%3Auser%3Aconfluence%20read%3Auser.property%3Aconfluence%20read%3Ainlinetask%3Aconfluence&redirect_uri=https%3A%2F%2Fapp.watermelontools.com%2Fatlassian&state=${
         "c" + userEmail
       }&response_type=code&prompt=consent`}
       className="button block"
