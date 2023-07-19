f3ff58f72eeca8b306a72d14550929c05a29a16e
Esteban Dalel R <estebandalelr@gmail.com>
2023-07-19 16:38:43 -0500

    Add confluence

---
+++ utils/actions/getConfluence.ts
@@ -0,0 +1,68 @@
+import updateTokensFromConfluence from "../../utils/confluence/updateTokens";
+import updateTokens from "../../utils/db/confluence/updateTokens";
+
+type ConfluenceResult = { error: string } | any[];
+function removeSpecialChars(inputString) {
+  const specialChars = '!"#$%&/()=-_"{}¨*[]'; // Edit this list to include or exclude characters
+  return inputString
+    .split("")
+    .filter((char) => !specialChars.includes(char))
+    .join("");
+}
+async function getConfluence({
+  confluence_token,
+  confluence_refresh_token,
+  confluence_id,
+  user,
+  randomWords,
+  amount = 3,
+}) {
+  if (!confluence_token || !confluence_refresh_token) {
+    return { error: "no confluence token" };
+  } else {
+    if (!user) {
+      return { error: "no user" };
+    }
+    if (!confluence_id) {
+      return { error: "no confluence cloudId" };
+    }
+    const newAccessTokens = await updateTokensFromConfluence({
+      refresh_token: confluence_refresh_token,
+    });
+    await updateTokens({
+      access_token: newAccessTokens.access_token,
+      refresh_token: newAccessTokens.refresh_token,
+      user,
+    });
+    let cleanRW = Array.from(
+      new Set(randomWords.map((word) => removeSpecialChars(word)))
+    );
+
+    const titleQuery = cleanRW.map((word) => `title ~ "${word}"`).join(" OR ");
+
+    const textQuery = cleanRW.map((word) => `text ~ "${word}"`).join(" OR ");
+
+    // Sorting by description might be better than completely filtering out tickets without a description
+    let cql = `(${titleQuery}) AND (${textQuery}) ORDER BY created DESC`;
+
+    let returnVal = await fetch(
+      `https://api.atlassian.com/ex/confluence/${confluence_id}/rest/api/search?cql=${cql}&limit=${amount}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: `Bearer ${confluence_token}`,
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((resJson) => resJson.issues)
+      .catch((error) => {
+        console.error(error);
+      });
+    console.log(returnVal);
+    return returnVal;
+  }
+}
+export default getConfluence;
---
+++ utils/db/confluence/updateTokens.ts
@@ -0,0 +1,22 @@
+import executeRequest from "../azuredb";
+
+export default async function updateTokens({
+  user,
+  access_token,
+  refresh_token,
+}: {
+  user: string;
+  access_token: string;
+  refresh_token: string;
+}): Promise<void> {
+  try {
+    if (user && access_token && refresh_token) {
+      let query = `EXEC dbo.update_confluence_tokens  @user='${user}', @access_token='${access_token}', @refresh_token='${refresh_token}'`;
+      let resp = await executeRequest(query);
+      return resp;
+    }
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+}
