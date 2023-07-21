552709c56b1e74ad6f4f2531a0f026c95149c9a7
Esteban Dalel R <estebandalelr@gmail.com>
2023-07-21 14:43:40 -0500

    Change how tokens are updated

--- utils/confluence/updateTokens.ts
+++ utils/confluence/updateTokens.ts
@@ -1,34 +1,38 @@
-export default async function updateTokens({
+export default async function updateTokensFromConfluence({
   refresh_token,
 }: {
   refresh_token: string;
 }): Promise<{ access_token: string; refresh_token: string }> {
   try {
-    let newAccessTokens = await fetch(
-      "https://auth.atlassian.com/oauth/token",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          grant_type: "refresh_token",
-          client_id: process.env.NEXT_PUBLIC_JIRA_CLIENT_ID,
-          client_secret: process.env.JIRA_CLIENT_SECRET,
-          refresh_token: refresh_token,
-        }),
-      }
-    )
-      .then((response) => response.json())
-      .catch((error) => {
-        console.error(error);
-      });
+    const response = await fetch("https://auth.atlassian.com/oauth/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        grant_type: "refresh_token",
+        client_id: process.env.NEXT_PUBLIC_JIRA_CLIENT_ID,
+        client_secret: process.env.JIRA_CLIENT_SECRET,
+        refresh_token: refresh_token,
+      }),
+    });
+
+    // Check if the response is not OK before proceeding
+    if (!response.ok) {
+      const errorText = await response.text();
+      console.error("Error while refreshing Confluence tokens:", errorText);
+      throw new Error(
+        `Failed to refresh tokens. HTTP Status: ${response.status}`
+      );
+    }
+
+    const newAccessTokens = await response.json();
     return {
       access_token: newAccessTokens.access_token,
       refresh_token: newAccessTokens.refresh_token,
     };
   } catch (error) {
-    console.error(error);
-    return error;
+    console.error("Error in updateTokensFromConfluence:", error);
+    throw error; // propagate the error up for caller to handle if necessary
   }
 }
--- utils/db/confluence/updateTokens.ts
+++ utils/db/confluence/updateTokens.ts
@@ -11,12 +11,14 @@ export default async function updateTokens({
 }): Promise<void> {
   try {
     if (user && access_token && refresh_token) {
-      let query = `EXEC dbo.update_confluence_tokens  @user='${user}', @access_token='${access_token}', @refresh_token='${refresh_token}'`;
+      const query = `EXEC dbo.update_confluence_tokens  @user='${user}', @access_token='${access_token}', @refresh_token='${refresh_token}'`;
       let resp = await executeRequest(query);
       return resp;
+    } else {
+      throw new Error("User or tokens missing. Cannot update DB.");
     }
   } catch (error) {
-    console.error(error);
-    return error;
+    console.error("Error in updateTokens:", error);
+    throw error; // propagate the error up for caller to handle if necessary
   }
 }
