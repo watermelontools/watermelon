286b5fce739cd9a078f171c4e8cef231820c2ead
Esteban Dalel R <estebandalelr@gmail.com>
2023-07-21 10:25:00 -0500

    Check if null

--- utils/db/github/addActionLog.ts
+++ utils/db/github/addActionLog.ts
@@ -16,14 +16,38 @@ export default async function addActionLog({
   count,
   watermelon_user,
 }): Promise<any> {
+  function isEmpty(value) {
+    // Check for undefined or null
+    if (value === null || value === undefined) return true;
+
+    // Check for empty string or empty array
+    if (typeof value === "string" || Array.isArray(value))
+      return value.length === 0;
+
+    // Check for empty object
+    if (typeof value === "object") return Object.keys(value).length === 0;
+
+    return false;
+  }
+
+  function stringifyAndEscape(value) {
+    if (isEmpty(value)) return "''"; // or return whatever you want in the case of empty values
+
+    return `'${JSON.stringify(value).replace(/'/g, "''")}'`;
+  }
+  const github_response = stringifyAndEscape(ghValue);
+  const jira_response = stringifyAndEscape(jiraValue);
+  const slack_response = stringifyAndEscape(slackValue);
+  const notion_response = stringifyAndEscape(notionValue);
+  const linear_response = stringifyAndEscape(linearValue);
   try {
     const saveLog = `EXEC dbo.create_gh_action_log 
     @randomWords='${randomWords.join(" ")}', 
-    @github_response='${JSON.stringify(ghValue).replace(/'/g, "''")}', 
-    @jira_response='${JSON.stringify(jiraValue).replace(/'/g, "''")}', 
-    @slack_response='${JSON.stringify(slackValue).replace(/'/g, "''")}', 
-    @notion_response='${JSON.stringify(notionValue).replace(/'/g, "''")}', 
-    @linear_response='${JSON.stringify(linearValue).replace(/'/g, "''")}', 
+    @github_response=${github_response}, 
+    @jira_response=${jira_response}, 
+    @slack_response=${slack_response}, 
+    @notion_response=${notion_response}, 
+    @linear_response=${linear_response},
     @markdown='${textToWrite}', 
     @GPT_summary='${businessLogicSummary}', 
     @github_owner='${owner}', 
