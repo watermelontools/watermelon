abb5bba170c24594134a404418d03345ff0bf065
Esteban Dalel R <estebandalelr@gmail.com>
2023-07-24 13:51:19 -0500

    use standard types

--- utils/actions/markdownHelpers/github.ts
+++ utils/actions/markdownHelpers/github.ts
@@ -1,24 +1,20 @@
-const githubMarkdown = ({
-  GitHubPRs,
-  ghValue,
-  userLogin,
-}: {
-  GitHubPRs: number;
-  ghValue: any;
-  userLogin: string;
-}) => {
-  let markdown = "";
+import { MarkdownRequest, MarkdownResponse } from "../../../types/watermelon";
 
-  markdown += `\n`;
-  markdown += "### GitHub PRs";
-  if (GitHubPRs) {
-    if (!Array.isArray(ghValue) && ghValue?.error === "no github token") {
+const githubMarkdown = ({
+  amount,
+  value,
+  userLogin,
+}: MarkdownRequest): MarkdownResponse => {
+  let markdown: MarkdownResponse = "";
+
+  markdown += `\n ### GitHub PRs`;
+  if (amount) {
+    if (!Array.isArray(value) && value?.error === "no github token") {
       markdown += `\n No results found :(`;
-    } else if (Array.isArray(ghValue) && ghValue?.length) {
-      for (let index = 0; index < ghValue?.length; index++) {
-        const element = ghValue[index];
-        markdown += `\n - [#${element.number} - ${element.title}](${element.html_url})`;
-        markdown += `\n`;
+    } else if (Array.isArray(value) && value?.length) {
+      for (let index = 0; index < value?.length; index++) {
+        const element = value[index];
+        markdown += `\n - [#${element.number} - ${element.title}](${element.html_url}) \n`;
       }
     }
   } else {
@@ -28,4 +24,5 @@ const githubMarkdown = ({
   }
   return markdown;
 };
+
 export default githubMarkdown;
