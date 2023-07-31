43915bf1278848928fbea4d3e03ba8129d76f359
Esteban Dalel R <estebandalelr@gmail.com>
2023-07-31 18:06:36 -0500

    Remove page replication

---
+++ utils/services/loading.tsx
@@ -0,0 +1,22 @@
+import TimeToRedirect from "../../components/redirect";
+
+export default function LoadingConnectedService() {
+  return (
+    <div className="Box" style={{ maxWidth: "100ch", margin: "auto" }}>
+      <div className="Subhead">
+        <h2 className="Subhead-heading px-2">
+          You have logged in with Service as Tulia in the team Watermelon
+        </h2>
+      </div>
+      <img
+        src="/logos/github.svg"
+        alt="linear user image"
+        className="avatar avatar-8"
+      />
+      <div>
+        <TimeToRedirect url={"/"} />
+        <p>If you are not redirected, please click here</p>
+      </div>
+    </div>
+  );
+}
