fe049b2fbaba24d2147f30e898d28c6d6316cc5e
Esteban Dalel R <estebandalelr@gmail.com>
2023-08-02 11:39:46 -0500

    Make keys defined

--- pages/api/actions/github.ts
+++ pages/api/actions/github.ts
@@ -9,8 +9,8 @@ import addActionCount from "../../../utils/db/teams/addActionCount";
 import getNotion from "../../../utils/actions/getNotion";
 
 const app = new App({
-  appId: process.env.GITHUB_APP_IDx,
-  privateKey: process.env.GITHUB_PRIVATE_KEY,
+  appId: process.env.GITHUB_APP_ID!,
+  privateKey: process.env.GITHUB_PRIVATE_KEY!,
 });
 
 export default async (req, res) => {
