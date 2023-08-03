f47f8551610189c6fffae085a5b64d9f79ab81be
Esteban Dalel R <estebandalelr@gmail.com>
2023-08-03 17:37:23 -0500

    dedupe code

--- app/billing/page.tsx
+++ app/billing/page.tsx
@@ -1,11 +1,6 @@
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import getServerSession from "next-auth";
 import CardElement from "./cardElement";
-import { Metadata } from "next";
-export const metadata: Metadata = {
-  title: "Billing",
-  description: "Pay for the Context of your team",
-};
 
 import { Metadata } from "next";
 export const metadata: Metadata = {
@@ -20,7 +15,6 @@ async function BillingPage({
   params: { slug: string };
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
-  console.log("BillingPage", { params, searchParams });
   const session = await getServerSession(authOptions);
   let userEmail = session?.user?.email;
   return (
