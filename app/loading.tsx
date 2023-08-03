d827279691004f9299d50da3cd6b32b68dab8cd6
Esteban Dalel R <estebandalelr@gmail.com>
2023-08-03 18:26:17 -0500

    DRY code

--- app/loading.tsx
+++ app/loading.tsx
@@ -7,8 +7,8 @@ async function loading({}) {
   const fakeObject = JSON.stringify({
     organization: "watermelon",
     user_avatar_url: "/logos/watermelon.png",
-    user_displayname: "Watermelon",
-    user_email: "tulia@watermelontools.com",
+    user_displayname: userName,
+    user_email: userEmail,
   });
   const fakeData = {
     github_data: fakeObject,
@@ -40,30 +40,20 @@ async function loading({}) {
               gridTemplateColumns: "repeat(auto-fit, minmax(350px, 1fr))",
             }}
           >
-            <div className="p-3">
-              <DownloadExtension
-                name="VSCode"
-                email={userEmail}
-                urlStart="vscode"
-                accessToken={userName}
-              />
-            </div>
-            <div className="p-3">
-              <DownloadExtension
-                name="VSCode Insiders"
-                urlStart="vscode-insiders"
-                email={userEmail}
-                accessToken={userName}
-              />
-            </div>
-            <div className="p-3">
-              <DownloadExtension
-                name="VSCodium"
-                urlStart="vscodium"
-                email={userEmail}
-                accessToken={userName}
-              />
-            </div>
+            {[
+              { name: "VSCode", urlStart: "vscode" },
+              { name: "VSCode Insiders", urlStart: "vscode-insiders" },
+              { name: "VSCodium", urlStart: "vscodium" },
+            ].map(({ name, urlStart }) => (
+              <div className="p-3">
+                <DownloadExtension
+                  name={name}
+                  urlStart={urlStart}
+                  email={userEmail}
+                  accessToken={userName}
+                />
+              </div>
+            ))}
           </div>
         </>
       </>
