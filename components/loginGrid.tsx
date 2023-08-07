0af2c4c485003d4d9300c354545df68728d029c4
Esteban Dalel R <estebandalelr@gmail.com>
2023-08-07 16:38:13 -0500

    Remove Discord

--- components/loginGrid.tsx
+++ components/loginGrid.tsx
@@ -1,5 +1,4 @@
 import InfoPanel from "../components/dashboard/InfoPanel";
-import DiscordLoginLink from "./DiscordLoginLink";
 import { SelectedServiceLoginLink } from "./services/loginArray";
 type LoginGridProps = {
   userEmail: string;
@@ -12,7 +11,6 @@ function LoginGrid({ userEmail, data }) {
   let slackUserData: null | LoginGridProps = null;
   let jiraUserData: null | LoginGridProps = null;
   let confluenceUserData: null | LoginGridProps = null;
-  let discordUserData: null | LoginGridProps = null;
   let notionUserData: null | LoginGridProps = null;
   let linearUserData: null | LoginGridProps = null;
   if (data?.github_data) {
@@ -32,9 +30,6 @@ function LoginGrid({ userEmail, data }) {
   }
   if (data?.confluence_data) {
     confluenceUserData = JSON.parse(data.confluence_data);
-  }
-  if (data?.discord_data) {
-    discordUserData = JSON.parse(data.discord_data);
   }
   if (data?.notion_data) {
     notionUserData = JSON.parse(data.notion_data);
@@ -213,22 +208,6 @@ function LoginGrid({ userEmail, data }) {
                   />
                 )}
               </div>
-              {/*         
-  DISCORD DOES NOT ALLOW MESSAGE SEARCH, DEVELOPMENT PAUSED FOR NOW
-  MAYBE READ THE LAST FEW DAYS?
-    <div className="p-3">
-              {discordUserData?.user_displayname ? (
-                <InfoPanel
-                  info={{
-                    user_avatar_url: `https://cdn.discordapp.com/avatars/${discordUserData.id}/${discordUserData.avatar_url}`,
-                    ...discordUserData,
-                    service_name: "Discord",
-                  }}
-                />
-              ) : (
-                <DiscordLoginLink userEmail={userEmail} />
-              )}
-            </div> */}
             </div>
           </div>
           <div>
