167b353e1fd2340c5da289138612eb4703488475
Esteban Dalel R <estebandalelr@gmail.com>
2023-08-08 18:33:40 -0400

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ export default async function NotFound() {
   return (
     <div>
       <h1>Not Found</h1>
-      <p>something went wrong!</p>
+      <p>Something went wrong!</p>
       <p>
         View <Link href="/">Go back home</Link>
       </p>
