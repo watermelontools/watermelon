ce8e89b3e6c25592a1a246ff1b3120eae26e10ef
Esteban Dalel R <estebandalelr@gmail.com>
2023-08-10 08:14:17 -0500

    Move updateSettings to app router

---
+++ app/api/user/updateSettings/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import validateParams from "../../../../utils/api/validateParams";
+import patchUserSettings from "../../../../utils/db/user/patchUserSettings";
+
+export async function POST(request: Request) {
+  const req = await request.json();
+  const { missingParams } = validateParams(req, ["email", "userSettings"]);
+
+  if (missingParams.length > 0) {
+    return NextResponse.json({
+      error: `Missing parameters: ${missingParams.join(", ")}`,
+    });
+  }
+
+  try {
+    let dbResponse = await patchUserSettings({
+      email: req.email,
+      userSettings: req.userSettings,
+    });
+    return NextResponse.json(dbResponse);
+  } catch (err) {
+    console.error("Error fetching db data:", err);
+    return NextResponse.json({ error: "Failed to fetch db data." });
+  }
+}
