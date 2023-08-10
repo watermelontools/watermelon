da02249acaadebdb1cfc44a2c3e66dc6f65a86df
Esteban Dalel R <estebandalelr@gmail.com>
2023-08-10 10:45:59 -0500

    Add getting test

--- tests/api/user/settings.test.ts
+++ tests/api/user/settings.test.ts
@@ -1,5 +1,20 @@
 import { POST } from "../../../app/api/user/settings/route";
-jest.mock("../../../utils/db/user/getAllPublicUserData");
+import getUserSettings from "../../../utils/db/user/settings";
+jest.mock("../../../utils/db/user/settings");
+
+const mockedGetUserSettings = getUserSettings as jest.Mock;
+mockedGetUserSettings.mockResolvedValue({
+  userSettings: {
+    AISummary: true,
+    JiraTickets: 5,
+    SlackMessages: 5,
+    GitHubPRs: 5,
+    NotionPages: 5,
+    LinearTickets: 5,
+    ConfluenceDocs: 5,
+  },
+});
+jest.mock("../../../utils/db/user/settings");
 describe("User Route POST function", () => {
   it("returns an error when email parameter is missing", async () => {
     // Mocking request object
@@ -13,5 +28,25 @@ describe("User Route POST function", () => {
     });
   });
 
-  // ... Add more test cases
+  it("Gets the userSettings", async () => {
+    // Mocking request object
+    const mockReq: any = {
+      json: async () => ({
+        email: "tulia@watermelontools.com",
+      }),
+    };
+
+    const response = await (await POST(mockReq)).json();
+    expect(response).toEqual({
+      userSettings: {
+        AISummary: true,
+        JiraTickets: 5,
+        SlackMessages: 5,
+        GitHubPRs: 5,
+        NotionPages: 5,
+        LinearTickets: 5,
+        ConfluenceDocs: 5,
+      },
+    });
+  });
 });
