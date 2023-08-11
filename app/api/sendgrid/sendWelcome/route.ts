04dd610b66aebde21d5434b57096ead1d5719a8a
Esteban Dalel R <estebandalelr@gmail.com>
2023-08-11 17:25:11 -0500

    Move Sendgrid to app router

--- app/api/sendgrid/sendWelcome/route.ts
+++ app/api/sendgrid/sendWelcome/route.ts
@@ -1,20 +1,18 @@
 import { NextResponse } from "next/server";
-import { missingParamsPosthogTracking } from "../../../../utils/api/posthogTracking";
-import { missingParamsResponse } from "../../../../utils/api/responses";
 import validateParams from "../../../../utils/api/validateParams";
 import sendWelcome from "../../../../utils/sendgrid/sendWelcome";
 
-export async function POST(request: Request) {
+export default async function POST(request: Request) {
   const req = await request.json();
   const { missingParams } = validateParams(req, ["sender", "emails"]);
 
   if (missingParams.length > 0) {
-    missingParamsPosthogTracking({ missingParams, url: request.url });
-    return missingParamsResponse({ missingParams });
+    return NextResponse.json({
+      error: `Missing parameters: ${missingParams.join(", ")}`,
+    });
   }
   const { sender, emails } = req;
 
   let emailSent = await sendWelcome({ sender, emails });
-
   return NextResponse.json(emailSent);
 }
