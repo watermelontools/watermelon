259cb2fc1cf681c6389548658d9424120140380a
Esteban Dalel R <estebandalelr@gmail.com>
2023-08-13 16:48:05 -0500

    Upgrade routes to return 400 on missing params

--- app/api/user/settings/route.ts
+++ app/api/user/settings/route.ts
@@ -1,42 +1,25 @@
+import { NextResponse } from "next/server";
 import getUserSettings from "../../../../utils/db/user/settings";
 import validateParams from "../../../../utils/api/validateParams";
-import {
-  failedToFetchResponse,
-  missingParamsResponse,
-  successResponse,
-} from "../../../../utils/api/responses";
-import {
-  failedPosthogTracking,
-  missingParamsPosthogTracking,
-  successPosthogTracking,
-} from "../../../../utils/api/posthogTracking";
 
 export async function POST(request: Request) {
   const req = await request.json();
   const { missingParams } = validateParams(req, ["email"]);
 
   if (missingParams.length > 0) {
-    missingParamsPosthogTracking({
-      missingParams,
-      url: request.url,
-    });
-    return missingParamsResponse({ missingParams });
+    return NextResponse.json(
+      {
+        error: `Missing parameters: ${missingParams.join(", ")}`,
+      },
+      { status: 400 }
+    );
   }
+
   try {
     let dbResponse = await getUserSettings({ email: req.email });
-    successPosthogTracking({
-      email: req.email,
-      url: request.url,
-      data: dbResponse,
-    });
-    return successResponse({ data: dbResponse });
+    return NextResponse.json(dbResponse);
   } catch (err) {
     console.error("Error fetching db data:", err);
-    failedPosthogTracking({
-      error: err,
-      email: req.email,
-      url: request.url,
-    });
-    return failedToFetchResponse({ error: err });
+    return NextResponse.json({ error: "Failed to fetch db data." });
   }
 }
