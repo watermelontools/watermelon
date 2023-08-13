e34d4cc39e3439821a54b9a3e15ebb459fb7004b
Esteban Dalel R <estebandalelr@gmail.com>
2023-08-13 16:48:05 -0500

    Upgrade routes to return 400 on missing params

--- app/api/stripe/createSubscription/route.ts
+++ app/api/stripe/createSubscription/route.ts
@@ -1,8 +1,7 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
-import { missingParamsResponse } from "../../../../utils/api/responses";
 import validateParams from "../../../../utils/api/validateParams";
-import { missingParamsPosthogTracking } from "../../../../utils/api/posthogTracking";
+
 const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY!, {
   apiVersion: "2022-08-01",
 });
@@ -12,8 +11,12 @@ export async function POST(request: Request) {
   const { missingParams } = validateParams(req, ["email"]);
 
   if (missingParams.length > 0) {
-    missingParamsPosthogTracking({ missingParams, url: request.url });
-    return missingParamsResponse({ missingParams });
+    return NextResponse.json(
+      {
+        error: `Missing parameters: ${missingParams.join(", ")}`,
+      },
+      { status: 400 }
+    );
   }
   try {
     // create a stripe customer and get its id
