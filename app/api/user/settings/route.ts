61a1b0e11351a4f6619c286bed6a527dc23fb1be
Esteban Dalel R <estebandalelr@gmail.com>
2023-08-14 15:37:00 -0500

    Feature/posthog backend (#243)
    * Create param validator
    
    * Move discord
    
    * Move user settings getter to app router
    
    * Use standard var
    
    * move getAllPublicUserData to app router
    
    * Better error handling
    
    * Delete getAllData.ts
    
    * Delete discord
    
    * Throw errors
    
    * Handle error throwing
    
    * Move updateSettings to app router
    
    * Update confluence.svg
    
    * Configure jest
    
    * Create settings.test.ts
    
    * Create updateSettings.test.ts
    
    * Add getting test
    
    * Use generalized validator
    
    * Move Stripe to app router
    
    * Move Sendgrid to app router
    
    * Move vscode to airtable Analytics to app router
    
    * Create posthog.ts
    
    * Add types, move to utils
    
    * Capture posthog event
    
    * Change export
    
    * Fix export
    
    * Fix export
    
    * Check dev env

--- app/api/user/settings/route.ts
+++ app/api/user/settings/route.ts
@@ -1,42 +1,29 @@
+import { NextResponse } from "next/server";
 import getUserSettings from "../../../../utils/db/user/settings";
 import validateParams from "../../../../utils/api/validateParams";
-import {
-  failedToFetchResponse,
-  missingParamsResponse,
-  successResponse,
-} from "../../../../utils/api/responses";
-import {
-  failedPosthogTracking,
-  missingParamsPosthogTracking,
-  successPosthogTracking,
-} from "../../../../utils/api/posthogTracking";
+import posthog from "../../../../utils/posthog/posthog";
 
 export async function POST(request: Request) {
   const req = await request.json();
   const { missingParams } = validateParams(req, ["email"]);
 
   if (missingParams.length > 0) {
-    missingParamsPosthogTracking({
-      missingParams,
-      url: request.url,
+    return NextResponse.json({
+      error: `Missing parameters: ${missingParams.join(", ")}`,
     });
-    return missingParamsResponse({ missingParams });
   }
+  posthog.capture({
+    distinctId: req.email,
+    event: "user_settings_viewed",
+    properties: {
+      email: req.email,
+    },
+  });
   try {
     let dbResponse = await getUserSettings({ email: req.email });
-    successPosthogTracking({
-      email: req.email,
-      url: request.url,
-      data: dbResponse,
-    });
-    return successResponse({ data: dbResponse });
+    return NextResponse.json(dbResponse);
   } catch (err) {
     console.error("Error fetching db data:", err);
-    failedPosthogTracking({
-      error: err,
-      email: req.email,
-      url: request.url,
-    });
-    return failedToFetchResponse({ error: err });
+    return NextResponse.json({ error: "Failed to fetch db data." });
   }
 }
