ddd95be63a93b64a87e552734f83cdc54577dccd
Esteban Dalel R <estebandalelr@gmail.com>
2023-08-15 08:54:35 -0500

    Remove event logging

--- utils/posthog/posthog.ts
+++ utils/posthog/posthog.ts
@@ -19,7 +19,6 @@ function PostHogClient(apiKey: string) {
         properties,
         groups,
       });
-      console.log("posthog event", event, properties);
       if (process.env.NODE_ENV === "development") {
         console.log(
           `PostHog event: ${event} with properties: ${JSON.stringify(
