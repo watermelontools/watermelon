02bd8c39946aad6f19be9a1677a4d9f8b93039be
Esteban Dalel R <estebandalelr@gmail.com>
2023-08-15 17:46:15 -0500

    Move to route

--- app/api/hover/getHoverData/route.ts
+++ app/api/hover/getHoverData/route.ts
@@ -1,9 +1,8 @@
-import getGitHub from "../../../../utils/actions/getGitHub";
-import getJira from "../../../../utils/actions/getJira";
-import getSlack from "../../../../utils/actions/getSlack";
-import getNotion from "../../../../utils/actions/getNotion";
-import getLinear from "../../../../utils/actions/getLinear";
-import getConfluence from "../../../../utils/actions/getConfluence";
+import getUserTokens from "../../../../utils/db/user/getUserTokens";
+import { Octokit } from "octokit";
+import updateTokensFromJira from "../../../../utils/jira/updateTokens";
+import updateTokens from "../../../../utils/db/jira/updateTokens";
+import searchMessageByText from "../../../../utils/slack/searchMessageByText";
 import validateParams from "../../../../utils/api/validateParams";
 import {
   failedPosthogTracking,
@@ -15,8 +14,11 @@ import {
   missingParamsResponse,
   successResponse,
 } from "../../../../utils/api/responses";
-import executeRequest from "../../../../utils/db/azuredb";
 
+function handleRejection(reason) {
+  console.error(reason);
+  return { error: reason };
+}
 export async function POST(request: Request) {
   const req = await request.json();
 
@@ -24,6 +26,7 @@ export async function POST(request: Request) {
     "email",
     "repo",
     "owner",
+    "gitSystem",
     "commitTitle",
   ]);
 
@@ -31,24 +34,10 @@ export async function POST(request: Request) {
     missingParamsPosthogTracking({ url: request.url, missingParams });
     return missingParamsResponse({ missingParams });
   }
-  const query = `EXEC dbo.get_all_user_tokens @watermelon_user='${req.email}'`;
-  let wmUserData = await executeRequest(query);
-  const {
-    github_token,
-    jira_token,
-    jira_refresh_token,
-    confluence_token,
-    confluence_refresh_token,
-    confluence_id,
-    cloudId,
-    slack_token,
-    notion_token,
-    linear_token,
-    user_email,
-    watermelon_user,
-  } = wmUserData;
+
+  let userTokens;
   try {
-    wmUserData = await executeRequest(query);
+    userTokens = await getUserTokens({ email: req.email });
   } catch (error) {
     console.error(
       "An error occurred while getting user tokens:",
@@ -61,77 +50,125 @@ export async function POST(request: Request) {
     });
     return failedToFetchResponse({ error: error.message });
   }
+  async function fetchGitHubIssues(userTokens, owner, repo) {
+    const { github_token } = userTokens;
 
-  const searchStringSet = Array.from(new Set(req.commitTitle.split(" "))).join(
-    " "
-  );
+    const octokit = new Octokit({
+      auth: github_token,
+    });
 
-  // select six random words from the search string
-  const randomWords = searchStringSet
-    .split(" ")
-    .sort(() => Math.random() - 0.5)
-    .slice(0, 6);
-  const { repo, owner } = req;
-  const [github, jira, confluence, slack, notion, linear] = await Promise.all([
-    getGitHub({
-      repo,
-      owner,
-      github_token,
-      randomWords,
-      amount: 1,
-    }),
-    getJira({
-      user: user_email,
-      token: jira_token,
-      refresh_token: jira_refresh_token,
-      randomWords,
-      amount: 1,
-    }),
-    getConfluence({
-      token: confluence_token,
-      refresh_token: confluence_refresh_token,
-      cloudId: confluence_id,
-      user: user_email,
-      randomWords,
-      amount: 1,
-    }),
-    getSlack({
-      slack_token,
-      searchString: randomWords.join(" "),
-      amount: 1,
-    }),
-    getNotion({
-      notion_token,
-      randomWords,
-      amount: 1,
-    }),
-    getLinear({
-      linear_token,
-      randomWords,
-      amount: 1,
-    }),
+    let q = `repo:${owner}/${repo}`;
+    let issues;
+    try {
+      issues = await octokit.rest.search.issuesAndPullRequests({
+        q,
+        is: "pr",
+        type: "pr",
+        per_page: 1,
+      });
+
+      return issues?.data?.items;
+    } catch (error) {
+      console.error("An error occurred while getting issues:", error.message);
+    }
+    return issues?.data?.items;
+  }
+  async function fetchJiraTickets(userTokens, commitTitle) {
+    const { jira_token, jira_refresh_token, cloudId, user } = userTokens;
+    if (!jira_token || !jira_refresh_token) {
+      return { error: "no jira token" };
+    } else {
+      const newAccessTokens = await updateTokensFromJira({
+        refresh_token: jira_refresh_token,
+      });
+      if (!newAccessTokens?.access_token) {
+        return { error: "no access_token" };
+      }
+
+      if (!cloudId) {
+        return { error: "no Jira cloudId" };
+      }
+      await updateTokens({
+        access_token: newAccessTokens.access_token,
+        refresh_token: newAccessTokens.refresh_token,
+        user: userTokens.user,
+      });
+
+      let jql = `(summary ~ "${commitTitle}") AND (description ~ "${commitTitle}") ORDER BY created DESC`;
+      let returnVal = await fetch(
+        `https://api.atlassian.com/ex/jira/${cloudId}/rest/api/3/search`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Bearer ${newAccessTokens.access_token}`,
+          },
+          body: JSON.stringify({
+            jql,
+            expand: ["renderedFields"],
+            maxResults: 1,
+          }),
+        }
+      )
+        .then((res) => res.json())
+        .then((resJson) => resJson.issues)
+        .catch((error) => {
+          console.error(error);
+        });
+
+      return returnVal;
+    }
+  }
+  async function fetchSlackConversations(userTokens, commitTitle) {
+    let { slack_token } = userTokens;
+    let slackValue = {};
+
+    if (!slack_token) {
+      slackValue = { error: "no slack token" };
+    } else {
+      let response = await searchMessageByText({
+        text: commitTitle,
+        user_token: slack_token,
+        count: 1,
+      });
+      slackValue = response?.messages?.matches;
+    }
+    return slackValue;
+  }
+  const [githubResult, jiraResult, slackResult] = await Promise.allSettled([
+    fetchGitHubIssues(userTokens, req.owner, req.repo),
+    fetchJiraTickets(userTokens, req.commitTitle),
+    fetchSlackConversations(userTokens, req.commitTitle),
   ]);
+
+  const githubIssues =
+    githubResult.status === "fulfilled"
+      ? githubResult.value
+      : handleRejection(githubResult.reason);
+  const jiraTickets =
+    jiraResult.status === "fulfilled"
+      ? jiraResult.value
+      : handleRejection(jiraResult.reason);
+  const slackConversations =
+    slackResult.status === "fulfilled"
+      ? slackResult.value
+      : handleRejection(slackResult.reason);
 
   successPosthogTracking({
     url: request.url,
     email: req.email,
     data: {
-      github: github.fullData || github.error,
-      jira: jira.fullData || jira.error,
-      confluence: confluence.fullData || confluence.error,
-      slack: slack.fullData || slack.error,
-      notion: notion.fullData || notion.error,
-      linear: linear.fullData || linear.error,
+      github: githubIssues,
+      jira: jiraTickets,
+      slack: slackConversations,
     },
   });
   return successResponse({
     data: {
-      github: github.data || github.error,
-      jira: jira.data || jira.error,
-      confluence: confluence.data || confluence.error,
-      slack: slack.data || slack.error,
-      notion: notion.data || notion.error,
-      linear: linear.data || linear.error,
+      github: githubIssues,
+      jira: jiraTickets,
+      slack: slackConversations,
     },
   });
 }
