3676f28719bf3e882a81059968b56c99620c886e
Esteban Dalel R <estebandalelr@gmail.com>
2023-08-15 10:08:15 -0500

    Extract tracking

--- app/api/user/settings/route.ts
+++ app/api/user/settings/route.ts
@@ -1,41 +1,41 @@
 import getUserSettings from "../../../../utils/db/user/settings";
 import validateParams from "../../../../utils/api/validateParams";
-import posthog from "../../../../utils/posthog/posthog";
 import {
   failedToFecthResponse,
   missingParamsResponse,
   successResponse,
 } from "../../../../utils/api/responses";
+import {
+  failedPosthogTracking,
+  missingParamsPosthogTracking,
+  successPosthogTracking,
+} from "../../../../utils/api/posthogTracking";
 
 export async function POST(request: Request) {
   const req = await request.json();
   const { missingParams } = validateParams(req, ["email"]);
 
   if (missingParams.length > 0) {
-    posthog.capture({
-      event: `${request.url}-missing-params`,
-      properties: missingParams,
+    missingParamsPosthogTracking({
+      missingParams,
+      url: request.url,
     });
     return missingParamsResponse({ missingParams });
   }
   try {
     let dbResponse = await getUserSettings({ email: req.email });
-    posthog.capture({
-      distinctId: req.email,
-      event: `${request.url}-success`,
-      properties: {
-        dbResponse,
-      },
+    successPosthogTracking({
+      email: req.email,
+      url: request.url,
+      data: dbResponse,
     });
     return successResponse({ data: dbResponse });
   } catch (err) {
     console.error("Error fetching db data:", err);
-    posthog.capture({
-      distinctId: req.email,
-      event: `${request.url}-failed`,
-      properties: {
-        error: err,
-      },
+    failedPosthogTracking({
+      error: err,
+      email: req.email,
+      url: request.url,
     });
     return failedToFecthResponse({ error: err });
   }
