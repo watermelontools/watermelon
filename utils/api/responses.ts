ea4408a7154e06a63275496147507ac988035d2b
Esteban Dalel R <estebandalelr@gmail.com>
2023-08-15 06:56:52 -0500

    Extract response types

---
+++ utils/api/responses.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+
+export function successResponse({ data }) {
+  return NextResponse.json(
+    {
+      data,
+    },
+    { status: 200 }
+  );
+}
+
+export function missingParamsResponse({ missingParams }) {
+  const missingParamsText = `Missing parameters: ${missingParams.join(", ")}`;
+  return NextResponse.json(
+    {
+      error: missingParamsText,
+    },
+    { status: 400 }
+  );
+}
+
+export function failedToFecthResponse({ error }) {
+  return NextResponse.json(
+    {
+      error,
+      message: "Failed to fetch db data.",
+    },
+    { status: 500 }
+  );
+}
