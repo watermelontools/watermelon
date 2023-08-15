cfbccfb9f107c5c2ea59e85e2fe796dee1d10160
Esteban Dalel R <estebandalelr@gmail.com>
2023-08-15 08:53:56 -0500

    Add error events

--- app/api/sendgrid/sendTeammateInvite/route.ts
+++ app/api/sendgrid/sendTeammateInvite/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { missingParamsResponse } from "../../../../utils/api/responses";
 import validateParams from "../../../../utils/api/validateParams";
+import posthog from "../../../../utils/posthog/posthog";
 import sendTeammateInvite from "../../../../utils/sendgrid/sendTeammateInvite";
 
 export async function POST(request: Request) {
@@ -13,6 +14,10 @@ export async function POST(request: Request) {
   ]);
 
   if (missingParams.length > 0) {
+    posthog.capture({
+      event: "api-sendgrid-sendTeammateInvite-missing-params",
+      properties: missingParams,
+    });
     return missingParamsResponse({ missingParams });
   }
   const { sender, email, inviteUrl, teamName } = req;
