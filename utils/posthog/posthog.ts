59350371443da61efb4a84bcec533ed1a5589bf1
Esteban Dalel R <estebandalelr@gmail.com>
2023-08-15 07:21:57 -0500

    Early return if no apiKey

--- utils/posthog/posthog.ts
+++ utils/posthog/posthog.ts
@@ -6,6 +6,7 @@ interface PostHogEvent {
   groups?: Record<string, any>;
 }
 function PostHogClient(apiKey: string) {
+  if (!apiKey) return { capture: () => {} };
   const posthogClient = new PostHog(apiKey, {
     host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   });
