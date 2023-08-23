df8b2e0742f637c34b56d8370d26ea3cad2de5ea
Esteban Dalel R <estebandalelr@gmail.com>
2023-08-23 16:40:17 -0500

    Fix imports, placeholder

--- utils/services/loading.tsx
+++ utils/services/loading.tsx
@@ -1,4 +1,4 @@
-import TimeToRedirect from "../../../components/redirect";
+import TimeToRedirect from "../../components/redirect";
 
 export default function LoadingConnectedService() {
   return (
@@ -10,7 +10,7 @@ export default function LoadingConnectedService() {
       </div>
       <img
         src="/logos/github.svg"
-        alt="linear user image"
+        alt="watermelon placeholder"
         className="avatar avatar-8"
       />
       <div>
