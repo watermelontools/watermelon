3bd2e27eb2bb21dbd56d5918c6ef78958700153d
Esteban Dalel R <estebandalelr@gmail.com>
2023-09-05 20:26:37 -0500

    Add workspace_name

--- utils/db/asana/saveUser.ts
+++ utils/db/asana/saveUser.ts
@@ -9,10 +9,11 @@ export default async ({
   refresh_token,
   avatar_url,
   workspace,
+  workspace_name,
   watermelon_user,
 }) => {
   let query = `EXEC dbo.create_asana @watermelon_user='${watermelon_user}', @id='${id}', 
- @avatar_url='${avatar_url}', @name='${name}', @location='${location}', @access_token='${access_token}', @refresh_token='${refresh_token}',@workspace='${workspace}', @email='${email}', @gid = '${gid}';
+ @avatar_url='${avatar_url}', @name='${name}', @location='${location}', @access_token='${access_token}', @refresh_token='${refresh_token}',@workspace='${workspace}',@workspace_name='${workspace_name}', @email='${email}', @gid = '${gid}';
  `;
   let resp = await executeRequest(query);
   return resp;
