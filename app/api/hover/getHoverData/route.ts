aa0d55a6fba22ca0d149cc61d458574be052524a
Esteban Dalel R <estebandalelr@gmail.com>
2023-09-07 14:22:26 -0500

    Use new service getter

--- app/api/hover/getHoverData/route.ts
+++ app/api/hover/getHoverData/route.ts
@@ -1,10 +1,3 @@
-import getGitHub from "../../../../utils/actions/getGitHub";
-import getJira from "../../../../utils/actions/getJira";
-import getSlack from "../../../../utils/actions/getSlack";
-import getNotion from "../../../../utils/actions/getNotion";
-import getLinear from "../../../../utils/actions/getLinear";
-import getConfluence from "../../../../utils/actions/getConfluence";
-import getAsana from "../../../../utils/actions/getAsana";
 import validateParams from "../../../../utils/api/validateParams";
 import {
   failedPosthogTracking,
@@ -16,7 +9,7 @@ import {
   missingParamsResponse,
   successResponse,
 } from "../../../../utils/api/responses";
-import executeRequest from "../../../../utils/db/azuredb";
+import getAllServices from "../../../../utils/actions/getAllServices";
 
 export async function POST(request: Request) {
   const req = await request.json();
@@ -32,38 +25,6 @@ export async function POST(request: Request) {
     missingParamsPosthogTracking({ url: request.url, missingParams });
     return missingParamsResponse({ missingParams });
   }
-  const query = `EXEC dbo.get_all_user_tokens @watermelon_user='${req.email}'`;
-  let wmUserData = await executeRequest(query);
-  const {
-    github_token,
-    jira_token,
-    jira_refresh_token,
-    confluence_token,
-    confluence_refresh_token,
-    confluence_id,
-    cloudId,
-    slack_token,
-    notion_token,
-    linear_token,
-    asana_token,
-    asana_workspace,
-    user_email,
-    watermelon_user,
-  } = wmUserData;
-  try {
-    wmUserData = await executeRequest(query);
-  } catch (error) {
-    console.error(
-      "An error occurred while getting user tokens:",
-      error.message
-    );
-    failedPosthogTracking({
-      url: request.url,
-      error: error.message,
-      email: req.email,
-    });
-    return failedToFetchResponse({ error: error.message });
-  }
 
   const searchStringSet = Array.from(new Set(req.commitTitle.split(" "))).join(
     " "
@@ -74,74 +35,48 @@ export async function POST(request: Request) {
     .split(" ")
     .sort(() => Math.random() - 0.5)
     .slice(0, 6);
-  const { repo, owner } = req;
-  const [github, jira, confluence, slack, notion, linear] = await Promise.all([
-    getGitHub({
-      repo,
-      owner,
-      github_token,
-      randomWords,
-      amount: 1,
-    }),
-    getJira({
-      user: user_email,
-      token: jira_token,
-      refresh_token: jira_refresh_token,
-      randomWords,
-      amount: 1,
-    }),
-    getConfluence({
-      token: confluence_token,
-      refresh_token: confluence_refresh_token,
-      cloudId: confluence_id,
-      user: user_email,
-      randomWords,
-      amount: 1,
-    }),
-    getSlack({
-      slack_token,
-      searchString: randomWords.join(" "),
-      amount: 1,
-    }),
-    getNotion({
-      notion_token,
-      randomWords,
-      amount: 1,
-    }),
-    getLinear({
-      linear_token,
-      randomWords,
-      amount: 1,
-    }),
-    getAsana({
-      access_token: asana_token,
-      user: user_email,
-      randomWords,
-      workspace: asana_workspace,
-      amount: 1,
-    }),
-  ]);
+  const { repo, owner, email } = req;
+
+  const serviceAnswers = await getAllServices({
+    email,
+    repo,
+    owner,
+    randomWords,
+    url: request.url,
+  });
+  const { error, github, jira, confluence, slack, notion, linear, asana } =
+    serviceAnswers;
+  if (error) {
+    failedPosthogTracking({
+      url: request.url,
+      error: error.message,
+      email: req.email,
+    });
+    return failedToFetchResponse({ error: error.message });
+  }
 
   successPosthogTracking({
     url: request.url,
     email: req.email,
     data: {
-      github: github.fullData || github.error,
-      jira: jira.fullData || jira.error,
-      confluence: confluence.fullData || confluence.error,
-      slack: slack.fullData || slack.error,
-      notion: notion.fullData || notion.error,
-      linear: linear.fullData || linear.error,
+      github: github?.fullData || github?.error,
+      jira: jira?.fullData || jira?.error,
+      confluence: confluence?.fullData || confluence?.error,
+      slack: slack?.fullData || slack?.error,
+      notion: notion?.fullData || notion?.error,
+      linear: linear?.fullData || linear?.error,
+      asana: asana?.fullData || asana?.error,
     },
   });
   return successResponse({
     data: {
-      github: github.data || github.error,
-      jira: jira.data || jira.error,
-      confluence: confluence.data || confluence.error,
-      slack: slack.data || slack.error,
-      notion: notion.data || notion.error,
-      linear: linear.data || linear.error,
+      github: github?.data || github?.error,
+      jira: jira?.data || jira?.error,
+      confluence: confluence?.data || confluence?.error,
+      slack: slack?.data || slack?.error,
+      notion: notion?.data || notion?.error,
+      linear: linear?.data || linear?.error,
+      asana: asana?.data || asana?.error,
     },
   });
 }
