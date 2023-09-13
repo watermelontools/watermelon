fcb992d12e6476ab2746ec517cfa15b20514f988
baristaGeek <estebanvargas94@gmail.com>
2023-09-13 11:54:38 -0500

    Refactor Watermelon as reviewer 0 to work sync and prevent race conditions

--- utils/actions/flagPullRequest.ts
+++ utils/actions/flagPullRequest.ts
@@ -1,44 +1,27 @@
 import { Octokit } from "octokit";
 
-async function flagPullRequest({
-  repo,
+function flagPullRequest({
+  repo, 
   owner,
   issue_number,
-  github_token,
-}): Promise<any> {
+  github_token
+}): any {
+
   if (!github_token) {
     return { error: "no github token" };
-  } else {
-    const octokit = new Octokit({
-      auth: github_token,
-    });
-
-    const labelsInPR = await octokit.rest.issues.listLabelsOnIssue({
-      owner,
-      repo,
-      issue_number,
-    });
-
-    await octokit.rest.issues.addLabels({
-      owner,
-      repo,
-      issue_number,
-      labels: ["🍉 Safe to Merge"],
-    });
-
-    // if (labelsInPR.data.some((label) => label.name === "🍉 Safe to Merge")) {
-    //   return { message: "PR already flagged as safe to merge" };
-    // } else {
-    //   await octokit.rest.issues.addLabels({
-    //     owner,
-    //     repo,
-    //     issue_number,
-    //     labels: ["🍉 Safe to Merge"],
-    //   });
-
-    //   return { message: "PR flagged as safe to merge" };
-    // }
   }
+
+  const octokit = new Octokit({
+    auth: github_token
+  });
+
+  octokit.rest.issues.addLabels({
+    owner,
+    repo,
+    issue_number,
+    labels: ["🍉 Safe to Merge"] 
+  });
+
 }
 
 export default flagPullRequest;
