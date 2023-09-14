0e857fec72aa9e0f2a51d0dea1803cfca1a32da4
baristaGeek <estebanvargas94@gmail.com>
2023-09-14 14:31:37 -0500

    Add param validation

--- utils/actions/labelPullRequest.ts
+++ utils/actions/labelPullRequest.ts
@@ -5,6 +5,11 @@ import {
   missingParamsPosthogTracking,
   successPosthogTracking,
 } from "../../utils/api/posthogTracking";
+import {
+  failedToFetchResponse,
+  missingParamsResponse
+} from "../../utils/api/responses";
+import validateParams from "../../utils/api/validateParams";
 
 const configuration = new Configuration({
   apiKey: process.env.OPEN_AI_KEY,
@@ -37,6 +42,16 @@ export default async function flagPullRequest({
 }) {
   const octokit = await app.getInstallationOctokit(installationId);
 
+  const { missingParams } = validateParams("", ["prTitle", "businessLogicSummary", "installationId", "owner", "repo", "issue_number", "reqUrl", "reqEmail"]);
+
+  if (missingParams.length > 0) {
+    missingParamsPosthogTracking({
+      missingParams,
+      url: reqUrl,
+    });
+    return missingParamsResponse({ missingParams });
+  }
+
   const prompt = `The goal of this PR is to: ${prTitle}. \n The information related to this PR is: ${businessLogicSummary}. \n On a scale of 1(very different)-10(very similar), how similar the PR's goal and the PR's related information are? Take into account semantics. Don't explain your reasoning, just print the rating. Don't give a range for the rating, print a single value.`;
 
   try {
@@ -63,7 +78,7 @@ export default async function flagPullRequest({
           },
         });
 
-        if (prRating >= 9) {
+        if (prRating >= 2) {
           octokit.request(
             "POST /repos/{owner}/{repo}/issues/{issue_number}/labels", //add label
             {
@@ -92,6 +107,6 @@ export default async function flagPullRequest({
       error: error.message,
       email: reqEmail,
     });
-    return "Error" + error;
+    return failedToFetchResponse({ error });
   }
 }
