3c1e55b18f63019186ea6a0b45e53d5cbe095fba
baristaGeek <estebanvargas94@gmail.com>
2023-09-28 15:58:56 -0500

    Move base54toUint8Array to the token salting file

--- utils/encryption/base64ToUint8Array.ts
+++
@@ -1,8 +0,0 @@
-export default function base64ToUint8Array(base64String: string): Uint8Array {
-    const binaryString = atob(base64String);
-    const byteArray = new Uint8Array(binaryString.length);
-    for (let i = 0; i < binaryString.length; i++) {
-        byteArray[i] = binaryString.charCodeAt(i);
-    }
-    return byteArray;
-}
--- utils/encryption/tokenSalting.ts
+++ utils/encryption/tokenSalting.ts
@@ -1,4 +1,11 @@
-import base64ToUint8Array from "./base64ToUint8Array";
+function base64ToUint8Array(base64String: string): Uint8Array {
+    const binaryString = atob(base64String);
+    const byteArray = new Uint8Array(binaryString.length);
+    for (let i = 0; i < binaryString.length; i++) {
+        byteArray[i] = binaryString.charCodeAt(i);
+    }
+    return byteArray;
+}
 
 export function encrypt(plaintext: string): string {
     if(!process.env.NEXT_PUBLIC_ENCRYPTION_KEY) {
