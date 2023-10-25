28a53a4f1bb2075b1bdbf5749ef0446f0e9d6bfa
baristaGeek <estebanvargas94@gmail.com>
2023-10-25 11:55:42 -0500

    Add consistent CSS for boxes regardless of login state + prettify design

--- components/AsanaLoginLink.tsx
+++ components/AsanaLoginLink.tsx
@@ -1,19 +1,33 @@
 import Link from "next/link";
 
 const AsanaLoginLink = ({ userEmail }) => (
-  <div className="Box">
-    <Link
-      href={`https://app.asana.com/-/oauth_authorize?client_id=${process.env.ASANA_CLIENT_ID}&redirect_uri=https%3A%2F%2Fapp.watermelontools.com%2Fasana&response_type=code&state=${userEmail}`}
-      className="button block"
-    >
-      <div className="Box d-flex flex-items-center flex-justify-start p-2">
-        <img className="avatar avatar-8" src="/logos/asana.svg" />
-        <div className="p-2">
-          <h2>Login to Asana</h2>
-          <p>View your Tasks and Stories</p>
+  <div className="Box" style={{ height: "100%" }}>
+    <div className="Subhead px-3 pt-2">
+      <h2 className="Subhead-heading d-flex flex-items-center flex-justify-start">
+        <img 
+          className="avatar avatar-4"
+          src="/logos/asana.svg" 
+        />
+        <span>Asana</span>
+      </h2>
+    </div>
+
+    <div className="pl-3 pr-3 pt-1 pb-3" style={{ flex: 1 }}>
+      <Link
+        href={`https://app.asana.com/-/oauth_authorize?client_id=${process.env.ASANA_CLIENT_ID}&redirect_uri=https%3A%2F%2Fapp.watermelontools.com%2Fasana&response_type=code&state=${userEmail}`}
+        className="button block"
+      >
+        <div className="d-flex flex-items-center">
+          <img className="avatar avatar-8 mr-2" src="/logos/asana.svg" />
+          <div style={{ flex: 1 }}>
+            <h3>Login to Asana</h3>
+            <p>View your Tasks and Stories</p>
+          </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   </div>
 );
+
+
 export default AsanaLoginLink;
--- components/GitLabLoginLink.tsx
+++ components/GitLabLoginLink.tsx
@@ -1,19 +1,33 @@
 import Link from "next/link";
 
 const GitLabLoginLink = ({ userEmail }) => (
-  <div>
-    <Link
-      href={`https://gitlab.com/oauth/authorize?client_id=399843bda5477334e51f2aa88d96dfff694ce2da3130dabc685e34e1aa55b8f3&redirect_uri=https://app.watermelontools.com/gitlab&response_type=code&state=${userEmail}&scope=api%20read_api%20read_user%20read_repository%20write_repository%20read_registry%20openid%20profile%20email`}
-      className="button block"
-    >
-      <div className="Box d-flex flex-items-center flex-justify-start p-2">
-        <img className="avatar avatar-8" src="/logos/gitlab.svg" />
-        <div className="p-2">
-          <h2>Login to GitLab</h2>
-          <p>View your Assigned Issues and Relevant Pull Requests</p>
+  <div className="Box" style={{ height: "100%" }}>
+    <div className="Subhead px-3 pt-2">
+      <h2 className="Subhead-heading d-flex flex-items-center flex-justify-start">
+        <img 
+          className="avatar avatar-4"
+          src="/logos/gitlab.svg" 
+        />
+        <span>GitLab</span>
+      </h2>
+    </div>
+
+    <div className="pl-3 pr-3 pt-1 pb-3" style={{ flex: 1 }}>
+      <Link
+        href={`https://gitlab.com/oauth/authorize?client_id=399843bda5477334e51f2aa88d96dfff694ce2da3130dabc685e34e1aa55b8f3&redirect_uri=https://app.watermelontools.com/gitlab&response_type=code&state=${userEmail}&scope=api%20read_api%20read_user%20read_repository%20write_repository%20read_registry%20openid%20profile%20email`}
+        className="button block"
+      >
+        <div className="d-flex flex-items-center">
+          <img className="avatar avatar-8 mr-2" src="/logos/gitlab.svg" />
+          <div style={{ flex: 1 }}>
+            <h3>Login to GitLab</h3>
+            <p>View your Assigned Issues and Relevant Pull Requests</p>
+          </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   </div>
 );
+
+
 export default GitLabLoginLink;
