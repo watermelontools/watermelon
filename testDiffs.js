87c86d5c956aa78ca72f4ad7990f68de723c2f0a
baristaGeek <estebanvargas94@gmail.com>
2023-11-08 11:23:06 -0500

    Version A

--- testDiffs.js
+++ testDiffs.js
@@ -1,1 +1,5 @@
-
+function processData(input) {
+    let result = input.map(item => item * 2).filter(item => item > 10).reduce((a, b) => a + b, 0);
+    console.log("Processing complete: ", result);
+    return result;
+}
