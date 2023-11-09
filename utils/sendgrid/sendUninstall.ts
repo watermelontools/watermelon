8a6e8a29c2a6b91f1bd54c9640ae61a4f9e5ad8e
Esteban Dalel R <estebandalelr@gmail.com>
2023-11-09 13:03:55 -0500

    Remove unused params

--- utils/sendgrid/sendUninstall.ts
+++ utils/sendgrid/sendUninstall.ts
@@ -1,19 +1,10 @@
-export default async function sendUninstall({
-  emails,
-  sender,
-}: {
-  emails: string[];
-  sender: string;
-}) {
+export default async function sendUninstall({ emails }: { emails: string[] }) {
   const sgMail = require("@sendgrid/mail");
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const msg = {
     to: emails,
     from: "info@watermelon.tools",
     templateId: "d-21dcf37205e4479d830dbcfd35cb41d6",
-    dynamic_template_data: {
-      sender,
-    },
   };
   sgMail
     .sendMultiple(msg)
