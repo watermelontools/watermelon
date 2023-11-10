a21fcc9e64b9a2aa61fc284f74414ce565c1f667
baristaGeek <estebanvargas94@gmail.com>
2023-11-10 11:52:19 -0500

    Random ruby function that contains counter example string

--- hello.rb
+++ hello.rb
@@ -1,1 +1,8 @@
-puts "Hello world"
+def check_email_selector(input)
+  if input.include?('input[type="email"]')
+    return "Email input detected"
+  else
+    return "No email input found"
+  end
+end
+
