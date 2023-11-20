d68e9346a342fec17f5cf410ff16a10cc6bf8426
baristaGeek <estebanvargas94@gmail.com>
2023-11-20 17:42:55 -0500

    Remove references to IDE extensions on the random text

--- utils/actions/markdownHelpers/randomText.ts
+++ utils/actions/markdownHelpers/randomText.ts
@@ -2,9 +2,6 @@ const randomText = () => {
   const textList = [
     "\n[Why not invite more people to your team?](https://app.watermelontools.com/team)",
     "\n[Have you starred Watermelon?](https://github.com/watermelontools/watermelon)",
-    "\n[Try us on VSCode!](https://marketplace.visualstudio.com/items?itemName=WatermelonTools.watermelon-tools)",
-    "\n[Try us on VSCodium!](https://open-vsx.org/extension/WatermelonTools/watermelon-tools)",
-    "\n[Try us on any JetBrains IDE!](https://plugins.jetbrains.com/plugin/22720-watermelon-context)",
   ];
 
   let randomChance = Math.random() * 100;
