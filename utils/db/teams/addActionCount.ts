156aaf33b1f751162cf4c47f563cebb8bc798c2e
baristaGeek <estebanvargas94@gmail.com>
2023-11-27 16:29:15 -0500

    Fix stored procedure call

--- utils/db/teams/addActionCount.ts
+++ utils/db/teams/addActionCount.ts
@@ -2,7 +2,8 @@ import executeRequest from "../azuredb";
 
 export default async ({ owner }) => {
   let query = await executeRequest(
-    `EXEC dbo.increment_owner_github_app_uses @watermelon_user = '${owner}'`
+    `EXEC dbo.increment_owner_github_app_uses @owner = '${owner}'`
   );
+  console.log("return query", query);
   return query;
 };
