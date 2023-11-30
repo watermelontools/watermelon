a72f82950bf43cb9d5bf4a7ae7e980c6ebde24da
Esteban Dalel R <estebandalelr@gmail.com>
2023-11-30 16:32:44 -0500

    Fix patching

--- utils/db/user/patchUserSettings.ts
+++ utils/db/user/patchUserSettings.ts
@@ -6,8 +6,27 @@ export default async function updateUserSettings({
 }): Promise<any> {
   try {
     let data = await executeRequest(
-      `EXEC dbo.update_userSettings @watermelon_user = '${email}', @AISummary='${userSettings.AISummary}', @JiraTickets='${userSettings.JiraTickets}', @SlackMessages='${userSettings.SlackMessages}', @GitHubPRs='${userSettings.GitHubPRs}', @NotionPages= '${userSettings.NotionPages}', @LinearTickets= '${userSettings.LinearTickets}', @ConfluenceDocs= '${userSettings.ConfluenceDocs}'`
+      `EXEC dbo.update_userSettings @watermelon_user = '${email}', @AISummary='${
+        userSettings.AISummary
+      }', 
+      @AdditionalSettings='${JSON.stringify(
+        userSettings.AdditionalSettings
+      )}', @Badges='${userSettings.Badges}', @CodeComments='${
+        userSettings.CodeComments
+      }'`
     );
+    console.log(
+      "pasted",
+      `EXEC dbo.update_userSettings @watermelon_user = '${email}', @AISummary='${
+        userSettings.AISummary
+      }', 
+      @AdditionalSettings=N'${JSON.stringify(
+        userSettings.AdditionalSettings
+      )}', @Badges='${userSettings.Badges}', @CodeComments='${
+        userSettings.CodeComments
+      }'  );`
+    );
+    console.log("data", data);
     return data;
   } catch (err) {
     console.error(err);
