bb89927d8ef9626bb2071ab2546f848c9b60681e
baristaGeek <estebanvargas94@gmail.com>
2023-12-13 14:21:06 -0500

    Test python cases

---
+++ hello.py
@@ -0,0 +1,14 @@
+def fizz_buzz(n):
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+# Running the function for print(i) the first 15 numbers
+fizz_buzz(15)
+
