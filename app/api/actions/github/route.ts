d855733f83b4c723954e8bfe387be0eb59ef68c4
Esteban Dalel R <estebandalelr@gmail.com>
2023-12-22 12:39:41 -0500

    Fix max

--- app/api/actions/github/route.ts
+++ app/api/actions/github/route.ts
@@ -551,41 +551,6 @@ export async function POST(request: Request) {
           });
       }
 
-      // If the count is surpassed, we replace the
-      if (count.github_app_uses > 500) {
-        textToWrite = `Your team has surpassed the free monthly usage. [Please click here](https://buy.stripe.com/28o0289KVaYV5wY004) to upgrade.`;
-
-        const comments = await octokit.request(
-          "GET /repos/{owner}/{repo}/issues/{issue_number}/comments?sort=created&direction=desc",
-          {
-            owner,
-            repo,
-            issue_number: number,
-            headers: {
-              "X-GitHub-Api-Version": "2022-11-28",
-            },
-          }
-        );
-
-        // Find our bot's comment
-        let botComment = comments.data.find((comment) => {
-          return comment?.user?.login.includes(
-            "watermelon-copilot-for-code-review"
-          );
-        });
-
-        // Update the existing comment
-        await octokit.request(
-          "PATCH /repos/{owner}/{repo}/issues/comments/{comment_id}",
-          {
-            owner,
-            repo,
-            comment_id: botComment.id,
-            body: textToWrite,
-          }
-        );
-      }
-
       successPosthogTracking({
         url: request.url,
         email: user_email,
