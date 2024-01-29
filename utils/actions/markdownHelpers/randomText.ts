bd377e81b97aeb80574ea2f6a08b60ec2abc69ed
Esteban Dalel R <estebandalelr@gmail.com>
2024-01-29 15:15:12 -0500

    Add more texts (#439)

--- utils/actions/markdownHelpers/randomText.ts
+++ utils/actions/markdownHelpers/randomText.ts
@@ -2,6 +2,9 @@ const randomText = () => {
   const textList = [
     "\n[Why not invite more people to your team?](https://app.watermelontools.com/team)",
     "\n[Have you starred Watermelon?](https://github.com/watermelontools/watermelon)",
+    "\n[Follow us Watermelon on 𝕏!](https://x.com/watermelontools)",
+    "\n[Have you checked out our blog?](https://watermelontools.com/blog)",
+    "\n[We are also on LinkedIn](https://www.linkedin.com/company/watermelon-tools)",
   ];
 
   let randomChance = Math.random() * 100;
